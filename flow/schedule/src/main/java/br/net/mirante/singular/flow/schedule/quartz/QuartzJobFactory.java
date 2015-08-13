78e80d452c56204baf5d06499737f7c5374fa775
Alfredo Fialho <alfredo.fialho@mirante.net.br>
2015-08-13 12:10:07 -0300

    [QUARTZ] Continuando com a implementação da interface de agendamento.

---
+++ flow/schedule/src/main/java/br/net/mirante/singular/flow/schedule/quartz/QuartzJobFactory.java
@@ -0,0 +1,14 @@
+package br.net.mirante.singular.flow.schedule.quartz;
+
+import org.quartz.Job;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.spi.JobFactory;
+import org.quartz.spi.TriggerFiredBundle;
+
+public class QuartzJobFactory implements JobFactory {
+    @Override
+    public Job newJob(TriggerFiredBundle triggerFiredBundle, Scheduler scheduler) throws SchedulerException {
+        return null;
+    }
+}
--- flow/schedule/src/main/java/br/net/mirante/singular/flow/schedule/quartz/QuartzSchedulerFactory.java
+++ flow/schedule/src/main/java/br/net/mirante/singular/flow/schedule/quartz/QuartzSchedulerFactory.java
@@ -15,14 +15,12 @@
  */
 package br.net.mirante.singular.flow.schedule.quartz;
 
-import javax.sql.DataSource;
-
 import java.io.IOException;
-import java.util.Map;
 import java.util.Properties;
 import java.util.ResourceBundle;
-import java.util.concurrent.Executor;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 import org.quartz.SchedulerFactory;
@@ -60,6 +58,8 @@ public class QuartzSchedulerFactory {
 
     public static final int DEFAULT_THREAD_COUNT = 10;
 
+    protected final Log logger = LogFactory.getLog(getClass());
+
     private Class<? extends SchedulerFactory> schedulerFactoryClass = StdSchedulerFactory.class;
 
     private String schedulerName;
@@ -67,8 +67,6 @@ public class QuartzSchedulerFactory {
     private ResourceBundle configLocation;
 
     private Properties quartzProperties;
-
-    private DataSource dataSource;
 
     private JobFactory jobFactory;
 
@@ -131,18 +129,6 @@ public class QuartzSchedulerFactory {
     }
 
     /**
-     * Set the default DataSource to be used by the Scheduler. If set,
-     * this will override corresponding settings in Quartz properties.
-     * <p>Note: If this is set, the Quartz settings should not define
-     * a job store "dataSource" to avoid meaningless double configuration.
-     *
-     * @see #setQuartzProperties
-     */
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    /**
      * Set the Quartz JobFactory to use for this Scheduler.
      * <p>Default is {@link QuartzJobFactory}, which supports
      * {@link br.net.mirante.singular.flow.schedule.IScheduledJob} objects as well as standard Quartz
@@ -186,70 +172,21 @@ public class QuartzSchedulerFactory {
         }
     }
 
-    //---------------------------------------------------------------------
-    // Implementation of InitializingBean interface
-    //---------------------------------------------------------------------
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        if (this.dataSource == null && this.nonTransactionalDataSource != null) {
-            this.dataSource = this.nonTransactionalDataSource;
-        }
-
-        if (this.applicationContext != null && this.resourceLoader == null) {
-            this.resourceLoader = this.applicationContext;
-        }
-
-        // Create SchedulerFactory instance...
-        SchedulerFactory schedulerFactory = BeanUtils.instantiateClass(this.schedulerFactoryClass);
+    public void initialize() throws Exception {
+        SchedulerFactory schedulerFactory = this.schedulerFactoryClass.newInstance();
         initSchedulerFactory(schedulerFactory);
 
-        if (this.resourceLoader != null) {
-            // Make given ResourceLoader available for SchedulerFactory configuration.
-            configTimeResourceLoaderHolder.set(this.resourceLoader);
-        }
-        if (this.taskExecutor != null) {
-            // Make given TaskExecutor available for SchedulerFactory configuration.
-            configTimeTaskExecutorHolder.set(this.taskExecutor);
-        }
-        if (this.dataSource != null) {
-            // Make given DataSource available for SchedulerFactory configuration.
-            configTimeDataSourceHolder.set(this.dataSource);
-        }
-        if (this.nonTransactionalDataSource != null) {
-            // Make given non-transactional DataSource available for SchedulerFactory configuration.
-            configTimeNonTransactionalDataSourceHolder.set(this.nonTransactionalDataSource);
-        }
+        this.scheduler = createScheduler(schedulerFactory, this.schedulerName);
+        populateSchedulerContext();
 
-        // Get Scheduler instance from SchedulerFactory.
-        try {
-            this.scheduler = createScheduler(schedulerFactory, this.schedulerName);
-            populateSchedulerContext();
-
-            if (!this.jobFactorySet && !(this.scheduler instanceof RemoteScheduler)) {
-                // Use AdaptableJobFactory as default for a local Scheduler, unless when
-                // explicitly given a null value through the "jobFactory" bean property.
-                this.jobFactory = new AdaptableJobFactory();
-            }
-            if (this.jobFactory != null) {
-                if (this.jobFactory instanceof SchedulerContextAware) {
-                    ((SchedulerContextAware) this.jobFactory).setSchedulerContext(this.scheduler.getContext());
-                }
-                this.scheduler.setJobFactory(this.jobFactory);
-            }
-        } finally {
-            if (this.resourceLoader != null) {
-                configTimeResourceLoaderHolder.remove();
-            }
-            if (this.taskExecutor != null) {
-                configTimeTaskExecutorHolder.remove();
-            }
-            if (this.dataSource != null) {
-                configTimeDataSourceHolder.remove();
-            }
-            if (this.nonTransactionalDataSource != null) {
-                configTimeNonTransactionalDataSourceHolder.remove();
-            }
+        if (!this.jobFactorySet && !(this.scheduler instanceof RemoteScheduler)) {
+            /* Use QuartzJobFactory as default for a local Scheduler, unless when
+             * explicitly given a null value through the "jobFactory" property.
+             */
+            this.jobFactory = new QuartzJobFactory();
+        }
+        if (this.jobFactory != null) {
+            this.scheduler.setJobFactory(this.jobFactory);
         }
 
         registerListeners();
@@ -262,32 +199,10 @@ public class QuartzSchedulerFactory {
      * @param schedulerFactory the SchedulerFactory to initialize
      */
     private void initSchedulerFactory(SchedulerFactory schedulerFactory) throws SchedulerException, IOException {
-        if (!(schedulerFactory instanceof StdSchedulerFactory)) {
-            if (this.configLocation != null || this.quartzProperties != null ||
-                    this.taskExecutor != null || this.dataSource != null) {
-                throw new IllegalArgumentException(
-                        "StdSchedulerFactory required for applying Quartz properties: " + schedulerFactory);
-            }
-            // Otherwise assume that no initialization is necessary...
-            return;
-        }
-
         Properties mergedProps = new Properties();
 
-        if (this.resourceLoader != null) {
-            mergedProps.setProperty(StdSchedulerFactory.PROP_SCHED_CLASS_LOAD_HELPER_CLASS,
-                    ResourceLoaderClassLoadHelper.class.getName());
-        }
-
-        if (this.taskExecutor != null) {
-            mergedProps.setProperty(StdSchedulerFactory.PROP_THREAD_POOL_CLASS,
-                    LocalTaskExecutorThreadPool.class.getName());
-        } else {
-            // Set necessary default properties here, as Quartz will not apply
-            // its default configuration when explicitly given properties.
-            mergedProps.setProperty(StdSchedulerFactory.PROP_THREAD_POOL_CLASS, SimpleThreadPool.class.getName());
-            mergedProps.setProperty(PROP_THREAD_COUNT, Integer.toString(DEFAULT_THREAD_COUNT));
-        }
+        mergedProps.setProperty(StdSchedulerFactory.PROP_THREAD_POOL_CLASS, SimpleThreadPool.class.getName());
+        mergedProps.setProperty(PROP_THREAD_COUNT, Integer.toString(DEFAULT_THREAD_COUNT));
 
         if (this.configLocation != null) {
             if (logger.isInfoEnabled()) {
@@ -297,10 +212,6 @@ public class QuartzSchedulerFactory {
         }
 
         CollectionUtils.mergePropertiesIntoMap(this.quartzProperties, mergedProps);
-
-        if (this.dataSource != null) {
-            mergedProps.put(StdSchedulerFactory.PROP_JOB_STORE_CLASS, LocalDataSourceJobStore.class.getName());
-        }
 
         // Make sure to set the scheduler name as configured in the Spring configuration.
         if (this.schedulerName != null) {
@@ -312,7 +223,7 @@ public class QuartzSchedulerFactory {
 
     /**
      * Create the Scheduler instance for the given factory and scheduler name.
-     * Called by {@link #afterPropertiesSet}.
+     * Called by {@link #initialize()}.
      * <p>The default implementation invokes SchedulerFactory's {@code getScheduler}
      * method. Can be overridden for custom Scheduler creation.
      *
@@ -321,40 +232,25 @@ public class QuartzSchedulerFactory {
      * @return the Scheduler instance
      *
      * @throws SchedulerException if thrown by Quartz methods
-     * @see #afterPropertiesSet
+     * @see #initialize()
      * @see org.quartz.SchedulerFactory#getScheduler
      */
+    @SuppressWarnings("SynchronizationOnLocalVariableOrMethodParameter")
     protected Scheduler createScheduler(SchedulerFactory schedulerFactory, String schedulerName)
             throws SchedulerException {
-
-        // Override thread context ClassLoader to work around naive Quartz ClassLoadHelper loading.
-        Thread currentThread = Thread.currentThread();
-        ClassLoader threadContextClassLoader = currentThread.getContextClassLoader();
-        boolean overrideClassLoader = (this.resourceLoader != null &&
-                !this.resourceLoader.getClassLoader().equals(threadContextClassLoader));
-        if (overrideClassLoader) {
-            currentThread.setContextClassLoader(this.resourceLoader.getClassLoader());
-        }
-        try {
-            SchedulerRepository repository = SchedulerRepository.getInstance();
-            synchronized (repository) {
-                Scheduler existingScheduler = (schedulerName != null ? repository.lookup(schedulerName) : null);
-                Scheduler newScheduler = schedulerFactory.getScheduler();
-                if (newScheduler == existingScheduler) {
-                    throw new IllegalStateException("Active Scheduler of name '" + schedulerName + "' already registered " +
-                            "in Quartz SchedulerRepository. Cannot create a new Spring-managed Scheduler of the same name!");
-                }
-                if (!this.exposeSchedulerInRepository) {
-                    // Need to remove it in this case, since Quartz shares the Scheduler instance by default!
-                    SchedulerRepository.getInstance().remove(newScheduler.getSchedulerName());
-                }
-                return newScheduler;
+        SchedulerRepository repository = SchedulerRepository.getInstance();
+        synchronized (repository) {
+            Scheduler existingScheduler = (schedulerName != null ? repository.lookup(schedulerName) : null);
+            Scheduler newScheduler = schedulerFactory.getScheduler();
+            if (newScheduler == existingScheduler) {
+                throw new IllegalStateException("Active Scheduler of name '" + schedulerName + "' already registered " +
+                        "in Quartz SchedulerRepository. Cannot create a new Spring-managed Scheduler of the same name!");
             }
-        } finally {
-            if (overrideClassLoader) {
-                // Reset original thread context ClassLoader.
-                currentThread.setContextClassLoader(threadContextClassLoader);
+            if (!this.exposeSchedulerInRepository) {
+                // Need to remove it in this case, since Quartz shares the Scheduler instance by default!
+                SchedulerRepository.getInstance().remove(newScheduler.getSchedulerName());
             }
+            return newScheduler;
         }
     }
 
@@ -400,16 +296,17 @@ public class QuartzSchedulerFactory {
                 public void run() {
                     try {
                         Thread.sleep(startupDelay * 1000);
-                    } catch (InterruptedException ex) {
-                        // simply proceed
+                    } catch (InterruptedException e) {
+                        logger.info(e.getMessage(), e);
                     }
                     if (logger.isInfoEnabled()) {
                         logger.info("Starting Quartz Scheduler now, after delay of " + startupDelay + " seconds");
                     }
                     try {
                         scheduler.start();
-                    } catch (SchedulerException ex) {
-                        throw new SchedulingException("Could not start Quartz Scheduler after delay", ex);
+                    } catch (SchedulerException e) {
+                        logger.error(e.getMessage(), e);
+                        throw new RuntimeException(e);
                     }
                 }
             };
@@ -419,86 +316,45 @@ public class QuartzSchedulerFactory {
         }
     }
 
-    //---------------------------------------------------------------------
-    // Implementation of FactoryBean interface
-    //---------------------------------------------------------------------
-
-    @Override
-    public Scheduler getScheduler() {
-        return this.scheduler;
+    public void start() throws SchedulerException {
+        start(0);
     }
 
-    @Override
-    public Scheduler getObject() {
-        return this.scheduler;
-    }
-
-    @Override
-    public Class<? extends Scheduler> getObjectType() {
-        return (this.scheduler != null) ? this.scheduler.getClass() : Scheduler.class;
-    }
-
-    @Override
-    public boolean isSingleton() {
-        return true;
-    }
-
-    //---------------------------------------------------------------------
-    // Implementation of SmartLifecycle interface
-    //---------------------------------------------------------------------
-
-    @Override
-    public void start() throws SchedulingException {
+    public void start(int startupDelay) throws SchedulerException {
         if (this.scheduler != null) {
-            try {
-                startScheduler(this.scheduler, this.startupDelay);
-            } catch (SchedulerException ex) {
-                throw new SchedulingException("Could not start Quartz Scheduler", ex);
-            }
+            startScheduler(this.scheduler, startupDelay);
         }
     }
 
-    @Override
-    public void stop() throws SchedulingException {
+    public void stop() throws SchedulerException {
         if (this.scheduler != null) {
-            try {
-                this.scheduler.standby();
-            } catch (SchedulerException ex) {
-                throw new SchedulingException("Could not stop Quartz Scheduler", ex);
-            }
+            this.scheduler.standby();
         }
     }
 
-    @Override
-    public void stop(Runnable callback) throws SchedulingException {
+    public void stop(Runnable callback) throws SchedulerException {
         stop();
         callback.run();
     }
 
-    @Override
-    public boolean isRunning() throws SchedulingException {
+    public boolean isRunning() {
         if (this.scheduler != null) {
             try {
                 return !this.scheduler.isInStandbyMode();
-            } catch (SchedulerException ex) {
+            } catch (SchedulerException e) {
+                logger.info(e.getMessage(), e);
                 return false;
             }
         }
         return false;
     }
 
-    //---------------------------------------------------------------------
-    // Implementation of DisposableBean interface
-    //---------------------------------------------------------------------
-
     /**
      * Shut down the Quartz scheduler on bean factory shutdown,
      * stopping all scheduled jobs.
      */
-    @Override
     public void destroy() throws SchedulerException {
         logger.info("Shutting down Quartz Scheduler");
         this.scheduler.shutdown(this.waitForJobsToCompleteOnShutdown);
     }
-
 }
