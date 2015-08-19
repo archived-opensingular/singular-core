2042403ee2cba9e1d9a42f105c52b3613ef95e0d
Alfredo Fialho <alfredo.fialho@mirante.net.br>
2015-08-19 17:02:49 -0300

    [UI-Admin] Iniciando "wicketization".

--- flow/ui-admin/src/main/java/br/net/mirante/singular/view/template/Template.java
+++ flow/ui-admin/src/main/java/br/net/mirante/singular/view/template/Template.java
@@ -1,6 +1,14 @@
 package br.net.mirante.singular.view.template;
 
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.ResourceModel;
 
 public class Template extends WebPage {
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        add(new Label("pageTitle", new ResourceModel("label.page.title.local")));
+    }
 }
