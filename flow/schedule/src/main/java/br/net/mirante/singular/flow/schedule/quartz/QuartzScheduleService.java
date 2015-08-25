e1e659c952946ccd5ea7973ef422d3ddbcb1c6e6
Lucas Lopes <lucas.lopes@mirante.net.br>
2015-08-25 18:26:51 -0300

    [quartz] implementação do schedule service

---
+++ flow/schedule/src/main/java/br/net/mirante/singular/flow/schedule/quartz/QuartzScheduleService.java
@@ -0,0 +1,43 @@
+package br.net.mirante.singular.flow.schedule.quartz;
+
+import java.util.ResourceBundle;
+
+import org.quartz.Scheduler;
+import org.quartz.Trigger;
+
+import br.net.mirante.singular.flow.schedule.IScheduleService;
+import br.net.mirante.singular.flow.schedule.IScheduledJob;
+
+import com.google.common.base.Throwables;
+
+public class QuartzScheduleService implements IScheduleService {
+
+    private final QuartzSchedulerFactory quartzSchedulerFactory;
+
+    public QuartzScheduleService() {
+        quartzSchedulerFactory = new QuartzSchedulerFactory();
+        quartzSchedulerFactory.setSchedulerName("SingularFlowScheduler");
+        quartzSchedulerFactory.setConfigLocation(ResourceBundle.getBundle("quartz"));
+        try {
+            quartzSchedulerFactory.initialize();
+            quartzSchedulerFactory.start();
+        } catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+    }
+
+    @Override
+    public void schedule(IScheduledJob scheduledJob) {
+        try {
+            Trigger trigger = QuartzTriggerFactory.newTrigger().forJob(scheduledJob::run).withScheduleData(scheduledJob.getScheduleData()).build();
+
+            getScheduler().scheduleJob(trigger);
+        } catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+    }
+
+    public Scheduler getScheduler() {
+        return quartzSchedulerFactory.getScheduler();
+    }
+}
