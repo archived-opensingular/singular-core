fb55d83b865eb280222bfc0231a3454397d16bbb
Alfredo Fialho <alfredo.fialho@mirante.net.br>
2015-08-28 11:34:19 -0300

    [UI-Admin] Revisão de código.

--- flow/ui-admin/src/main/java/br/net/mirante/singular/service/PesquisaService.java
+++ flow/ui-admin/src/main/java/br/net/mirante/singular/service/PesquisaService.java
@@ -1,15 +1,14 @@
 package br.net.mirante.singular.service;
 
-import br.net.mirante.singular.dao.PesquisaDAO;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import javax.inject.Inject;
-import java.util.Base64;
 import java.util.List;
 import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.net.mirante.singular.dao.PesquisaDAO;
 
 @Service
 @Transactional(readOnly = true)
@@ -17,19 +16,6 @@ public class PesquisaService {
 
     @Inject
     private PesquisaDAO pesquisaDAO;
-
-    public int countAll() {
-        return pesquisaDAO.countAll();
-    }
-
-    public byte[] retrieveProcessDiagram(String sigla) {
-        RestTemplate restTemplate = new RestTemplate();
-        UriComponentsBuilder uriComponentsBuilder =
-                UriComponentsBuilder.fromHttpUrl("http://localhost:8080/alocpro/rest/diagram")
-                        .queryParam("sigla", sigla);
-        String encodedImage = restTemplate.getForObject(uriComponentsBuilder.build().encode().toUri(), String.class);
-        return Base64.getDecoder().decode(encodedImage);
-    }
 
     public List<Map<String, String>> retrieveMeanTimeByProcess() {
         return pesquisaDAO.retrieveMeanTimeByProcess();
