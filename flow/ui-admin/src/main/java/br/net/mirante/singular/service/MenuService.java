ebec2e28f68d469a6c1186fbacd28af16c5771f2
Alfredo Fialho <alfredo.fialho@mirante.net.br>
2015-09-17 18:04:57 -0300

    [FLOW-PERSISTENCE] Implementação do UIAdminFacade: Menu.

--- flow/ui-admin/src/main/java/br/net/mirante/singular/service/MenuService.java
+++ flow/ui-admin/src/main/java/br/net/mirante/singular/service/MenuService.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import org.apache.commons.lang3.tuple.Pair;
 
-import br.net.mirante.singular.flow.core.dto.IMenuItemDTO;
+import br.net.mirante.singular.dao.MenuItemDTO;
 
 public interface MenuService {
 
-    List<IMenuItemDTO> retrieveAllCategories();
+    List<MenuItemDTO> retrieveAllCategories();
 
     Pair<Long, Long> retrieveCategoryDefinitionIdsByCode(String code);
 }
