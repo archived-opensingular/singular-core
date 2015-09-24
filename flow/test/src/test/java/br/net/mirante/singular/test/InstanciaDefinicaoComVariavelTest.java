b52610b91fd2595881f6e98c21ee36184d988041
Alfredo Fialho <alfredo.fialho@mirante.net.br>
2015-09-24 10:39:02 -0300

    [MERGE] Revisão de código.

--- flow/test/src/test/java/br/net/mirante/singular/test/InstanciaDefinicaoComVariavelTest.java
+++ flow/test/src/test/java/br/net/mirante/singular/test/InstanciaDefinicaoComVariavelTest.java
@@ -1,10 +1,20 @@
 package br.net.mirante.singular.test;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.net.mirante.singular.definicao.InstanciaDefinicaoComVariavel;
+import br.net.mirante.singular.flow.core.MBPM;
+
+import static org.junit.Assert.assertNotNull;
 
 public class InstanciaDefinicaoComVariavelTest extends TestSupport {
+
+    @Before
+    public void setup() {
+        assertNotNull(mbpmBean);
+        MBPM.setConf(mbpmBean);
+    }
 
     @Test
     public void testarUsoDeVariaveis() {
