25d3b623162ec233e8824ff73efa6b12190088d4
Vinicius Uriel <viniciusuriel@gmail.com>
2015-09-28 15:42:49 -0300

    commitando classe esquecida

---
+++ flow/core/src/main/java/br/net/mirante/singular/flow/util/vars/DefaultVarInstance.java
@@ -0,0 +1,35 @@
+package br.net.mirante.singular.flow.util.vars;
+
+import br.net.mirante.singular.commons.base.SingularException;
+
+import java.util.Objects;
+
+public class DefaultVarInstance extends AbstractVarInstance {
+
+    private boolean historySaved = false;
+
+    private Object valor;
+
+    public DefaultVarInstance(VarDefinition definition) {
+        super(definition);
+    }
+
+    @Override
+    public VarInstance setValor(Object valor) {
+        try {
+            Object antes = this.valor;
+            this.valor = valor;
+            if (needToNotifyAboutValueChanged() && !Objects.equals(antes, this.valor)) {
+                notifyValueChanged();
+            }
+            return this;
+        } catch (RuntimeException e) {
+            throw new SingularException("Erro setando valor '" + valor + "' em " + getRef() + " (" + getNome() + ")", e);
+        }
+    }
+
+    @Override
+    public Object getValor() {
+        return valor;
+    }
+}
