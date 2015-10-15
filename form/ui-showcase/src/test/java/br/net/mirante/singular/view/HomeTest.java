3dff4687ef4c1cf47ca71ffcda97ddfe1c6167fa
Alfredo Fialho <alfredo.fialho@mirante.net.br>
2015-10-15 10:30:02 -0300

    [REVIEW] Revisão de código: jUnit.

--- form/ui-showcase/src/test/java/br/net/mirante/singular/view/HomeTest.java
+++ form/ui-showcase/src/test/java/br/net/mirante/singular/view/HomeTest.java
@@ -17,19 +17,23 @@ import br.net.mirante.singular.wicket.ShowcaseApplication;
 //BASED on https://ci.apache.org/projects/wicket/guide/6.x/guide/testingspring.html
 public class HomeTest {
 
-	private WicketTester driver;
-	
-	@Autowired private ApplicationContext ctx;
+    private WicketTester driver;
 
-    @Autowired private ShowcaseApplication app;
-	
-	@Before public void setup(){
+    @Autowired
+    private ApplicationContext ctx;
+
+    @Autowired
+    private ShowcaseApplication app;
+
+    @Before
+    public void setup() {
 //		driver = new WicketTester(new ShowcaseApplication());
-		driver = new WicketTester(app);
-	}
-	
-	@Test public void what(){
-		driver.startPage(FormPage.class);
-		driver.assertRenderedPage(FormPage.class);
-	}
+        driver = new WicketTester(app);
+    }
+
+    @Test
+    public void what() {
+        driver.startPage(FormPage.class);
+        driver.assertRenderedPage(FormPage.class);
+    }
 }
