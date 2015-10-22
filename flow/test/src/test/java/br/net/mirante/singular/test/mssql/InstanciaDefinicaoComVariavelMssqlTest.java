73ea8070ddb38dcab9aceb0865b63fe26a3cfa60
Alfredo Fialho <alfredo.fialho@mirante.net.br>
2015-10-22 16:22:05 -0200

    [REVIEW] Revisão de código: jUnit.

--- flow/test/src/test/java/br/net/mirante/singular/test/mssql/InstanciaDefinicaoComVariavelMssqlTest.java
+++ flow/test/src/test/java/br/net/mirante/singular/test/mssql/InstanciaDefinicaoComVariavelMssqlTest.java
@@ -1,15 +1,17 @@
 package br.net.mirante.singular.test.mssql;
 
-import br.net.mirante.singular.commons.base.SingularProperties;
-import br.net.mirante.singular.test.InstanciaDefinicaoComVariavelTest;
 import org.junit.BeforeClass;
 import org.springframework.test.context.ActiveProfiles;
+
+import br.net.mirante.singular.commons.base.SingularProperties;
+import br.net.mirante.singular.test.InstanciaDefinicaoComVariavelTest;
 
 @ActiveProfiles("mssql")
 public class InstanciaDefinicaoComVariavelMssqlTest extends InstanciaDefinicaoComVariavelTest {
 
     @BeforeClass
     public static void configProperites() {
-        SingularProperties.INSTANCE.loadFrom(ClassLoader.getSystemClassLoader().getResourceAsStream("singular-mssql.properties"));
+        SingularProperties.INSTANCE.loadFrom(ClassLoader.getSystemClassLoader()
+                .getResourceAsStream("singular-mssql.properties"));
     }
 }
