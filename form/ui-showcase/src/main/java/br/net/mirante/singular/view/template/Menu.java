9e7f02b73e1deb1fddbbdb0f538320f1a063aa8e
Alfredo Fialho <alfredo.fialho@mirante.net.br>
2015-10-28 15:24:50 -0200

    [REVIEW] Revisão de código: SONAR.

--- form/ui-showcase/src/main/java/br/net/mirante/singular/view/template/Menu.java
+++ form/ui-showcase/src/main/java/br/net/mirante/singular/view/template/Menu.java
@@ -2,11 +2,8 @@ package br.net.mirante.singular.view.template;
 
 import javax.inject.Inject;
 
-import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.panel.Fragment;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.markup.repeater.RepeatingView;
 
 import br.net.mirante.singular.util.wicket.util.WicketUtils;
 import br.net.mirante.singular.wicket.UIAdminWicketFilterContext;
@@ -23,11 +20,10 @@ public class Menu extends Panel {
     @Override
     protected void onInitialize() {
         super.onInitialize();
-        queue(new WebMarkupContainer("home").add(
-        	WicketUtils.$b.attr("href", uiAdminWicketFilterContext.getRelativeContext()))
-        );
-        queue(new WebMarkupContainer("crud").add(
-        	WicketUtils.$b.attr("href", uiAdminWicketFilterContext.getRelativeContext().concat("form/crud")))
-        );
+        queue(new WebMarkupContainer("home")
+                .add(WicketUtils.$b.attr("href", uiAdminWicketFilterContext.getRelativeContext())));
+        queue(new WebMarkupContainer("crud")
+                .add(WicketUtils.$b.attr("href", uiAdminWicketFilterContext.getRelativeContext()
+                        .concat("form/crud"))));
     }
 }
