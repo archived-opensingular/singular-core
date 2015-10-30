b1f73a363187ffc333e40f9eb8754f4e108bf76d
Delfino Filho <delfino.filho@mirante.net.br>
2015-10-30 18:03:37 -0200

    Atualizando cliente do singularws para receber a sigla do processo em todos os métodos.

--- lib/ws-client/src/main/java/br/net/mirante/singular/ws/client/ExecuteTransition.java
+++ lib/ws-client/src/main/java/br/net/mirante/singular/ws/client/ExecuteTransition.java
@@ -16,6 +16,7 @@ import javax.xml.bind.annotation.XmlType;
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
+ *         &lt;element name="processAbbreviation" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *         &lt;element name="codProcessInstance" type="{http://www.w3.org/2001/XMLSchema}long" minOccurs="0"/>
  *         &lt;element name="transitionName" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
  *       &lt;/sequence>
@@ -28,13 +29,39 @@ import javax.xml.bind.annotation.XmlType;
  */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "executeTransition", propOrder = {
+    "processAbbreviation",
     "codProcessInstance",
     "transitionName"
 })
 public class ExecuteTransition {
 
+    protected String processAbbreviation;
     protected Long codProcessInstance;
     protected String transitionName;
+
+    /**
+     * Obtém o valor da propriedade processAbbreviation.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getProcessAbbreviation() {
+        return processAbbreviation;
+    }
+
+    /**
+     * Define o valor da propriedade processAbbreviation.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setProcessAbbreviation(String value) {
+        this.processAbbreviation = value;
+    }
 
     /**
      * Obtém o valor da propriedade codProcessInstance.
