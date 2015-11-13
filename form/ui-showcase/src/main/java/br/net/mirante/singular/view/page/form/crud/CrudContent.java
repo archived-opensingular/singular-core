5c6bb54f7a6dff9317cbcc137b27b11fe8e07cfb
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2015-11-13 17:16:18 -0200

    Fix MIR-126 : Validating empty form

--- form/ui-showcase/src/main/java/br/net/mirante/singular/view/page/form/crud/CrudContent.java
+++ form/ui-showcase/src/main/java/br/net/mirante/singular/view/page/form/crud/CrudContent.java
@@ -185,7 +185,6 @@ public class CrudContent extends Content implements SingularWicketContainer<Crud
         inputModal.show(target);
     }
 
-    @SuppressWarnings("unchecked")
     private void createInstance(String nomeDoTipo) {
         MTipo<?> tipo = TemplateRepository.get().loadType(nomeDoTipo);
         currentInstance = new MInstanceRootModel<MInstancia>(tipo.novaInstancia());
@@ -232,24 +231,49 @@ public class CrudContent extends Content implements SingularWicketContainer<Crud
                 .add(new AjaxButton("save-btn") {
                     @Override
                     protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-                        StringWriter buffer = new StringWriter();
-                MInstancia trueInstance = currentInstance.getObject();
-
-                        InstanceValidationContext validationContext = new InstanceValidationContext(trueInstance);
-                        InstanceValidationUtils.associateErrorsToComponents(validationContext, form);
-
-                        if (validationContext.hasErrorsAboveLevel(ValidationErrorLevel.WARNING)) {
-                            target.add(form);
-                            return;
-                        }
-
-                        MformPersistenciaXML.toXML(trueInstance).printTabulado(
-                                new PrintWriter(buffer));
-                        currentModel.setXml(buffer.toString());
+                        MInstancia trueInstance = currentInstance.getObject();
+                        MElement rootXml = MformPersistenciaXML.toXML(trueInstance);
+                        
+			try {
+			     addValidationErrors(target, form, trueInstance, rootXml);
+			} catch (Exception e) {
+			    target.add(form);
+			    return;
+			}
+                        currentModel.setXml(printXml(rootXml));
                         dao.save(currentModel);
                         updateListTableFromModal(target);
                         inputModal.hide(target);
                     }
+
+		    private void addValidationErrors(AjaxRequestTarget target, Form<?> form,
+			    MInstancia trueInstance, MElement rootXml) throws Exception{
+			runDefaultValidators(form, trueInstance);
+                        validateEmptyForm(form, rootXml);
+		    }
+
+		    private void validateEmptyForm(Form<?> form, MElement rootXml) {
+                        if (rootXml == null){
+                            form.error(getMessage("form.message.empty").getString());
+                            throw new RuntimeException("Has empty form");
+                        }
+		    }
+
+		    private void runDefaultValidators(Form<?> form, MInstancia trueInstance) {
+			InstanceValidationContext validationContext = new InstanceValidationContext(trueInstance);
+                        InstanceValidationUtils.associateErrorsToComponents(validationContext, form);
+
+                        if (validationContext.hasErrorsAboveLevel(ValidationErrorLevel.WARNING)) {
+                            throw new RuntimeException("Has form errors");
+                        }
+		    }
+
+		    private String printXml(MElement rootXml) {
+			StringWriter buffer = new StringWriter();
+			rootXml.printTabulado(new PrintWriter(buffer));
+                        String xml = buffer.toString();
+			return xml;
+		    }
                 }));
 
         return inputModal;
