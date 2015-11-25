5a430e64624e85a738b94976188017d8debd4d97
Daniel Bordin <daniel@mirante.net.br>
2015-11-25 18:45:36 -0200

    [FORM-SHOWCASE] Melhorias menores no Cases

--- form/ui-showcase/src/main/java/br/net/mirante/singular/showcase/input/core/CaseInputCoreDatePackage.java
+++ form/ui-showcase/src/main/java/br/net/mirante/singular/showcase/input/core/CaseInputCoreDatePackage.java
@@ -3,13 +3,16 @@ package br.net.mirante.singular.showcase.input.core;
 import br.net.mirante.singular.form.mform.MPacote;
 import br.net.mirante.singular.form.mform.MTipoComposto;
 import br.net.mirante.singular.form.mform.PacoteBuilder;
+import br.net.mirante.singular.form.mform.basic.ui.AtrBasic;
 
 public class CaseInputCoreDatePackage extends MPacote {
 
+    //@formatter:off
     @Override
     protected void carregarDefinicoes(PacoteBuilder pb) {
         MTipoComposto<?> tipoMyForm = pb.createTipoComposto("testForm");
-        tipoMyForm.addCampoData("inicio");
+        tipoMyForm.addCampoData("inicio")
+            .as(AtrBasic.class).label("Data inicio");
     }
 
 }
