16533cf6670fd8286277c9d3f965ed80b179b598
danilo.mesquita <danilo.mesquita@mirante.net.br>
2015-12-07 16:08:24 -0200

    Ajustando codigo para apresentação e removendo botão de help do plugin de syntax

--- form/ui-showcase/src/main/java/br/net/mirante/singular/showcase/input/core/CaseInputCoreIntegerPackage.java
+++ form/ui-showcase/src/main/java/br/net/mirante/singular/showcase/input/core/CaseInputCoreIntegerPackage.java
@@ -3,12 +3,16 @@ package br.net.mirante.singular.showcase.input.core;
 import br.net.mirante.singular.form.mform.MPacote;
 import br.net.mirante.singular.form.mform.MTipoComposto;
 import br.net.mirante.singular.form.mform.PacoteBuilder;
+import br.net.mirante.singular.form.mform.basic.ui.AtrBasic;
+import br.net.mirante.singular.form.wicket.AtrWicket;
 
 public class CaseInputCoreIntegerPackage extends MPacote {
 
     @Override
     protected void carregarDefinicoes(PacoteBuilder pb) {
         MTipoComposto<?> tipoMyForm = pb.createTipoComposto("testForm");
-        tipoMyForm.addCampoInteger("qtd");
+        tipoMyForm.addCampoInteger("qtd")
+                  .as(AtrBasic.class).label("Quantidade")
+                  .as(AtrWicket::new).larguraPref(2);
     }
 }
