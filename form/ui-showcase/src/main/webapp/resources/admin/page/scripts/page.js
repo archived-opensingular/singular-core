d9d5eb6d44f40dcd183ad4a0f1fcdaee2dad8342
danilo.mesquita <danilo.mesquita@mirante.net.br>
2015-12-08 17:01:49 -0200

    Ajustando chamadas de inicialização a componentes javascript

--- form/ui-showcase/src/main/webapp/resources/admin/page/scripts/page.js
+++ form/ui-showcase/src/main/webapp/resources/admin/page/scripts/page.js
@@ -1,32 +1,37 @@
-var handleDatePickers = function() {
-    if (jQuery().datepicker) {
-        $('.date-picker').datepicker({
-            rtl: Metronic.isRTL(),
-            orientation: "right",
-            autoclose: true,
-            language: 'pt-BR'
+var Page = function() {
+
+    function handleSlimScroll(){
+        $('.scroller').slimScroll({});
+    }
+
+    function handleDatePickers() {
+        if (jQuery().datepicker) {
+            $('.date-picker').datepicker({
+                rtl: Metronic.isRTL(),
+                orientation: "right",
+                autoclose: true,
+                language: 'pt-BR'
+            });
+        }
+    }
+
+    function handleBootstrapSelect() {
+        $('.bs-select').selectpicker({
+            iconBase: 'fa',
+            tickIcon: 'fa-check'
         });
     }
-};
 
-var handleBootstrapSelect = function() {
-    $('.bs-select').selectpicker({
-        iconBase: 'fa',
-        tickIcon: 'fa-check'
-    });
-};
-
-var handleMultiSelect = function() {
-    $('.multi-select').multiSelect();
-};
-
-var Page = function() {
+    function handleMultiSelect() {
+        $('.multi-select').multiSelect();
+    }
 
     return {
         init: function() {
             handleDatePickers();
             handleBootstrapSelect();
             handleMultiSelect();
+            handleSlimScroll();
         }
     };
 
