824ee5e05b6a027aa81105a50e780a44410d7d94
danilo.mesquita <danilo.mesquita@mirante.net.br>
2015-12-16 15:44:34 -0200

    criando processador de codigo fonte, que remove linhas considerada "lixo" e reconhece linhas que devem ser destacadas

--- form/ui-showcase/src/main/java/br/net/mirante/singular/view/page/showcase/ItemCodePanel.java
+++ form/ui-showcase/src/main/java/br/net/mirante/singular/view/page/showcase/ItemCodePanel.java
@@ -1,10 +1,14 @@
 package br.net.mirante.singular.view.page.showcase;
 
+import br.net.mirante.singular.form.wicket.util.ProcessadorCondigoFonte;
+import br.net.mirante.singular.util.wicket.util.WicketUtils;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
+
+import java.util.List;
 
 public class ItemCodePanel extends Panel {
 
@@ -16,7 +20,26 @@ public class ItemCodePanel extends Panel {
 
     public ItemCodePanel(String id, IModel<String> code) {
         super(id);
-        add(new Label("code", code));
+        final ProcessadorCondigoFonte pcf = new ProcessadorCondigoFonte(code.getObject());
+        add(new Label("code", pcf.getFonteProcessado()).add(WicketUtils.$b.classAppender(getSyntaxHightliterConfig(pcf.getLinhasParaDestacar()))));
+    }
+
+    private String getSyntaxHightliterConfig(List<Integer> linhasParaDestacar) {
+        StringBuilder config = new StringBuilder();
+        config.append("brush: java;");
+
+        if (!linhasParaDestacar.isEmpty()) {
+            config.append(" highlight: [");
+            linhasParaDestacar.forEach(l -> {
+                config.append(l);
+                if (linhasParaDestacar.indexOf(l) != linhasParaDestacar.size() - 1) {
+                    config.append(", ");
+                }
+            });
+            config.append("]; ");
+        }
+
+        return config.toString();
     }
 
     @Override
