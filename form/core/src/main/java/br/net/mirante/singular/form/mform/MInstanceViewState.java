da874cd9cb7aa581472f41368bef0d7d5ef62221
danilo.mesquita <danilo.mesquita@mirante.net.br>
2015-12-28 15:20:47 -0200

    corrigindo bugs apontados pelo sonar

--- form/core/src/main/java/br/net/mirante/singular/form/mform/MInstanceViewState.java
+++ form/core/src/main/java/br/net/mirante/singular/form/mform/MInstanceViewState.java
@@ -1,9 +1,8 @@
 package br.net.mirante.singular.form.mform;
 
-import java.util.function.Function;
-
 import br.net.mirante.singular.form.mform.basic.ui.MPacoteBasic;
 import br.net.mirante.singular.form.mform.core.MPacoteCore;
+import java.util.function.Function;
 
 public enum MInstanceViewState {
     EDITABLE, READONLY, HIDDEN;
@@ -38,23 +37,21 @@ public enum MInstanceViewState {
             return MInstanceViewState.HIDDEN;
 
         final Function<Boolean, Boolean> intern = b -> (b == null) ? null : b.booleanValue();
-        final Boolean Y = Boolean.TRUE;
-        final Boolean N = Boolean.FALSE;
 
         final boolean exists = instance.exists();
         final Boolean visible = intern.apply(MInstances.attributeValue(instance, MPacoteBasic.ATR_VISIVEL, null));
         final Boolean enabled = intern.apply(MInstances.attributeValue(instance, MPacoteBasic.ATR_ENABLED, null));
 
         if (exists) {
-            if (visible == N) {
+            if (!visible) {
                 return MInstanceViewState.HIDDEN;
-            } else if (enabled == N) {
+            } else if (!enabled) {
                 return MInstanceViewState.READONLY;
             } else {
                 return MInstanceViewState.EDITABLE;
             }
         } else {
-            if (visible == Y) {
+            if (visible) {
                 return MInstanceViewState.READONLY;
             } else {
                 return MInstanceViewState.HIDDEN;
