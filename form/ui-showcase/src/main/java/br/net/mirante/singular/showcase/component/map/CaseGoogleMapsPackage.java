e8bdfaf93bd37443102118fda5fb75b7cf6ddcf4
danilo.mesquita <danilo.mesquita@mirante.net.br>
2016-01-06 11:08:06 -0200

    melhorando exemplo do showcase

--- form/ui-showcase/src/main/java/br/net/mirante/singular/showcase/component/map/CaseGoogleMapsPackage.java
+++ form/ui-showcase/src/main/java/br/net/mirante/singular/showcase/component/map/CaseGoogleMapsPackage.java
@@ -4,18 +4,18 @@ import br.net.mirante.singular.form.mform.MIComposto;
 import br.net.mirante.singular.form.mform.MPacote;
 import br.net.mirante.singular.form.mform.MTipoComposto;
 import br.net.mirante.singular.form.mform.PacoteBuilder;
+import br.net.mirante.singular.form.mform.core.AtrCore;
 import br.net.mirante.singular.form.mform.core.MTipoLatitudeLongitude;
 
-/**
- * Created by danilo.mesquita on 04/01/2016.
- */
 public class CaseGoogleMapsPackage extends MPacote {
 
     @Override
     protected void carregarDefinicoes(PacoteBuilder pb) {
         super.carregarDefinicoes(pb);
 
-        MTipoComposto<? extends MIComposto> form = pb.createTipoComposto("testForm");
-        form.addCampo("coordenada", MTipoLatitudeLongitude.class);
+        final MTipoComposto<? extends MIComposto> form = pb.createTipoComposto("testForm");
+        final MTipoLatitudeLongitude campoCoordenada = form.addCampo("coordenada", MTipoLatitudeLongitude.class);
+
+        campoCoordenada.as(AtrCore::new).obrigatorio(true);
     }
 }
