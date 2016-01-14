3749fee21d09e6152f444e1ea472796fb00ea419
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2016-01-14 12:07:40 -0200

    Now we have a non functional mapper

---
+++ form/core/src/main/java/br/net/mirante/singular/form/mform/basic/view/MAnnotationView.java
@@ -0,0 +1,7 @@
+package br.net.mirante.singular.form.mform.basic.view;
+
+/**
+ * Created by nuk on 14/01/16.
+ */
+public class MAnnotationView extends MView {
+}
---
+++ form/wicket/src/main/java/br/net/mirante/singular/form/wicket/mapper/annotation/AnnotationDefaultMapper.java
@@ -0,0 +1,9 @@
+package br.net.mirante.singular.form.wicket.mapper.annotation;
+
+import br.net.mirante.singular.form.wicket.mapper.DefaultCompostoMapper;
+
+/**
+ * Created by nuk on 14/01/16.
+ */
+public class AnnotationDefaultMapper extends DefaultCompostoMapper {
+}
