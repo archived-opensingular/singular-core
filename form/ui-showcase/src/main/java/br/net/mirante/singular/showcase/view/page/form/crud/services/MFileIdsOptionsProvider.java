c3a320f8aae189a4b743e7648fb999d0e8e6b9f8
Daniel Bordin <daniel@mirante.net.br>
2016-01-19 14:19:26 -0200

    [FORM-SHOWCASE] Melhoria no exemplo do provider externo de opções para seleção

--- form/ui-showcase/src/main/java/br/net/mirante/singular/showcase/view/page/form/crud/services/MFileIdsOptionsProvider.java
+++ form/ui-showcase/src/main/java/br/net/mirante/singular/showcase/view/page/form/crud/services/MFileIdsOptionsProvider.java
@@ -1,52 +1,34 @@
 package br.net.mirante.singular.showcase.view.page.form.crud.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Component;
 
-import br.net.mirante.singular.showcase.dao.form.ExampleFile;
-import br.net.mirante.singular.showcase.dao.form.FileDao;
-import br.net.mirante.singular.showcase.dao.form.TemplateRepository;
-import br.net.mirante.singular.form.mform.MDicionario;
 import br.net.mirante.singular.form.mform.MILista;
 import br.net.mirante.singular.form.mform.MInstancia;
-import br.net.mirante.singular.form.mform.MTipo;
-import br.net.mirante.singular.form.mform.core.MTipoString;
 import br.net.mirante.singular.form.mform.options.MOptionsProvider;
+import br.net.mirante.singular.showcase.dao.form.ExampleFile;
+import br.net.mirante.singular.showcase.dao.form.FileDao;
 
 @SuppressWarnings("serial")
 @Component("filesChoiceProvider")
 public class MFileIdsOptionsProvider implements MOptionsProvider {
     @Inject private FileDao filePersistence;
 
-    @Override
-    public String toDebug() {
-        return null;
-    }
-
     /**
      * Returns a MILista of the type of the field it will be used.
      * @param optionsInstance : Current instance of the selection.
      */
     @Override
+    // @destacar:bloco
     public MILista<? extends MInstancia> listOptions(MInstancia optionsInstance) {
-        MILista<?> list = newMIStringList(optionsInstance);
-        files().forEach((f) -> list.addValor(f.getId()));
+        MILista<?> list = optionsInstance.getMTipo().novaLista();
+        files().forEach(f -> list.addValor(f.getId()));
         return list;
     }
-
-    private MILista<?> newMIStringList(MInstancia optionsInstance) {
-        MTipoString tipoString = dictionary(optionsInstance).getTipo(MTipoString.class);
-        return tipoString.novaLista();
-    }
-
-    private MDicionario dictionary(MInstancia optionsInstance) {
-        MTipo<?> type = optionsInstance.getMTipo();
-        return type.getDicionario();
-    }
+    // @destacar:fim
 
     private List<ExampleFile> files() {
         return filePersistence.list();
