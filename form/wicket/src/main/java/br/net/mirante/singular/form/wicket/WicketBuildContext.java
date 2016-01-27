8d2f47beaa0430f13ffdeea0290c65fba9f3e7aa
danilo.mesquita <danilo.mesquita@mirante.net.br>
2016-01-27 08:39:06 -0200

    [Form][BugFix] Controle de Visibilidade via lambda não funciona corretamente no tabview

--- form/wicket/src/main/java/br/net/mirante/singular/form/wicket/WicketBuildContext.java
+++ form/wicket/src/main/java/br/net/mirante/singular/form/wicket/WicketBuildContext.java
@@ -68,8 +68,7 @@ public class WicketBuildContext implements Serializable {
         container.add(ConfigureByMInstanciaAttributesBehavior.getInstance());
     }
 
-    public WicketBuildContext init(UIBuilderWicket uiBuilderWicket,
-                                   ViewMode viewMode) {
+    public WicketBuildContext init(UIBuilderWicket uiBuilderWicket, ViewMode viewMode) {
 
         final MInstancia instance = getCurrenttInstance();
 
@@ -78,7 +77,7 @@ public class WicketBuildContext implements Serializable {
         this.viewMode = viewMode;
 
         if (isRootContext()) {
-            getContainer().add(new InitRootContainerBehavior(getModel()));
+            initContainerBehavior();
         }
 
         if (getContainer().getDefaultModel() == null) {
@@ -99,6 +98,20 @@ public class WicketBuildContext implements Serializable {
         }
 
         return this;
+    }
+
+    /**
+     * Adiciona um behavior que executa o update atributes do SDocument em toda requisição.
+     *
+     * Normalmente este método não deve ser chamado externamente,
+     * porem pode existir situações em que o container root não é atualizado
+     * e novos componentes filhos são adicionados.
+     *
+     * @see br.net.mirante.singular.form.mform.document.SDocument
+     * @see br.net.mirante.singular.form.wicket.mapper.TabMapper
+     */
+    public void initContainerBehavior(){
+        getContainer().add(new InitRootContainerBehavior(getModel()));
     }
 
     /**
