e465a87f234dfa42a3222728fc23239ad140194b
Delfino Filho <delfino.filho@mirante.net.br>
2016-02-01 11:09:24 -0200

    Implementando conteúdo da tela de rascunho

--- singular-pet-server/src/main/java/br/net/mirante/singular/pet/server/wicket/view/racunho/RascunhoContent.java
+++ singular-pet-server/src/main/java/br/net/mirante/singular/pet/server/wicket/view/racunho/RascunhoContent.java
@@ -1,9 +1,16 @@
 package br.net.mirante.singular.pet.server.wicket.view.racunho;
 
-import br.net.mirante.singular.pet.module.wicket.view.template.Content;
+import static br.net.mirante.singular.util.wicket.util.WicketUtils.$m;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.IModel;
 
-import static br.net.mirante.singular.util.wicket.util.WicketUtils.$m;
+import br.net.mirante.singular.pet.module.wicket.view.template.Content;
 
 public class RascunhoContent extends Content {
 
@@ -11,17 +18,57 @@ public class RascunhoContent extends Content {
         super(id);
     }
 
+    private String filtro;
+
     public RascunhoContent(String id, boolean withInfoLink, boolean withBreadcrumb) {
         super(id, withInfoLink, withBreadcrumb);
     }
 
     @Override
+    protected void onInitialize() {
+        super.onInitialize();
+
+        Form form = new Form("form") {
+            @Override
+            protected void onSubmit() {
+                super.onSubmit();
+            }
+        };
+
+        add(form);
+
+        queue(new Label("filtroRapidoLabel", getString("label.filtro.rapido")));
+        queue(new TextField<String>("filtroRapido", $m.property(this, "filtro")));
+        queue(new Button("pesquisar", $m.ofValue(getString("label.pesquisar"))));
+
+        queue(new ListView<Object>("tabela") {
+
+            @Override
+            protected void populateItem(ListItem<Object> item) {
+
+                item.add(new Label("numero"));
+                item.add(new Label("descricao"));
+                item.add(new Label("processo"));
+                item.add(new Label("dataCriacao"));
+            }
+        });
+    }
+
+    @Override
     protected IModel<?> getContentTitlelModel() {
-        return $m.ofValue("Página inicial");
+        return $m.ofValue("Rascunho");
     }
 
     @Override
     protected IModel<?> getContentSubtitlelModel() {
-        return $m.ofValue("Página inicial");
+        return $m.ofValue("Petições de rascunho");
+    }
+
+    public String getFiltro() {
+        return filtro;
+    }
+
+    public void setFiltro(String filtro) {
+        this.filtro = filtro;
     }
 }
