861ccd4e7c8faa505d267ee82d712ccc4a0ff8f1
danilo.mesquita <danilo.mesquita@mirante.net.br>
2016-02-03 09:56:20 -0200

    [Form] [Showcase] Corrigindo exemplo de validação parcial.

--- form/ui-showcase/src/main/java/br/net/mirante/singular/showcase/component/validation/CaseValidationPartial.java
+++ form/ui-showcase/src/main/java/br/net/mirante/singular/showcase/component/validation/CaseValidationPartial.java
@@ -1,11 +1,11 @@
 package br.net.mirante.singular.showcase.component.validation;
 
-import br.net.mirante.singular.showcase.component.CaseBase;
-import br.net.mirante.singular.showcase.component.ResourceRef;
-import org.apache.wicket.ajax.markup.html.form.AjaxButton;
-
 import java.util.Optional;
 
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+
+import br.net.mirante.singular.showcase.component.CaseBase;
+import br.net.mirante.singular.showcase.component.ResourceRef;
 import static br.net.mirante.singular.showcase.component.ResourceRef.forSource;
 import static br.net.mirante.singular.util.wicket.util.WicketUtils.$b;
 
@@ -13,6 +13,7 @@ public class CaseValidationPartial extends CaseBase {
 
     public CaseValidationPartial() {
         super("Partial");
+        setDescriptionHtml("É possível validar somente uma parte do formulário, no exemplo a seguir somente o campo \"Obrigatório 1\" será validado ao acionar a validação parcial." );
         getBotoes().add((id, currentInstance) -> {
             final AjaxButton aj = new PartialValidationButton(id, currentInstance);
 
