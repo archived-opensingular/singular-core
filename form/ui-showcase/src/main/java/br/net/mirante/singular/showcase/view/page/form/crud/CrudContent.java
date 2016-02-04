72a737ab134e6a6d837057242b63811f7c8e4136
danilo.mesquita <danilo.mesquita@mirante.net.br>
2016-02-04 13:06:36 -0200

    Abrindo versão 0.5.8

--- form/ui-showcase/src/main/java/br/net/mirante/singular/showcase/view/page/form/crud/CrudContent.java
+++ form/ui-showcase/src/main/java/br/net/mirante/singular/showcase/view/page/form/crud/CrudContent.java
@@ -63,251 +63,251 @@ import br.net.mirante.singular.util.wicket.tab.BSTabPanel;
 @SuppressWarnings("serial")
 public class CrudContent extends Content implements SingularWicketContainer<CrudContent, Void> {
 
-	private final static Logger LOGGER = LoggerFactory.getLogger(CrudContent.class);
+    private final static Logger LOGGER = LoggerFactory.getLogger(CrudContent.class);
 
-	private BSDataTable<ExampleDataDTO, String> listTable;
-	private List<ExampleDataDTO> dataList = new LinkedList<>();
-	private FormVO selectedTemplate;
+    private BSDataTable<ExampleDataDTO, String> listTable;
+    private List<ExampleDataDTO> dataList = new LinkedList<>();
+    private FormVO selectedTemplate;
 
-	private final BFModalBorder deleteModal = new BFModalBorder("deleteModal");
-	private final BFModalBorder viewXmlModal = new BFModalBorder("viewXmlModal");
+    private final BFModalBorder deleteModal = new BFModalBorder("deleteModal");
+    private final BFModalBorder viewXmlModal = new BFModalBorder("viewXmlModal");
 
-	@Inject
-	ExampleDataDAO dao;
+    @Inject
+    ExampleDataDAO dao;
 
-	@Inject
-	FileDao filePersistence;
+    @Inject
+    FileDao filePersistence;
 
-	private ExampleDataDTO currentModel;
+    private ExampleDataDTO currentModel;
 
-	public CrudContent(String id, StringValue pType) {
-		super(id, false, true);
-		setActiveTemplate(pType);
-	}
+    public CrudContent(String id, StringValue pType) {
+        super(id, false, true);
+        setActiveTemplate(pType);
+    }
 
-	private void setActiveTemplate(StringValue pType) {
-		if (!pType.isEmpty()) {
-			String strType = pType.toString();
-			TemplateEntry t = TemplateRepository.get().findEntryByType(strType);
-			selectedTemplate = new FormVO(t);
-		} else {
-			selectedTemplate = new FormVO(null, null);
-		}
-	}
+    private void setActiveTemplate(StringValue pType) {
+        if (!pType.isEmpty()) {
+            String strType = pType.toString();
+            TemplateEntry t = TemplateRepository.get().findEntryByType(strType);
+            selectedTemplate = new FormVO(t);
+        } else {
+            selectedTemplate = new FormVO(null, null);
+        }
+    }
 
-	@Override
-	protected void onInitialize() {
+    @Override
+    protected void onInitialize() {
 
-		super.onInitialize();
+        super.onInitialize();
 
-		add(new Form<>("optionsForm").add(setUpTemplatesOptions()));
-		add(new Form<>("delete-form").add(deleteModal));
-		add(setUpInsertButton());
-		add(listTable = setupDataTable());
-		add(viewXmlModal);
-		add(new BFFeedbackPanel("feedback", this));
+        add(new Form<>("optionsForm").add(setUpTemplatesOptions()));
+        add(new Form<>("delete-form").add(deleteModal));
+        add(setUpInsertButton());
+        add(listTable = setupDataTable());
+        add(viewXmlModal);
+        add(new BFFeedbackPanel("feedback", this));
 
-		deleteModal.setTitleText(Model.of(getString("label.delete.message")));
+        deleteModal.setTitleText(Model.of(getString("label.delete.message")));
 
-		deleteModal.addButton(ButtonStyle.PRIMARY, Model.of(getString("label.button.ok")),
-				new AjaxButton("delete-btn") {
-					@Override
-					protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-						dao.remove(currentModel);
-						currentModel = null;
-						updateListTableFromModal(target);
-						deleteModal.hide(target);
-					}
-				});
+        deleteModal.addButton(ButtonStyle.PRIMARY, Model.of(getString("label.button.ok")),
+                new AjaxButton("delete-btn") {
+                    @Override
+                    protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                        dao.remove(currentModel);
+                        currentModel = null;
+                        updateListTableFromModal(target);
+                        deleteModal.hide(target);
+                    }
+                });
 
-		deleteModal.addButton(ButtonStyle.DEFAULT, Model.of(getString("label.button.cancel")),
-				new AjaxButton("cancel-btn") {
-					@Override
-					protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-						deleteModal.hide(target);
-					}
-				});
-		
-		deleteModal.setSize(Size.SMALL);
+        deleteModal.addButton(ButtonStyle.DEFAULT, Model.of(getString("label.button.cancel")),
+                new AjaxButton("cancel-btn") {
+                    @Override
+                    protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                        deleteModal.hide(target);
+                    }
+                });
 
-	}
+        deleteModal.setSize(Size.SMALL);
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private DropDownChoice setUpTemplatesOptions() {
-		List<SelectOption> options = TemplateRepository.get().getEntries().stream()
-				.map(t -> new SelectOption(t.getDisplayName(), new FormVO(t))).collect(Collectors.toList());
+    }
 
-		ChoiceRenderer choiceRenderer = new ChoiceRenderer("selectLabel", "value");
-		return new DropDownChoice<SelectOption>("options",
-				new SelectOption(selectedTemplate.getKey(), selectedTemplate), options, choiceRenderer) {
-			@Override
-			protected boolean wantOnSelectionChangedNotifications() {
-				return true;
-			}
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private DropDownChoice setUpTemplatesOptions() {
+        List<SelectOption> options = TemplateRepository.get().getEntries().stream()
+                .map(t -> new SelectOption(t.getDisplayName(), new FormVO(t))).collect(Collectors.toList());
 
-			@Override
-			protected void onSelectionChanged(SelectOption newSelection) {
-				FormVO value = (FormVO) newSelection.getValue();
-				selectedTemplate = value;
-				updateDataList();
-			}
+        ChoiceRenderer choiceRenderer = new ChoiceRenderer("selectLabel", "value");
+        return new DropDownChoice<SelectOption>("options",
+                new SelectOption(selectedTemplate.getKey(), selectedTemplate), options, choiceRenderer) {
+            @Override
+            protected boolean wantOnSelectionChangedNotifications() {
+                return true;
+            }
 
-		};
-	}
+            @Override
+            protected void onSelectionChanged(SelectOption newSelection) {
+                FormVO value = (FormVO) newSelection.getValue();
+                selectedTemplate = value;
+                updateDataList();
+            }
 
-	private void updateDataList() {
-		dataList = dao.list(selectedTemplate.getTypeName());
-	}
+        };
+    }
 
-	private MarkupContainer setUpInsertButton() {
-		return new Form<>("form").add(new AjaxButton("insert") {
-			@Override
-			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
-				PageParameters params = new PageParameters().add(FormPage.TYPE_NAME, selectedTemplate.getTypeName());
-				setResponsePage(FormPage.class, params);
-			}
+    private void updateDataList() {
+        dataList = dao.list(selectedTemplate.getTypeName());
+    }
 
-			@Override
-			public boolean isVisible() {
-				return selectedTemplate != null && selectedTemplate.getKey() != null;
-			}
+    private MarkupContainer setUpInsertButton() {
+        return new Form<>("form").add(new AjaxButton("insert") {
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                PageParameters params = new PageParameters().add(FormPage.TYPE_NAME, selectedTemplate.getTypeName());
+                setResponsePage(FormPage.class, params);
+            }
 
-		});
-	}
+            @Override
+            public boolean isVisible() {
+                return selectedTemplate != null && selectedTemplate.getKey() != null;
+            }
 
-	private BSDataTable<ExampleDataDTO, String> setupDataTable() {
-		updateDataList();
-		BSDataTableBuilder<ExampleDataDTO, String, IColumn<ExampleDataDTO, String>> builder = new BSDataTableBuilder<>(
-				createDataProvider());
-		builder.appendPropertyColumn(getMessage("label.table.column.key"), "key", ExampleDataDTO::getKey)
-				.appendColumn(new BSActionColumn<ExampleDataDTO, String>($m.ofValue(""))
-						.appendAction(getMessage("label.table.column.edit"), Icone.PENCIL_SQUARE, (target, model) -> {
-							setResponsePage(FormPage.class,
-									new PageParameters().add(FormPage.TYPE_NAME, selectedTemplate.getTypeName())
-											.add(FormPage.MODEL_KEY, model.getObject().getKey())
-											.add(FormPage.VIEW_MODE, ViewMode.EDITION));
-						}))
-				.appendColumn(new BSActionColumn<ExampleDataDTO, String>($m.ofValue(""))
-						.appendAction(getMessage("label.table.column.visualizar"), Icone.EYE, (target, model) -> {
-							setResponsePage(FormPage.class,
-									new PageParameters().add(FormPage.TYPE_NAME, selectedTemplate.getTypeName())
-											.add(FormPage.MODEL_KEY, model.getObject().getKey())
-											.add(FormPage.VIEW_MODE, ViewMode.VISUALIZATION));
-						}));
-		addAnnotationColumnIfNeeded(builder);
-		builder.appendColumn(new BSActionColumn<ExampleDataDTO, String>($m.ofValue(""))
-				.appendAction(getMessage("label.table.column.delete"), Icone.MINUS, this::deleteSelected))
-				.appendColumn(new BSActionColumn<ExampleDataDTO, String>($m.ofValue(""))
-						.appendAction(getMessage("label.table.column.visualizar.xml"), Icone.EYE, this::viewXml))
-				.setRowsPerPage(Long.MAX_VALUE); // TODO: proper pagination
-		return builder.build("data-list");
-	}
+        });
+    }
 
-	private void addAnnotationColumnIfNeeded(
-			BSDataTableBuilder<ExampleDataDTO, String, IColumn<ExampleDataDTO, String>> builder) {
-		builder.appendColumn(new BSActionColumn<ExampleDataDTO, String>($m.ofValue("")) {
-			@Override
-			public String getCssClass() {
-				return hasAnnotations() ? "" : "hidden";
-			}
-		}.appendAction(getMessage("label.table.column.analisar"), Icone.COMMENT, (target, model) -> {
-			setResponsePage(FormPage.class,
-					new PageParameters().add(FormPage.TYPE_NAME, selectedTemplate.getTypeName())
-							.add(FormPage.MODEL_KEY, model.getObject().getKey())
-							.add(FormPage.VIEW_MODE, ViewMode.VISUALIZATION).add(FormPage.ANNOTATION_ENABLED, true));
-		}));
-	}
+    private BSDataTable<ExampleDataDTO, String> setupDataTable() {
+        updateDataList();
+        BSDataTableBuilder<ExampleDataDTO, String, IColumn<ExampleDataDTO, String>> builder = new BSDataTableBuilder<>(
+                createDataProvider());
+        builder.appendPropertyColumn(getMessage("label.table.column.key"), "key", ExampleDataDTO::getKey)
+                .appendColumn(new BSActionColumn<ExampleDataDTO, String>($m.ofValue(""))
+                        .appendAction(getMessage("label.table.column.edit"), Icone.PENCIL_SQUARE, (target, model) -> {
+                            setResponsePage(FormPage.class,
+                                    new PageParameters().add(FormPage.TYPE_NAME, selectedTemplate.getTypeName())
+                                            .add(FormPage.MODEL_KEY, model.getObject().getKey())
+                                            .add(FormPage.VIEW_MODE, ViewMode.EDITION));
+                        }))
+                .appendColumn(new BSActionColumn<ExampleDataDTO, String>($m.ofValue(""))
+                        .appendAction(getMessage("label.table.column.visualizar"), Icone.EYE, (target, model) -> {
+                            setResponsePage(FormPage.class,
+                                    new PageParameters().add(FormPage.TYPE_NAME, selectedTemplate.getTypeName())
+                                            .add(FormPage.MODEL_KEY, model.getObject().getKey())
+                                            .add(FormPage.VIEW_MODE, ViewMode.VISUALIZATION));
+                        }));
+        addAnnotationColumnIfNeeded(builder);
+        builder.appendColumn(new BSActionColumn<ExampleDataDTO, String>($m.ofValue(""))
+                .appendAction(getMessage("label.table.column.delete"), Icone.MINUS, this::deleteSelected))
+                .appendColumn(new BSActionColumn<ExampleDataDTO, String>($m.ofValue(""))
+                        .appendAction(getMessage("label.table.column.visualizar.xml"), Icone.EYE, this::viewXml))
+                .setRowsPerPage(Long.MAX_VALUE); // TODO: proper pagination
+        return builder.build("data-list");
+    }
 
-	private boolean hasAnnotations() {
-		boolean hasAnntations = false;
-		if (selectedTemplate.getType() != null && selectedTemplate.getType() instanceof STypeComposite) {
-			STypeComposite<?> type = (STypeComposite<?>) selectedTemplate.getType();
-			for (SType<?> i : (Collection<SType<?>>) type.getFields()) {
-				hasAnntations |= i.as(AtrAnnotation::new).isAnnotated();
-			}
-		}
-		return hasAnntations;
-	}
+    private void addAnnotationColumnIfNeeded(
+            BSDataTableBuilder<ExampleDataDTO, String, IColumn<ExampleDataDTO, String>> builder) {
+        builder.appendColumn(new BSActionColumn<ExampleDataDTO, String>($m.ofValue("")) {
+            @Override
+            public String getCssClass() {
+                return hasAnnotations() ? "" : "hidden";
+            }
+        }.appendAction(getMessage("label.table.column.analisar"), Icone.COMMENT, (target, model) -> {
+            setResponsePage(FormPage.class,
+                    new PageParameters().add(FormPage.TYPE_NAME, selectedTemplate.getTypeName())
+                            .add(FormPage.MODEL_KEY, model.getObject().getKey())
+                            .add(FormPage.VIEW_MODE, ViewMode.VISUALIZATION).add(FormPage.ANNOTATION_ENABLED, true));
+        }));
+    }
 
-	private BaseDataProvider<ExampleDataDTO, String> createDataProvider() {
-		return new BaseDataProvider<ExampleDataDTO, String>() {
+    private boolean hasAnnotations() {
+        boolean hasAnntations = false;
+        if (selectedTemplate.getType() != null && selectedTemplate.getType() instanceof STypeComposite) {
+            STypeComposite<?> type = (STypeComposite<?>) selectedTemplate.getType();
+            for (SType<?> i : (Collection<SType<?>>) type.getFields()) {
+                hasAnntations |= i.as(AtrAnnotation::new).isAnnotated();
+            }
+        }
+        return hasAnntations;
+    }
 
-			@Override
-			public long size() {
-				return dataList.size();
-			}
+    private BaseDataProvider<ExampleDataDTO, String> createDataProvider() {
+        return new BaseDataProvider<ExampleDataDTO, String>() {
 
-			@Override
-			public Iterator<? extends ExampleDataDTO> iterator(int first, int count, String sortProperty,
-					boolean ascending) {
-				return dataList.iterator();
-			}
-		};
-	}
+            @Override
+            public long size() {
+                return dataList.size();
+            }
 
-	private void deleteSelected(AjaxRequestTarget target, IModel<ExampleDataDTO> model) {
-		currentModel = model.getObject();
-		deleteModal.show(target);
-	}
+            @Override
+            public Iterator<? extends ExampleDataDTO> iterator(int first, int count, String sortProperty,
+                    boolean ascending) {
+                return dataList.iterator();
+            }
+        };
+    }
 
-	private void viewXml(AjaxRequestTarget target, IModel<ExampleDataDTO> model) {
+    private void deleteSelected(AjaxRequestTarget target, IModel<ExampleDataDTO> model) {
+        currentModel = model.getObject();
+        deleteModal.show(target);
+    }
 
-		final String xmlPersistencia = model.getObject().getXml();
-		final String xmlTabulado = getXmlTabulado(xmlPersistencia);
-		final String definicao = getDefinicao(model.getObject().getType());
+    private void viewXml(AjaxRequestTarget target, IModel<ExampleDataDTO> model) {
 
-		final BSTabPanel xmlTabs = new BSTabPanel("xmlTabs");
-		xmlTabs.addTab(getString("label.xml.tabulado"),
-				new BOutputPanel(BSTabPanel.getTabPanelId(), $m.ofValue(xmlTabulado)));
-		xmlTabs.addTab(getString("label.xml.persistencia"),
-				new BOutputPanel(BSTabPanel.getTabPanelId(), $m.ofValue(xmlPersistencia)));
-		xmlTabs.addTab(getString("label.definicao"),
-				new BOutputPanel(BSTabPanel.getTabPanelId(), $m.ofValue(definicao)));
+        final String xmlPersistencia = model.getObject().getXml();
+        final String xmlTabulado = getXmlTabulado(xmlPersistencia);
+        final String definicao = getDefinicao(model.getObject().getType());
 
-		viewXmlModal.addOrReplace(xmlTabs);
-		viewXmlModal.show(target);
-		viewXmlModal.setSize(BFModalBorder.Size.LARGE);
-	}
+        final BSTabPanel xmlTabs = new BSTabPanel("xmlTabs");
+        xmlTabs.addTab(getString("label.xml.tabulado"),
+                new BOutputPanel(BSTabPanel.getTabPanelId(), $m.ofValue(xmlTabulado)));
+        xmlTabs.addTab(getString("label.xml.persistencia"),
+                new BOutputPanel(BSTabPanel.getTabPanelId(), $m.ofValue(xmlPersistencia)));
+        xmlTabs.addTab(getString("label.definicao"),
+                new BOutputPanel(BSTabPanel.getTabPanelId(), $m.ofValue(definicao)));
 
-	private String getXmlTabulado(String xmlString) {
-		if (!xmlString.isEmpty()) {
-			try {
-				final MElement xml = MParser.parse(xmlString);
-				final StringWriter sw = new StringWriter();
-				final PrintWriter writer = new PrintWriter(sw);
-				xml.printTabulado(writer);
-				return sw.toString();
-			} catch (SAXException | IOException e) {
-				LOGGER.error(e.getMessage(), e);
-			}
-		}
-		return StringUtils.EMPTY;
-	}
+        viewXmlModal.addOrReplace(xmlTabs);
+        viewXmlModal.show(target);
+        viewXmlModal.setSize(BFModalBorder.Size.LARGE);
+    }
 
-	private String getDefinicao(String typeName) {
-		final SPackage pacote = TemplateRepository.get().loadType(typeName).getPacote();
-		StringBuilder definicaoOutput = new StringBuilder();
-		pacote.debug(definicaoOutput);
-		return definicaoOutput.toString();
-	}
+    private String getXmlTabulado(String xmlString) {
+        if (!xmlString.isEmpty()) {
+            try {
+                final MElement xml = MParser.parse(xmlString);
+                final StringWriter sw = new StringWriter();
+                final PrintWriter writer = new PrintWriter(sw);
+                xml.printTabulado(writer);
+                return sw.toString();
+            } catch (SAXException | IOException e) {
+                LOGGER.error(e.getMessage(), e);
+            }
+        }
+        return StringUtils.EMPTY;
+    }
 
-	private void updateListTableFromModal(AjaxRequestTarget target) {
-		updateDataList();
-		target.add(listTable);
-	}
+    private String getDefinicao(String typeName) {
+        final SPackage pacote = TemplateRepository.get().loadType(typeName).getPacote();
+        StringBuilder definicaoOutput = new StringBuilder();
+        pacote.debug(definicaoOutput);
+        return definicaoOutput.toString();
+    }
 
-	@Override
-	protected WebMarkupContainer getBreadcrumbLinks(String id) {
-		return new Fragment(id, "breadcrumbForm", this);
-	}
+    private void updateListTableFromModal(AjaxRequestTarget target) {
+        updateDataList();
+        target.add(listTable);
+    }
 
-	@Override
-	protected IModel<?> getContentTitlelModel() {
-		return new ResourceModel("label.content.title");
-	}
+    @Override
+    protected WebMarkupContainer getBreadcrumbLinks(String id) {
+        return new Fragment(id, "breadcrumbForm", this);
+    }
 
-	@Override
-	protected IModel<?> getContentSubtitlelModel() {
-		return new ResourceModel("label.content.subtitle");
-	}
+    @Override
+    protected IModel<?> getContentTitlelModel() {
+        return new ResourceModel("label.content.title");
+    }
+
+    @Override
+    protected IModel<?> getContentSubtitlelModel() {
+        return new ResourceModel("label.content.subtitle");
+    }
 }
