634c32463e32299bdb445f467e0657f870f5ba83
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2016-02-12 14:28:39 -0200

    now we properly hide overlaping boxes

--- form/wicket/src/main/java/br/net/mirante/singular/form/wicket/mapper/annotation/annotation.js
+++ form/wicket/src/main/java/br/net/mirante/singular/form/wicket/mapper/annotation/annotation.js
@@ -10,45 +10,16 @@ if( window.Annotation == undefined){
     window.Annotation.prototype = {
         setup : function(){
             var thiz = this;
-
-            var show_button = this.create_show_button();
-
             this.target_component.find('h3').append(
-                $('<div>')
-                    .attr('style','position:absolute;top:10px;right: 15px;')
-                    .append(show_button)
-                    .click(function(){
-                        if(thiz.is_blank()){
-                            thiz.open_modal[0].click();
-                            thiz.this_component.fadeOut();
-                        }else{
-                            if(!thiz.this_component.is(":visible")){
-                                var base_start = thiz.this_component.parent().offset()['top'],
-                                    base_height = thiz.this_component.height();
-                                var base_end = base_start+base_height;
-                                $(".sannotation-snipet-box:visible").each(function(i,e){
-                                    var start = $(e).parent().offset()['top'],
-                                        this_height = $(e).height();
-                                    var end = start+this_height;
-                                    if(start < base_end && end > base_start){
-                                        console.log(start, end, base_start, base_end)
-                                        $(e).fadeOut();
-                                    }
-                                });
-                                thiz.this_component.fadeIn();
-                            }else{
-                                thiz.this_component.fadeOut();
-                            }
-                        }
-                    })
+                $('<div>').attr('style','position:absolute;top:10px;right: 15px;')
+                            .append(this.create_show_button())
+                            .click(function(){thiz.toggle_button_on_click()})
             );
             this.adjust_height_position();
-            if(thiz.is_blank()) {   thiz.this_component.hide(); }
+            if(this.is_blank()) {   this.this_component.hide(); }
         },
 
-        is_blank : function () {
-            return this.approved == null;
-        },
+        is_blank : function () {    return this.approved == null;   },
 
         create_show_button : function(){
             return $('<a>')
@@ -74,6 +45,38 @@ if( window.Annotation == undefined){
                 this_offset = this.this_component.parent().offset()['top'];
             var result_offset = target_offset-this_offset;
             this.this_component.css('top',(result_offset)+"px");
+        },
+
+        toggle_button_on_click: function(){
+            if(this.is_blank()){
+                this.open_modal[0].click();
+                this.this_component.fadeOut();
+            }else{
+                if(!this.this_component.is(":visible")){
+                    this.close_overlaping_boxes();
+                    this.this_component.fadeIn();
+                }else{
+                    this.this_component.fadeOut();
+                }
+            }
+        },
+
+        close_overlaping_boxes: function(){
+            var thiz = this;
+            var base = thiz.box_bounds(thiz.this_component);
+            $(".sannotation-snipet-box:visible").each(function(i,e){
+                var bound = thiz.box_bounds($(e));
+                if(bound.start < base.end && bound.end > base.start){
+                    $(e).fadeOut();
+                }
+            });
+        },
+
+        box_bounds : function(e){
+            var start = e.css('top'), height = e.height();
+            start = Number.parseFloat(start.replace('px',''));
+            var end = start+height;
+            return {    'start' : start , 'end' : end   };
         }
     }
 
