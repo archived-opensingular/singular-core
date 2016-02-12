f77936a18d57f140804c9ebf85a75fe3334e20b1
Fabricio Nogueira Buzeto <nuk.anime13@gmail.com>
2016-02-12 15:20:31 -0200

    Now showing the annotation XML

--- form/ui-showcase/src/main/java/br/net/mirante/singular/showcase/view/page/form/crud/CrudContent.java
+++ form/ui-showcase/src/main/java/br/net/mirante/singular/showcase/view/page/form/crud/CrudContent.java
@@ -278,6 +278,11 @@ public class CrudContent extends Content
         xmlTabs.addTab(getString("label.xml.tabulado"), new BOutputPanel(BSTabPanel.getTabPanelId(), $m.ofValue(xmlTabulado)));
         xmlTabs.addTab(getString("label.xml.persistencia"), new BOutputPanel(BSTabPanel.getTabPanelId(), $m.ofValue(xmlPersistencia)));
         xmlTabs.addTab(getString("label.definicao"), new BOutputPanel(BSTabPanel.getTabPanelId(), $m.ofValue(definicao)));
+        if(hasAnnotations()){
+            final String xmlAnnotations = getXmlTabulado(model.getObject().getAnnnotations());
+            xmlTabs.addTab(getString("label.xml.anotacao"),
+                    new BOutputPanel(BSTabPanel.getTabPanelId(), $m.ofValue(xmlAnnotations)));
+        }
 
         viewXmlModal.addOrReplace(xmlTabs);
         viewXmlModal.show(target);
