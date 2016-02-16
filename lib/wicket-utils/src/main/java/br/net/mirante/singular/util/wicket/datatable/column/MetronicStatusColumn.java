e20524cf5245e736bfc8ce3bb50029bf5726e938
Vinicius Nunes <viniciusuriel@gmail.com>
2016-02-16 11:50:46 -0200

    adicionando componente para estilizar coluna de situação/status

---
+++ lib/wicket-utils/src/main/java/br/net/mirante/singular/util/wicket/datatable/column/MetronicStatusColumn.java
@@ -0,0 +1,81 @@
+package br.net.mirante.singular.util.wicket.datatable.column;
+
+import br.net.mirante.singular.lambda.IFunction;
+import br.net.mirante.singular.util.wicket.bootstrap.layout.BSContainer;
+import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.model.IModel;
+
+public class MetronicStatusColumn<T, S> extends BSPropertyColumn<T, S> {
+
+    public static enum BagdeType {
+        WARNING,
+        DANGER,
+        SUCCESS,
+        INFO
+    }
+
+    private BadgeTypeMapper<T> badgeTypeMapper;
+
+    public MetronicStatusColumn(IModel<String> displayModel, IFunction<T, Object> propertyFunction) {
+        super(displayModel, propertyFunction);
+    }
+
+    public MetronicStatusColumn(IModel<String> displayModel, String propertyExpression) {
+        super(displayModel, propertyExpression);
+    }
+
+    public MetronicStatusColumn(IModel<String> displayModel, S sortProperty, IFunction<T, Object> propertyFunction) {
+        super(displayModel, sortProperty, propertyFunction);
+    }
+
+    public MetronicStatusColumn(IModel<String> displayModel, S sortProperty, String propertyExpression) {
+        super(displayModel, sortProperty, propertyExpression);
+    }
+
+
+    public MetronicStatusColumn(IModel<String> displayModel, IFunction<T, Object> propertyFunction, BadgeTypeMapper<T> badgeTypeMapper) {
+        super(displayModel, propertyFunction);
+        this.badgeTypeMapper = badgeTypeMapper;
+    }
+
+    public MetronicStatusColumn(IModel<String> displayModel, String propertyExpression, BadgeTypeMapper<T> badgeTypeMapper) {
+        super(displayModel, propertyExpression);
+        this.badgeTypeMapper = badgeTypeMapper;
+    }
+
+    public MetronicStatusColumn(IModel<String> displayModel, S sortProperty, IFunction<T, Object> propertyFunction, BadgeTypeMapper<T> badgeTypeMapper) {
+        super(displayModel, sortProperty, propertyFunction);
+        this.badgeTypeMapper = badgeTypeMapper;
+    }
+
+    public MetronicStatusColumn(IModel<String> displayModel, S sortProperty, String propertyExpression, BadgeTypeMapper<T> badgeTypeMapper) {
+        super(displayModel, sortProperty, propertyExpression);
+        this.badgeTypeMapper = badgeTypeMapper;
+    }
+
+    @Override
+    public void populateItem(Item<ICellPopulator<T>> cellItem, String componentId, IModel<T> rowModel) {
+        BSContainer container = new BSContainer<>(componentId);
+        container.setOutputMarkupPlaceholderTag(false);
+        IModel<Object> cellModel = getDataModel(rowModel);
+        container.appendTag("span", true, getBadgeCssClassAttribute(cellModel, rowModel), new Label("label", cellModel));
+        cellItem.add(container);
+    }
+
+    private String getBadgeCssClassAttribute(IModel<Object> cellModel, IModel<T> rowModel) {
+        String css = "class=\"label label-sm ";
+        if (badgeTypeMapper != null) {
+            return css + " label-" + badgeTypeMapper.getType(cellModel, rowModel).name().toLowerCase() + "\"";
+        }
+        return css + " \" style=\"background-color: #889988;\" ";
+    }
+
+    @FunctionalInterface
+    public static interface BadgeTypeMapper<T> {
+
+        MetronicStatusColumn.BagdeType getType(IModel<Object> cellModel, IModel<T> rowModel);
+
+    }
+}
