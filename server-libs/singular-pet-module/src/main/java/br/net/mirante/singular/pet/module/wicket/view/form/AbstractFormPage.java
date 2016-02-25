fdb6cbc373bb45528614ee4ec7b150b15dde174c
Vinicius Uriel <viniciusuriel@gmail.com>
2016-02-25 15:22:20 -0300

    evolução da listagem de análise

--- server-libs/singular-pet-module/src/main/java/br/net/mirante/singular/pet/module/wicket/view/form/AbstractFormPage.java
+++ server-libs/singular-pet-module/src/main/java/br/net/mirante/singular/pet/module/wicket/view/form/AbstractFormPage.java
@@ -20,6 +20,7 @@ public abstract class AbstractFormPage extends Template {
         return false;
     }
 
+
     @Override
     protected Content getContent(String id) {
         FormPageUtil.URLParams param = FormPageUtil.readParameters(getRequest());
