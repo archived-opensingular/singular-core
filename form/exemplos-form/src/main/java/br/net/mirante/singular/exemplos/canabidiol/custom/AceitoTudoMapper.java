dd9a1b2f32708ee793ec728950f04b2444420b33
Delfino Filho <delfino.filho@mirante.net.br>
2016-03-03 11:17:33 -0300

    Implementando CustomMapper para o canabidiol.

---
+++ form/exemplos-form/src/main/java/br/net/mirante/singular/exemplos/canabidiol/custom/AceitoTudoMapper.java
@@ -0,0 +1,24 @@
+package br.net.mirante.singular.exemplos.canabidiol.custom;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.Model;
+
+import br.net.mirante.singular.form.wicket.mapper.BooleanMapper;
+import br.net.mirante.singular.form.wicket.model.AtributoModel;
+
+/**
+ * Mapper customizado para substituir os "\n"
+ * do texto da label por "<br />"
+ */
+public class AceitoTudoMapper extends BooleanMapper {
+
+    @Override
+    protected Label buildLabel(String id, AtributoModel<String> labelModel) {
+        String s = labelModel.getObject();
+        s = s.replace("\n", "<br />");
+        Label label = new Label(id, Model.of(s));
+        label.setEscapeModelStrings(false);
+        return label;
+    }
+
+}
