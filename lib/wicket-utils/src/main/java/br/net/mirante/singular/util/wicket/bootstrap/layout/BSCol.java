c196a2cc89686bc3f45273e70a67c39c63ce9159
danilo.mesquita <danilo.mesquita@mirante.net.br>
2016-03-10 19:21:21 -0300

    corrigindo grid

--- lib/wicket-utils/src/main/java/br/net/mirante/singular/util/wicket/bootstrap/layout/BSCol.java
+++ lib/wicket-utils/src/main/java/br/net/mirante/singular/util/wicket/bootstrap/layout/BSCol.java
@@ -2,19 +2,11 @@ package br.net.mirante.singular.util.wicket.bootstrap.layout;
 
 import org.apache.wicket.behavior.Behavior;
 
-import br.net.mirante.singular.util.wicket.util.WicketUtils;
-
 public class BSCol extends BSContainer<BSCol> implements IBSGridCol<BSCol> {
 
     public BSCol(String id) {
         super(id);
         add(newBSGridColBehavior());
-        /**
-         * Configuração temporaria para correção do problema de grid ( espaço em branco ao lado do campo),
-         * essa config padroniza o tamanho minimo dos componentes inseridos,
-         * evitando tamanhos aleatorios para componentes simples.
-         */
-        add(WicketUtils.$b.attrAppender("style", "min-height: 75px", ";"));
     }
 
     @Override
