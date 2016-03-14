07ebd34341a90077a15a0d91574434f968625831
Delfino Filho <delfino.filho@mirante.net.br>
2016-03-14 14:16:48 -0300

    Adição de copyright.

--- flow/persistence/src/main/java/br/net/mirante/singular/persistence/util/HybridIdentityOrSequenceGenerator.java
+++ flow/persistence/src/main/java/br/net/mirante/singular/persistence/util/HybridIdentityOrSequenceGenerator.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2016, Mirante and/or its affiliates. All rights reserved.
+ * Mirante PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
 package br.net.mirante.singular.persistence.util;
 
 import br.net.mirante.singular.commons.base.SingularProperties;
--- form/wicket/src/main/java/br/net/mirante/singular/form/wicket/model/IMInstanciaAwareModel.java
+++ form/wicket/src/main/java/br/net/mirante/singular/form/wicket/model/IMInstanciaAwareModel.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2016, Mirante and/or its affiliates. All rights reserved.
+ * Mirante PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
 package br.net.mirante.singular.form.wicket.model;
 
 import java.util.Optional;
