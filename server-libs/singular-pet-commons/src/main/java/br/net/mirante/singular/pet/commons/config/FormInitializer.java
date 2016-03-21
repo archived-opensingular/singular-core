fce210f684ff75d4703737bc2b16dee51b9b6d88
Vinicius Uriel <viniciusuriel@gmail.com>
2016-03-21 13:06:05 -0300

    refatoramento das configurações do singular.

--- server-libs/singular-pet-commons/src/main/java/br/net/mirante/singular/pet/commons/config/FormInitializer.java
+++ server-libs/singular-pet-commons/src/main/java/br/net/mirante/singular/pet/commons/config/FormInitializer.java
@@ -1,21 +1,47 @@
 package br.net.mirante.singular.pet.commons.config;
 
 import br.net.mirante.singular.form.spring.SpringSDocumentFactory;
-import org.springframework.web.context.WebApplicationContext;
+import br.net.mirante.singular.form.spring.SpringTypeLoader;
+import br.net.mirante.singular.persistence.util.HibernateSingularFlowConfigurationBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 import javax.servlet.ServletContext;
 
 public abstract class FormInitializer {
 
-    public abstract Class<? extends SpringSDocumentFactory> documentFactory();
+    public static final Logger logger = LoggerFactory.getLogger(FormInitializer.class);
+    static final String SINGULAR_FORM = "[SINGULAR][FORM] %s";
 
+    protected abstract Class<? extends SpringSDocumentFactory> documentFactory();
 
-    public void TypeLoader(){
+    protected abstract Class<? extends SpringTypeLoader> typeLoader();
 
-    }
+    protected abstract Class<? extends HibernateSingularFlowConfigurationBean> flowConfigurationBean();
 
     public void init(ServletContext ctx, AnnotationConfigWebApplicationContext applicationContext) {
-        applicationContext.register(documentFactory());
+        Class<?> documentFactory = documentFactory();
+        if (documentFactory != null) {
+            applicationContext.register(documentFactory);
+        } else {
+            logger.info(String.format(SINGULAR_FORM, " Null Form Document Factory, skipping Form Document Factory configuration. "));
+        }
+
+        Class<?> typeLoader = typeLoader();
+        if (typeLoader != null) {
+            applicationContext.register(typeLoader);
+        } else {
+            logger.info(String.format(SINGULAR_FORM, " Null Form Type Loader, skipping Form Type Loader configuration. "));
+        }
+
+        Class<?> flowConfigurationBean = flowConfigurationBean();
+        if (flowConfigurationBean != null) {
+            applicationContext.register(flowConfigurationBean);
+        } else {
+            logger.info(String.format(SINGULAR_FORM, " Null Flow Configuration Bean, skipping Flow Configuration. "));
+        }
     }
+
+
 }
--- server-libs/singular-pet-commons/src/main/java/br/net/mirante/singular/pet/commons/config/SingularInitializer.java
+++ server-libs/singular-pet-commons/src/main/java/br/net/mirante/singular/pet/commons/config/SingularInitializer.java
@@ -2,32 +2,67 @@ package br.net.mirante.singular.pet.commons.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
-import javax.servlet.ServletContainerInitializer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import java.util.Set;
+import java.util.Optional;
 
-public interface SingularInitializer extends ServletContainerInitializer {
+public interface SingularInitializer extends WebApplicationInitializer {
 
     public static final Logger logger = LoggerFactory.getLogger(SingularInitializer.class);
+    static final String SINGULAR = "[SINGULAR] %s";
 
     @Override
-    default void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
-        logger.info(" Initializing Singular.... ");
-        logger.info(" Initializing WebConfiguration ");
-        webConfiguration().init(ctx);
-        logger.info(" Initializing SpringHibernateConfiguration ");
+    default void onStartup(ServletContext ctx) throws ServletException {
+        logger.info(String.format(SINGULAR, " Initializing Singular.... "));
+        logger.info(String.format(SINGULAR, " Initializing WebConfiguration "));
+        WebInitializer webInitializer = webConfiguration();
+        if (webInitializer != null) {
+            webConfiguration().init(ctx);
+        } else {
+            logger.info(String.format(SINGULAR, " Null webInitializer, skipping web configuration"));
+        }
+
+        logger.info(String.format(SINGULAR, " Initializing SpringHibernateConfiguration "));
         SpringHibernateInitializer springHibernateInitializer = springHibernateConfiguration();
-        AnnotationConfigWebApplicationContext applicationContext = springHibernateInitializer.init(ctx);
-        logger.info(" Initializing SpringSecurity ");
-        springSecurityConfiguration().init(ctx, applicationContext, springHibernateInitializer.getSpringMVCServletMapping());
-        logger.info(" Initializing FormConfiguration ");
-        formConfiguration().init(ctx, applicationContext);
-        logger.info(" Initializing FlowConfiguration ");
-        flowConfiguration().init(ctx, applicationContext);
+        AnnotationConfigWebApplicationContext applicationContext = null;
+        if (springHibernateInitializer != null) {
+            applicationContext = springHibernateInitializer.init(ctx);
+        } else {
+            logger.info(String.format(SINGULAR, " Null springHibernateInitializer, skipping Spring configuration"));
+        }
+
+        logger.info(String.format(SINGULAR, " Initializing SpringSecurity "));
+        SpringSecurityInitializer springSecurityInitializer = springSecurityConfiguration();
+        if (springSecurityInitializer != null) {
+            springSecurityConfiguration().init(ctx, applicationContext,
+                    Optional
+                            .ofNullable(springHibernateInitializer)
+                            .map(SpringHibernateInitializer::getSpringMVCServletMapping)
+                            .orElse(null));
+        } else {
+            logger.info(String.format(SINGULAR, " Null springSecurityInitializer, skipping Spring Security configuration"));
+        }
+
+        logger.info(String.format(SINGULAR, " Initializing FormConfiguration "));
+        FormInitializer formInitializer = formConfiguration();
+        if (formInitializer != null) {
+            formConfiguration().init(ctx, applicationContext);
+        } else {
+            logger.info(String.format(SINGULAR, " Null formInitializer, skipping Singular Form configuration"));
+        }
+
+        logger.info(String.format(SINGULAR, " Initializing FlowConfiguration "));
+        FlowInitializer flowInitializer = flowConfiguration();
+        if (flowInitializer != null) {
+            flowConfiguration().init(ctx, applicationContext);
+        } else {
+            logger.info(String.format(SINGULAR, " Null flowInitializer, skipping Singular Flow configuration"));
+        }
     }
+
 
     public WebInitializer webConfiguration();
 
