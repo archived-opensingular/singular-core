fb007acffb50d3a1fb7d1a38efa0e71e6faae81e
Vinicius Nunes <viniciusuriel@gmail.com>
2016-03-22 11:11:20 -0300

    correção do locale do freemarker para manter independencia de ambiente de execução

--- form/core/src/main/java/br/net/mirante/singular/form/mform/freemarker/FormFreemarkerUtil.java
+++ form/core/src/main/java/br/net/mirante/singular/form/mform/freemarker/FormFreemarkerUtil.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -108,6 +109,7 @@ public final class FormFreemarkerUtil {
         if (cfg == null) {
             Configuration novo = new Configuration(Configuration.VERSION_2_3_22);
             novo.setDefaultEncoding("UTF-8");
+            novo.setLocale(new Locale("pt", "BR"));
             novo.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
 
             cfg = novo;
