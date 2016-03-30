7b599ae80a4e3d42ccc1425c02b561aaf809d02a
danilo.mesquita <danilo.mesquita@mirante.net.br>
2016-03-30 15:53:26 -0300

    Adicionando tabelas

---
+++ form/exemplos-form/src/main/java/br/net/mirante/singular/exemplos/notificacaosimpliciada/domain/enums/TipoObrigatoriedade.java
@@ -0,0 +1,49 @@
+package br.net.mirante.singular.exemplos.notificacaosimpliciada.domain.enums;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlEnum
+@XmlType(namespace="http://www.anvisa.gov.br/reg-med/schema/domains")
+public enum TipoObrigatoriedade {
+	
+	@XmlEnumValue("0")
+	NAO_APLICAVEL('1', "Não aplicável"),
+
+	@XmlEnumValue("1")
+	OBRIGATORIO('1', "Obrigatório"),
+	
+	@XmlEnumValue("2")
+	NAO_OBRIGATORIO('2', "Não obrigatório");
+
+	public static final String ENUM_CLASS_NAME = "br.gov.anvisa.reg.medicamento.domain.enums.TipoObrigatoriedade";
+	
+	private Character codigo;
+	private String descricao;
+
+	private TipoObrigatoriedade(Character codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	public Character getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static TipoObrigatoriedade valueOfEnum(Character codigo) {
+
+		TipoObrigatoriedade status[] = TipoObrigatoriedade.values();
+
+		for (TipoObrigatoriedade st : status) {
+			if (st.getCodigo().equals(codigo)){
+				return st;
+			}
+		}
+		return null;
+	}
+}
---
+++ form/exemplos-form/src/main/java/br/net/mirante/singular/exemplos/notificacaosimpliciada/domain/enums/TipoRestricaoSexo.java
@@ -0,0 +1,49 @@
+package br.net.mirante.singular.exemplos.notificacaosimpliciada.domain.enums;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlEnum
+@XmlType(namespace="http://www.anvisa.gov.br/reg-med/schema/domains")
+public enum TipoRestricaoSexo {
+
+	@XmlEnumValue("M")
+	MASCULINO('M', "Masculino"),
+	
+	@XmlEnumValue("F")
+	FEMININO('F', "Feminino"),
+	
+	@XmlEnumValue("A")
+	AMBOS('A', "Ambos");
+
+	public static final String ENUM_CLASS_NAME = "br.gov.anvisa.reg.medicamento.domain.enums.TipoRestricaoSexo";
+	
+	private Character codigo;
+	private String descricao;
+
+	private TipoRestricaoSexo(Character codigo, String descricao) {
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+
+	public Character getCodigo() {
+		return codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static TipoRestricaoSexo valueOfEnum(Character codigo) {
+
+		TipoRestricaoSexo status[] = TipoRestricaoSexo.values();
+
+		for (TipoRestricaoSexo st : status) {
+			if (codigo != null && st.getCodigo().charValue() == codigo.charValue()){
+				return st;
+			}
+		}
+		return null;
+	}
+}
