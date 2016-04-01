43f05c514039ecae37e3d14e64995e510c2cb6ed
Vinicius Nunes <viniciusuriel@gmail.com>
2016-04-01 15:53:51 -0300

    versão inicial do formulário dinamizado

--- form/exemplos-form/src/main/java/br/net/mirante/singular/exemplos/notificacaosimplificada/form/dinamizado/SPackageNotificacaoSimplificadaDinamizado.java
+++ form/exemplos-form/src/main/java/br/net/mirante/singular/exemplos/notificacaosimplificada/form/dinamizado/SPackageNotificacaoSimplificadaDinamizado.java
@@ -6,6 +6,7 @@
 package br.net.mirante.singular.exemplos.notificacaosimplificada.form.dinamizado;
 
 import br.net.mirante.singular.exemplos.notificacaosimplificada.domain.FormaFarmaceuticaBasica;
+import br.net.mirante.singular.exemplos.notificacaosimplificada.domain.LinhaCbpf;
 import br.net.mirante.singular.exemplos.notificacaosimplificada.form.STypeAcondicionamento;
 import br.net.mirante.singular.exemplos.notificacaosimplificada.form.baixorisco.SPackageNotificacaoSimplificadaBaixoRisco;
 import br.net.mirante.singular.exemplos.notificacaosimplificada.form.vocabulario.STypeCategoriaRegulatoria;
@@ -21,9 +22,12 @@ import br.net.mirante.singular.form.mform.STypeAttachmentList;
 import br.net.mirante.singular.form.mform.STypeComposite;
 import br.net.mirante.singular.form.mform.STypeList;
 import br.net.mirante.singular.form.mform.STypeSimple;
+import br.net.mirante.singular.form.mform.basic.view.SViewAutoComplete;
 import br.net.mirante.singular.form.mform.basic.view.SViewListByMasterDetail;
-import br.net.mirante.singular.form.mform.basic.view.SViewTab;
+import br.net.mirante.singular.form.mform.basic.view.SViewListByTable;
 import br.net.mirante.singular.form.mform.core.STypeString;
+import br.net.mirante.singular.form.mform.util.transformer.Value;
+import org.apache.commons.lang3.tuple.Triple;
 
 @SInfoType(spackage = SPackageNotificacaoSimplificadaDinamizado.class)
 public class SPackageNotificacaoSimplificadaDinamizado extends SPackage {
@@ -59,6 +63,55 @@ public class SPackageNotificacaoSimplificadaDinamizado extends SPackage {
                 .asAtrBootstrap()
                 .colPreference(8);
 
+
+        final STypeComposite<?> linhaProducao = notificacaoSimplificada.addFieldComposite("linhaProducao");
+        STypeSimple idLinhaProducao = linhaProducao.addFieldInteger("id");
+        STypeSimple descricaoLinhaProducao = linhaProducao.addFieldString("descricao");
+
+        linhaProducao
+                .asAtrBasic()
+                .required()
+                .label("Linha de Produção")
+                .asAtrBootstrap()
+                .colPreference(4);
+        linhaProducao.setView(SViewAutoComplete::new);
+        linhaProducao.withSelectionFromProvider(descricaoLinhaProducao, (ins, filter) -> {
+            final SIList<?> list = ins.getType().newList();
+            for (LinhaCbpf lc : dominioService(ins).linhasProducao(filter)) {
+                final SIComposite c = (SIComposite) list.addNew();
+                c.setValue(idLinhaProducao, lc.getId());
+                c.setValue(descricaoLinhaProducao, lc.getDescricao());
+            }
+            return list;
+        });
+
+
+        final STypeComposite<?> configuracaoLinhaProducao = notificacaoSimplificada.addFieldComposite("configuracaoLinhaProducao");
+        STypeSimple idConfiguracaoLinhaProducao = configuracaoLinhaProducao.addFieldInteger("id");
+        STypeSimple idLinhaProducaoConfiguracao = configuracaoLinhaProducao.addFieldInteger("idLinhaProducao");
+        STypeSimple descConfiguracaoLinhaProducao = configuracaoLinhaProducao.addFieldString("descricao");
+
+        configuracaoLinhaProducao
+                .asAtrBasic()
+                .label("Descrição")
+                .required()
+                .dependsOn(linhaProducao)
+                .visivel(i -> Value.notNull(i, idLinhaProducao))
+                .asAtrBootstrap()
+                .colPreference(4);
+        configuracaoLinhaProducao
+                .withSelectView()
+                .withSelectionFromProvider(descConfiguracaoLinhaProducao, (optionsInstance, lb) -> {
+                    Integer id = (Integer) Value.of(optionsInstance, idLinhaProducao);
+                    for (Triple p : dominioService(optionsInstance).configuracoesLinhaProducaoDinamizado(id)) {
+                        lb
+                                .add()
+                                .set(idConfiguracaoLinhaProducao, p.getLeft())
+                                .set(idLinhaProducaoConfiguracao, p.getMiddle())
+                                .set(descConfiguracaoLinhaProducao, p.getRight());
+                    }
+                });
+
         final STypeComposite<?> formaFarmaceutica = notificacaoSimplificada.addFieldComposite("formaFarmaceutica");
         SType<?> idFormaFormaceutica = formaFarmaceutica.addFieldInteger("id");
         STypeSimple descFormaFormaceutica = formaFarmaceutica.addFieldString("descricao");
@@ -66,6 +119,8 @@ public class SPackageNotificacaoSimplificadaDinamizado extends SPackage {
                 .asAtrBasic()
                 .required()
                 .label("Forma Farmacêutica")
+                .dependsOn(configuracaoLinhaProducao)
+                .visivel(i -> Value.notNull(i, idConfiguracaoLinhaProducao))
                 .asAtrBootstrap()
                 .colPreference(4);
         formaFarmaceutica
@@ -78,6 +133,65 @@ public class SPackageNotificacaoSimplificadaDinamizado extends SPackage {
                         c.setValue(descFormaFormaceutica, ffb.getDescricao());
                     }
                     return list;
+                });
+
+
+        final STypeList<STypeComposite<SIComposite>, SIComposite> formulasHomeopaticas = notificacaoSimplificada.addFieldListOfComposite("formulasHomeopaticas", "formulaHomeopatica");
+        formulasHomeopaticas
+                .withMiniumSizeOf(1)
+                .withView(SViewListByTable::new)
+                .asAtrBasic()
+                .label("Descrição")
+                .dependsOn(configuracaoLinhaProducao)
+                .visivel(i -> Value.notNull(i, idConfiguracaoLinhaProducao));
+
+        final STypeComposite<?> formulaHomeopatica = formulasHomeopaticas.getElementsType();
+        final STypeComposite<?> descricaoDinamizada = formulaHomeopatica.addFieldComposite("descricaoDinamizada");
+        STypeSimple idDescricaoDinamizada = descricaoDinamizada.addFieldInteger("id");
+        STypeSimple idConfiguracaoLinhaProducaoDescricaoDinamizada = descricaoDinamizada.addFieldInteger("configuracaoLinhaProducao");
+        STypeSimple descricaoDescricaoDinamizada = descricaoDinamizada.addFieldString("descricao");
+        descricaoDinamizada
+                .asAtrBasic()
+                .label("Descrição")
+                .required()
+                .asAtrBootstrap()
+                .colPreference(6);
+        descricaoDinamizada
+                .withSelectView()
+                .withSelectionFromProvider(descricaoDescricaoDinamizada, (optionsInstance, lb) -> {
+                    Integer id = (Integer) Value.of(optionsInstance, idConfiguracaoLinhaProducao);
+                    for (Triple p : dominioService(optionsInstance).descricoesHomeopaticas(id)) {
+                        lb
+                                .add()
+                                .set(idDescricaoDinamizada, p.getLeft())
+                                .set(idConfiguracaoLinhaProducaoDescricaoDinamizada, p.getMiddle())
+                                .set(descricaoDescricaoDinamizada, p.getRight());
+                    }
+                });
+
+
+        final STypeComposite<?> diluicao = formulaHomeopatica.addFieldComposite("diluicao");
+        SType<?> idConcentracacao = diluicao.addFieldInteger("id");
+        STypeSimple idSubstanciaConcentracao = diluicao.addFieldInteger("idSubstancia");
+        STypeSimple descConcentracao = diluicao.addFieldString("descricao");
+        diluicao
+                .asAtrBasic()
+                .required()
+                .label("Faixa de Diluição / Potência")
+                .dependsOn(descricaoDinamizada)
+                .asAtrBootstrap()
+                .colPreference(6);
+        diluicao
+                .withSelectView()
+                .withSelectionFromProvider(descConcentracao, (optionsInstance, lb) -> {
+                    Integer id = (Integer) Value.of(optionsInstance, idDescricaoDinamizada);
+                    for (Triple p : dominioService(optionsInstance).diluicoes(id)) {
+                        lb
+                                .add()
+                                .set(idConcentracacao, p.getLeft())
+                                .set(idSubstanciaConcentracao, p.getMiddle())
+                                .set(descConcentracao, p.getRight());
+                    }
                 });
 
 
