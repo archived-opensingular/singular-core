7d0e48cfb4a9cbf0b62427aee96f8eec130b0baf
Delfino Filho <delfino.filho@mirante.net.br>
2016-04-15 14:29:45 -0300

    Merge

---
+++ server-libs/server-commons/src/main/java/br/net/mirante/singular/server/commons/service/IServerMetadataREST.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2016, Mirante and/or its affiliates. All rights reserved.
+ * Mirante PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package br.net.mirante.singular.server.commons.service;
+
+import java.util.List;
+
+import br.net.mirante.singular.server.commons.service.dto.MenuGroupDTO;
+
+public interface IServerMetadataREST {
+
+    String PATH_LIST_MENU = "/menu/list";
+
+    List<MenuGroupDTO> listMenu();
+
+}
