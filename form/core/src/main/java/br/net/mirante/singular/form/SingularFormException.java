30bf54d0f4ada7bc6ffb3c4f980c222bda5b537c
Daniel Bordin <daniel@mirante.net.br>
2016-05-31 13:55:11 -0300

    [FORM-CORE] Adicionada interface fluente em SingularException e SingularFormException para faciltiar inclusão de informações de detalhe na Exception (novos métodos add(XXXXX)).

--- form/core/src/main/java/br/net/mirante/singular/form/SingularFormException.java
+++ form/core/src/main/java/br/net/mirante/singular/form/SingularFormException.java
@@ -8,6 +8,8 @@ package br.net.mirante.singular.form;
 
 import br.net.mirante.singular.commons.base.SingularException;
 
+import java.util.ArrayList;
+
 public class SingularFormException extends SingularException {
 
     public SingularFormException() {
@@ -22,7 +24,8 @@ public class SingularFormException extends SingularException {
      * SInstance, então simplesmente dá um toString() no mesmo.
      */
     public SingularFormException(String msg, Object target) {
-        super(createErroMsg(msg, target, null));
+        super(msg);
+        add(target);
     }
 
     /**
@@ -30,14 +33,16 @@ public class SingularFormException extends SingularException {
      * ocorreu o erro.
      */
     public SingularFormException(String msg, SInstance instance) {
-        super(createErroMsg(msg, instance, null));
+        super(msg);
+        add(instance);
     }
 
     /**
      * Cria o erro incluindo informações de contexto do tipo sobre o qual ocorreu o erro.
      */
     public SingularFormException(String msg, SType<?> type) {
-        super(createErroMsg(msg, type, null));
+        super(msg);
+        add(type);
     }
 
     /**
@@ -45,7 +50,9 @@ public class SingularFormException extends SingularException {
      * ocorreu o erro e permite acrescentar informação adicional.
      */
     public SingularFormException(String msg, SInstance instance, String additionalMsgContext) {
-        super(createErroMsg(msg, instance, additionalMsgContext));
+        super(msg);
+        add(instance);
+        add(additionalMsgContext);
     }
 
     public SingularFormException(String msg, Throwable cause) {
@@ -57,59 +64,69 @@ public class SingularFormException extends SingularException {
      * ocorreu o erro.
      */
     public SingularFormException(String msg, Throwable cause, SInstance instance) {
-        super(createErroMsg(msg, instance, null), cause);
+        super(msg, cause);
+        add(instance);
     }
 
-    private static String createErroMsg(String msg, Object target, String complement) {
-        if (target instanceof SType) {
-            return createErroMsg(msg, (SType<?>) target, complement);
-        } else if (target instanceof SInstance) {
-            return createErroMsg(msg, (SInstance) target, complement);
-        } else if (target != null) {
-            try {
-                StringBuilder sb = new StringBuilder(msg).append('\n');
-                sb.append(", target=").append(target);
-                if (complement != null) {
-                    sb.append("\n, ").append(complement);
-                }
-                return sb.toString();
-            } catch (Exception e) {
-            }
+    public SingularFormException add(SScope scope) {
+        if (scope instanceof SType) {
+            add((SType<?>) scope);
+        } else if (scope instanceof SInstance) {
+            add((SInstance) scope);
+        } else {
+            super.add(scope);
         }
-        return msg;
+        return this ;
     }
 
-    private static String createErroMsg(String msg, SType<?> type, String complement) {
-        if (type != null) {
-            try {
-                StringBuilder sb = new StringBuilder(msg).append('\n');
-                sb.append(", tipo=").append(type);
-                if (complement != null) {
-                    sb.append("\n, ").append(complement);
-                }
-                return sb.toString();
-            } catch (Exception e) {
-            }
-        }
-        return msg;
-    }
-
-    private static String createErroMsg(String msg, SInstance instance, String complemento) {
+    public SingularFormException add(SInstance instance) {
         if (instance != null) {
-            try {
-                StringBuilder sb = new StringBuilder(msg);
-                sb.append(" (instancia=").append(instance.getPathFull());
-                sb.append(", classeInstancia=").append(instance.getClass());
-                sb.append(", tipo=").append(instance.getType().getName());
-                sb.append(", classeTipo=").append(instance.getType().getClass().getName());
-                if (complemento != null) {
-                    sb.append(", ").append(complemento);
-                }
-                sb.append(")");
-                return sb.toString();
-            } catch (Exception e) {
-            }
+            add("instancia path", instance.getPathFull());
+            add("instancia classe", instance.getClass());
+            add("instancia type", instance.getType());
+            add("instancia type class", instance.getType().getClass().getName());
         }
-        return msg;
+        return this;
+    }
+
+    public SingularFormException add(SType<?> type) {
+        if (type != null) {
+            add("type", type);
+        }
+        return this;
+    }
+
+    /**
+     * Adiciona um nova linha de informação extra na exception a ser exibida junto com a mensagem da mesma.
+     * @param value Valor da informação (pode ser null)
+     */
+    public SingularFormException add(Object value) {
+        if (value instanceof SType) {
+            add((SType<?>) value);
+        } else if (value instanceof SInstance) {
+            add((SInstance) value);
+        } else {
+            super.add(value);
+        }
+        return this;
+    }
+
+    /**
+     * Adiciona um nova linha de informação extra na exception a ser exibida junto com a mensagem da mesma.
+     * @param label Label da informação (pode ser null)
+     * @param value Valor da informação (pode ser null)
+     */
+    public SingularFormException add(String label, Object value) {
+        return (SingularFormException) super.add(label, value);
+    }
+
+    /**
+     * Adiciona um nova linha de informação extra na exception a ser exibida junto com a mensagem da mesma.
+     * @param level Nível de indentação da informação
+     * @param label Label da informação (pode ser null)
+     * @param value Valor da informação (pode ser null)
+     */
+    public SingularFormException add(int level, String label, Object value) {
+        return (SingularFormException) super.add(level, label, value);
     }
 }
