d771c8b3bd58f7a8a32e85ef552460b9e7e2b24c
Danilo Mesquita <danilofmesquita@gmail.com>
2016-06-10 16:42:52 -0300

    Escondendo box quando estiver vazio

--- form/wicket/src/main/java/br/net/mirante/singular/form/wicket/mapper/composite/BlocksCompositeMapper.java
+++ form/wicket/src/main/java/br/net/mirante/singular/form/wicket/mapper/composite/BlocksCompositeMapper.java
@@ -6,13 +6,24 @@ import br.net.mirante.singular.form.type.core.SPackageBootstrap;
 import br.net.mirante.singular.form.view.Block;
 import br.net.mirante.singular.form.view.SViewByBlock;
 import br.net.mirante.singular.form.wicket.WicketBuildContext;
+import br.net.mirante.singular.form.wicket.model.IMInstanciaAwareModel;
 import br.net.mirante.singular.form.wicket.model.SInstanceCampoModel;
 import br.net.mirante.singular.util.wicket.bootstrap.layout.BSGrid;
 import br.net.mirante.singular.util.wicket.bootstrap.layout.BSRow;
 import br.net.mirante.singular.util.wicket.bootstrap.layout.TemplatePanel;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.wicket.Component;
+import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.behavior.Behavior;
+import org.apache.wicket.event.IEvent;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.util.visit.IVisit;
+import org.apache.wicket.util.visit.IVisitor;
+
+import java.io.Serializable;
 
 import static br.net.mirante.singular.util.wicket.util.WicketUtils.$b;
 
@@ -29,21 +40,104 @@ public class BlocksCompositeMapper extends AbstractCompositeMapper {
             super(ctx);
         }
 
-        private BSGrid buildPortletPanel(BSGrid grid, String id, Block block) {
-            String templatePortlet = "";
-            templatePortlet += " <div class='portlet light'>                         ";
-            templatePortlet += "     <div class='portlet-title' wicket:id='title' /> ";
-            templatePortlet += "     <div class='portlet-body'>                      ";
-            templatePortlet += "         <div wicket:id='grid' />                     ";
-            templatePortlet += "     </div>                                          ";
-            templatePortlet += " </div>                                              ";
+        @Override
+        protected void buildFields(WicketBuildContext ctx, BSGrid grid) {
+            final SViewByBlock view = (SViewByBlock) ctx.getView();
 
-            final TemplatePanel portlet = new TemplatePanel(id, templatePortlet);
-            final BSGrid        newGrid = new BSGrid("grid");
+            for (int i = 0; i < view.getBlocks().size(); i++) {
 
-            portlet.add(newGrid, buildPortletTitle(block));
-            grid.appendTag("div", portlet);
-            return newGrid;
+                final Block        block   = view.getBlocks().get(i);
+                final PortletPanel portlet = new PortletPanel("_portlet" + i, block);
+                final BSGrid       newGrid = portlet.getNewGrid();
+                BSRow              row     = newGrid.newRow();
+
+                grid.appendTag("div", portlet);
+
+                for (String typeName : block.getTypes()) {
+                    final SType<?>                       field  = type.getField(typeName);
+                    final Boolean                        newRow = field.getAttributeValue(SPackageBootstrap.ATR_COL_ON_NEW_ROW);
+                    final SInstanceCampoModel<SInstance> im     = fieldModel(field);
+                    if (newRow != null && newRow) {
+                        row = newGrid.newRow();
+                    }
+                    buildField(ctx.getUiBuilderWicket(), row, im);
+                }
+
+                portlet.add(new ConfigurePortletVisibilityBehaviour(block));
+            }
+
+        }
+    }
+
+    private static class ConfigurePortletVisibilityBehaviour extends Behavior {
+
+        private final Block block;
+
+        private ConfigurePortletVisibilityBehaviour(Block block) {
+            this.block = block;
+        }
+
+        @Override
+        public void onConfigure(Component c) {
+            super.onConfigure(c);
+            final MarkupContainer container    = (MarkupContainer) c;
+            final Boolean         isAnyVisible = container.visitChildren(Component.class, new VisibilityVisitor(block));
+            container.setVisible(isAnyVisible != null && isAnyVisible);
+        }
+
+    }
+
+    private static class VisibilityVisitor implements IVisitor<Component, Boolean>, Serializable {
+
+        private final Block block;
+
+        private VisibilityVisitor(Block block) {
+            this.block = block;
+        }
+
+        @Override
+        public void component(Component component, IVisit<Boolean> visit) {
+            IModel<?> model = component.getDefaultModel();
+            if (model != null && IMInstanciaAwareModel.class.isAssignableFrom(model.getClass())) {
+                SInstance si = ((IMInstanciaAwareModel) model).getMInstancia();
+                if (block.getTypes().contains(si.getType().getNameSimple())) {
+                    if (si.asAtr().isVisible()) {
+                        visit.stop(true);
+                    }
+                }
+            }
+        }
+    }
+
+    private static class PortletPanel extends TemplatePanel {
+
+        private static final String templatePortlet = ""
+                + " <div class='portlet light'>                          "
+                + "     <div class='portlet-title' wicket:id='title' />  "
+                + "     <div class='portlet-body'>                       "
+                + "         <div wicket:id='grid' />                     "
+                + "     </div>                                           "
+                + " </div>                                               ";
+
+        private final Block  block;
+        private final BSGrid newGrid;
+
+
+        protected PortletPanel(String id, Block block) {
+            super(id, templatePortlet);
+            this.block = block;
+            this.newGrid = new BSGrid("grid");
+            add(newGrid, buildPortletTitle(block));
+        }
+
+        @Override
+        public void onEvent(IEvent<?> event) {
+            super.onEvent(event);
+            final Boolean isAnyVisible = visitChildren(Component.class, new VisibilityVisitor(block));
+            setVisible(isAnyVisible != null && isAnyVisible);
+            if (AjaxRequestTarget.class.isAssignableFrom(event.getPayload().getClass())) {
+                ((AjaxRequestTarget) event.getPayload()).add(this);
+            }
         }
 
         private TemplatePanel buildPortletTitle(Block block) {
@@ -58,26 +152,10 @@ public class BlocksCompositeMapper extends AbstractCompositeMapper {
             return portletTitle;
         }
 
-        @Override
-        protected void buildFields(WicketBuildContext ctx, BSGrid grid) {
-            final SViewByBlock view = (SViewByBlock) ctx.getView();
-
-            for (int i = 0; i < view.getBlocks().size(); i++) {
-
-                final Block block = view.getBlocks().get(i);
-                BSRow       row   = buildPortletPanel(grid, "_portlet" + i, block).newRow();
-
-                for (String typeName : block.getTypes()) {
-                    final SType<?>                       field  = type.getField(typeName);
-                    final Boolean                        newRow = field.getAttributeValue(SPackageBootstrap.ATR_COL_ON_NEW_ROW);
-                    final SInstanceCampoModel<SInstance> im     = fieldModel(field);
-                    if (newRow != null && newRow) {
-                        row = grid.newRow();
-                    }
-                    buildField(ctx.getUiBuilderWicket(), row, im);
-                }
-            }
+        public BSGrid getNewGrid() {
+            return newGrid;
         }
     }
+
 
 }
