4b50b7bbed96629d7a89ee0e9cfd632a9752af9a
Fabricio Buzeto <nuk.anime13@gmail.com>
2016-06-10 16:14:52 -0300

    Testing new layout

---
+++ form/wicket/src/main/java/br/net/mirante/singular/form/wicket/mapper/attachment/FileListUploadPanel.java
@@ -0,0 +1,13 @@
+package br.net.mirante.singular.form.wicket.mapper.attachment;
+
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+
+/**
+ * Created by nuk on 10/06/16.
+ */
+public class FileListUploadPanel extends Panel {
+    public FileListUploadPanel(String id, IModel<?> model) {
+        super(id, model);
+    }
+}
