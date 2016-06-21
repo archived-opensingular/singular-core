6b7915b2ba2ccc66b71bc2c0978d0e156ccde2fb
Danilo Mesquita <danilofmesquita@gmail.com>
2016-06-21 19:23:03 -0300

    Extraindo SType

---
+++ form/exemplos-form/src/main/java/br/net/mirante/singular/exemplos/ggtox/primariasimplificada/form/STypePeticaoPrimariaSimplificada.java
@@ -0,0 +1,19 @@
+package br.net.mirante.singular.exemplos.ggtox.primariasimplificada.form;
+
+import br.net.mirante.singular.form.SIComposite;
+import br.net.mirante.singular.form.SInfoType;
+import br.net.mirante.singular.form.STypeComposite;
+import br.net.mirante.singular.form.TypeBuilder;
+
+@SInfoType(name = "STypePeticaoPrimariaSimplificada", spackage = SPackagePeticaoPrimariaSimplificada.class)
+public class STypePeticaoPrimariaSimplificada extends STypeComposite<SIComposite> {
+
+    @Override
+    protected void onLoadType(TypeBuilder tb) {
+        super.onLoadType(tb);
+
+        asAtr().label("Petição primaria Simplificada");
+
+        SPackagePeticaoPrimariaSimplificada.onLoadType(this);
+    }
+}
