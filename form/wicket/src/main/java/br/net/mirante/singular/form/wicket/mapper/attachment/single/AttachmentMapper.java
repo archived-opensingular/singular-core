cb9ec4bb0a26a6fca63c683564e2c6a5d43a8f7d
Vinicius Nunes <viniciusuriel@gmail.com>
2016-06-27 14:59:32 -0300

    [SINGULAR-FORM] correção no modo visualizaçãoo do anexo simples

--- form/wicket/src/main/java/br/net/mirante/singular/form/wicket/mapper/attachment/single/AttachmentMapper.java
+++ form/wicket/src/main/java/br/net/mirante/singular/form/wicket/mapper/attachment/single/AttachmentMapper.java
@@ -11,10 +11,11 @@ import br.net.mirante.singular.form.type.core.attachment.SIAttachment;
 import br.net.mirante.singular.form.wicket.IAjaxUpdateListener;
 import br.net.mirante.singular.form.wicket.enums.ViewMode;
 import br.net.mirante.singular.form.wicket.mapper.ControlsFieldComponentAbstractMapper;
+import br.net.mirante.singular.form.wicket.mapper.attachment.DownloadLink;
+import br.net.mirante.singular.form.wicket.mapper.attachment.DownloadSupportedBehavior;
 import br.net.mirante.singular.util.wicket.bootstrap.layout.BSWellBorder;
 import br.net.mirante.singular.util.wicket.bootstrap.layout.TemplatePanel;
 import org.apache.wicket.Component;
-import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
@@ -47,21 +48,18 @@ public class AttachmentMapper extends ControlsFieldComponentAbstractMapper {
         markup += " <div wicket:id='well'> ";
         markup += "     <div stype='min-height: 20px; white-space: pre-wrap; word-wrap: break-word;'> ";
         markup += "         <i class='fa fa-file'></i> ";
-        markup += "         <a wicket:id='downloadLink'><span wicket:id='fileName'></span></a> ";
+        markup += "         <a wicket:id='downloadLink'></a> ";
         markup += "     </div> ";
         markup += " </div> ";
 
         final BSWellBorder well = BSWellBorder.small("well");
-        final Label fileName = new Label("fileName", Model.of(((SIAttachment) model.getObject()).getFileName()));
-        final WebMarkupContainer downloadLink = new WebMarkupContainer("downloadLink");
         final TemplatePanel panel = new TemplatePanel("_readOnlyAttachment", markup);
-        //TODO vinciius organizar isso
-        downloadLink.add(fileName);
+        final DownloadSupportedBehavior downloadSupportedBehavior = new DownloadSupportedBehavior(model);
+        final DownloadLink downloadLink = new DownloadLink("downloadLink", (IModel<SIAttachment>) model, downloadSupportedBehavior);
+        panel.add(downloadSupportedBehavior);
         well.add(downloadLink);
         panel.add(well);
-
         formGroup.appendTag("div", panel);
-
         return panel;
     }
 
