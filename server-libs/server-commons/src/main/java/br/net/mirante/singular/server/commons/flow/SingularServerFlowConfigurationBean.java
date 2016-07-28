368a176b0a1d9df5b858d2876cdc2e2d47c0e444
Vinicius Uriel <viniciusuriel@gmail.com>
2016-07-28 09:36:38 -0300

    [PET-SERVER] carga EAGER das definições de fluxo

--- server-libs/server-commons/src/main/java/br/net/mirante/singular/server/commons/flow/SingularServerFlowConfigurationBean.java
+++ server-libs/server-commons/src/main/java/br/net/mirante/singular/server/commons/flow/SingularServerFlowConfigurationBean.java
@@ -1,29 +1,58 @@
 package br.net.mirante.singular.server.commons.flow;
 
+import br.net.mirante.singular.commons.util.Loggable;
 import br.net.mirante.singular.flow.core.Flow;
 import br.net.mirante.singular.flow.core.renderer.IFlowRenderer;
 import br.net.mirante.singular.flow.core.renderer.YFilesFlowRenderer;
 import br.net.mirante.singular.persistence.util.HibernateSingularFlowConfigurationBean;
+import br.net.mirante.singular.server.commons.config.ConfigProperties;
 import br.net.mirante.singular.server.commons.config.SingularServerConfiguration;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
-public class SingularServerFlowConfigurationBean extends HibernateSingularFlowConfigurationBean {
+public class SingularServerFlowConfigurationBean extends HibernateSingularFlowConfigurationBean implements Loggable {
 
     @Inject
     private SingularServerConfiguration singularServerConfiguration;
+
+    @Inject
+    private SessionFactory sessionFactory;
 
     @PostConstruct
     protected void postConstruct() {
         this.setProcessGroupCod(singularServerConfiguration.getProcessGroupCod());
         this.setDefinitionsBasePackage(singularServerConfiguration.getDefinitionsBasePackage());
         Flow.setConf(this);
+        initializeFlowDefinitionsDatabase();
     }
 
     @Override
     public IFlowRenderer getFlowRenderer() {
 //        return JGraphFlowRenderer.INSTANCE;
         return YFilesFlowRenderer.getInstance();
+    }
+
+    public void initializeFlowDefinitionsDatabase() {
+        if ("true".equals(ConfigProperties.get(ConfigProperties.SINGULAR_EAGER_LOAD_FLOW_DEFINITIONS))) {
+            TransactionSynchronizationManager.bindResource(this.sessionFactory, sessionFactory.openSession());
+            getLogger().info("INITIALIZING FLOW DEFINITIONS");
+            singularServerConfiguration.processDefinitionFormNameMap().keySet().stream().forEach(pdclass -> {
+                try {
+                    getLogger().info("INITIALIZING " + pdclass.getName() + "....");
+                    pdclass.newInstance().getEntityProcessVersion();
+                } catch (InstantiationException | IllegalAccessException e) {
+                    getLogger().error(e.getMessage(), e);
+                }
+            });
+            Session session = (Session) TransactionSynchronizationManager.unbindResource(this.sessionFactory);
+            if (session.isOpen()) {
+                session.flush();
+                session.close();
+            }
+        }
     }
 }
