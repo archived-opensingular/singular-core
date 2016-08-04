a4f9de3b6cb4f24714be6cd769a700281becfd03
Danilo Mesquita <danilofmesquita@gmail.com>
2016-08-04 12:45:00 -0300

    Configurando skin do ckeditor para utilizar 'office2013'

--- lib/wicket-utils/src/main/java/br/net/mirante/singular/util/wicket/behavior/CKEditorInitBehaviour.java
+++ lib/wicket-utils/src/main/java/br/net/mirante/singular/util/wicket/behavior/CKEditorInitBehaviour.java
@@ -6,7 +6,16 @@ public class CKEditorInitBehaviour extends InitScriptBehaviour {
 
     @Override
     public String getScript(Component component) {
-        return String.format("CKEDITOR.replace('%s', {skin : 'flat'} );", component.getMarkupId(true));
+
+        String js = "";
+
+        js += " (function(id){";
+//        js += "     if( typeof CKEDITOR.instances[id] === 'undefined' ) { ";
+        js += "         CKEDITOR.replace(id, {skin : 'office2013', language : 'pt-br'} );";
+//        js += "     }";
+        js += " }('%s'));";
+
+        return String.format(js, component.getMarkupId(true));
     }
 
 }
