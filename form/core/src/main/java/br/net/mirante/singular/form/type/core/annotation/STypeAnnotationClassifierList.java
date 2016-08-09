52a819e999f22d131de6c18074a719d879ae96c8
Danilo Mesquita <danilofmesquita@gmail.com>
2016-08-09 14:02:56 -0300

    Corrigindo compilação

--- form/core/src/main/java/br/net/mirante/singular/form/type/core/annotation/STypeAnnotationClassifierList.java
+++ form/core/src/main/java/br/net/mirante/singular/form/type/core/annotation/STypeAnnotationClassifierList.java
@@ -3,8 +3,8 @@ package br.net.mirante.singular.form.type.core.annotation;
 import br.net.mirante.singular.form.SInfoType;
 import br.net.mirante.singular.form.STypeList;
 import br.net.mirante.singular.form.TypeBuilder;
+import br.net.mirante.singular.form.type.basic.SPackageBasic;
 import br.net.mirante.singular.form.type.core.SIString;
-import br.net.mirante.singular.form.type.core.SPackageCore;
 import br.net.mirante.singular.form.type.core.STypeString;
 
 /**
@@ -12,7 +12,7 @@ import br.net.mirante.singular.form.type.core.STypeString;
  * estar presentes em um mesmo STYPE
  * @author Vinicius Uriel
  */
-@SInfoType(name = "AnnotationClassifierList", spackage = SPackageCore.class)
+@SInfoType(name = "AnnotationClassifierList", spackage = SPackageBasic.class)
 public class STypeAnnotationClassifierList extends STypeList<STypeString, SIString> {
 
     @Override
