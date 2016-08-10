0cdbc5723a0a87c74bb1a2d4f5b9c6c75db12396
Danilo Mesquita <danilofmesquita@gmail.com>
2016-08-10 17:32:31 -0300

    Corrigindo save do form

--- lib/singular-support/src/main/java/br/net/mirante/singular/support/persistence/GenericDAO.java
+++ lib/singular-support/src/main/java/br/net/mirante/singular/support/persistence/GenericDAO.java
@@ -1,39 +1,36 @@
-/*
- * Copyright (c) 2016, Mirante and/or its affiliates. All rights reserved.
- * Mirante PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
+package br.net.mirante.singular.support.persistence;
 
-package br.net.mirante.singular.exemplos.notificacaosimplificada.dao;
-
-import br.net.mirante.singular.support.persistence.BaseDAO;
+import br.net.mirante.singular.support.persistence.SimpleDAO;
+import br.net.mirante.singular.support.persistence.entity.BaseEntity;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Repository
-public class GenericDAO extends BaseDAO {
+public class GenericDAO extends SimpleDAO {
 
-    public GenericDAO() {
-        super(null);
+    public <PK extends Serializable, T extends BaseEntity<PK>> void saveOrUpdate(T entity) {
+        getSession().saveOrUpdate(entity);
     }
 
-    public <T extends Object> List<T> findByProperty(Class<T> classe, String propertyName, String value) {
+    public <T> List<T> findByProperty(Class<T> classe, String propertyName, String value) {
         return findByProperty(classe, propertyName, value, null, null);
     }
 
-    public <T extends Object> List<T> findByProperty(Class<T> classe, String propertyName, String value, Integer maxResults) {
+    public <T> List<T> findByProperty(Class<T> classe, String propertyName, String value, Integer maxResults) {
         return findByProperty(classe, propertyName, value, null, maxResults);
     }
 
-    public <T> T findByUniqueProperty(Class<T> clazz, String propertyName, Object value){
+    public <T> T findByUniqueProperty(Class<T> clazz, String propertyName, Object value) {
         return (T) getSession().createCriteria(clazz).add(Restrictions.eq(propertyName, value)).setMaxResults(1).uniqueResult();
     }
 
     @SuppressWarnings("unchecked")
-    public <T extends Object> List<T> findByProperty(Class<T> classe, String propertyName, String value, MatchMode matchMode, Integer maxResults) {
+    public <T> List<T> findByProperty(Class<T> classe, String propertyName, String value, MatchMode matchMode, Integer maxResults) {
         Criteria criteria = getSession().createCriteria(classe);
 
         if (matchMode == null) {
