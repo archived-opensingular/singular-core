54c4008a00492a0d593d95bd2c9949e2a17d2934
danilo.mesquita <danilofmesquita@gmail.com>
2016-08-17 18:10:38 -0300

    Adicionando listagem de analises

--- form/exemplos-form/src/main/java/br/net/mirante/singular/exemplos/ggtox/primariasimplificada/form/STypeParecer.java
+++ form/exemplos-form/src/main/java/br/net/mirante/singular/exemplos/ggtox/primariasimplificada/form/STypeParecer.java
@@ -13,7 +13,7 @@ public class STypeParecer extends STypePersistentComposite {
     protected void onLoadType(TypeBuilder tb) {
         super.onLoadType(tb);
         STypeHTML parecer = addField("parecer", STypeHTML.class);
-        parecer.asAtr().label("Parecer").required();
+        parecer.asAtr().label("Parecer");
     }
 
 }
