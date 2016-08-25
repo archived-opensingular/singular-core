d8c861ebad3d55f211c55fc1093e47acc71546fb
danilo.mesquita <danilofmesquita@gmail.com>
2016-08-25 17:25:41 -0300

    Adicionando campo para indicar o resultado da analise técnica

--- form/exemplos-form/src/main/java/br/net/mirante/singular/exemplos/ggtox/primariasimplificada/form/STypeParecer.java
+++ form/exemplos-form/src/main/java/br/net/mirante/singular/exemplos/ggtox/primariasimplificada/form/STypeParecer.java
@@ -4,6 +4,7 @@ import br.net.mirante.singular.form.SInfoType;
 import br.net.mirante.singular.form.TypeBuilder;
 import br.net.mirante.singular.form.persistence.STypePersistentComposite;
 import br.net.mirante.singular.form.type.core.STypeHTML;
+import br.net.mirante.singular.form.type.core.STypeString;
 
 
 @SInfoType(name = "STypeParecer", spackage = SPackagePeticaoPrimariaSimplificada.class)
@@ -12,8 +13,20 @@ public class STypeParecer extends STypePersistentComposite {
     @Override
     protected void onLoadType(TypeBuilder tb) {
         super.onLoadType(tb);
-        STypeHTML parecer = addField("parecer", STypeHTML.class);
-        parecer.asAtr().label("Parecer");
+
+        final STypeString resultadoAnalise = addField("resultadoAnalise", STypeString.class);
+        final STypeHTML   parecer          = addField("parecer", STypeHTML.class);
+
+        parecer.asAtr()
+                .label("Parecer")
+                .required();
+
+        resultadoAnalise.asAtr()
+                .label("Resultado da Análise")
+                .required();
+
+        resultadoAnalise.selectionOf("Deferir", "Indeferir");
+        resultadoAnalise.withRadioView();
     }
 
 }
