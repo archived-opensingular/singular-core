df2d816a065429a989f107f2d5ef47c06b7c0fa3
ronald.tetsuo <ronald.tetsuo@mirante.net.br>
2016-09-08 18:11:10 -0300

    [#128015541] Corrigir navegação via tab - desabilitando link de download quando ainda não há arquivo (foco em link invisível)

--- form/wicket/src/main/java/br/net/mirante/singular/form/wicket/mapper/attachment/DownloadLink.java
+++ form/wicket/src/main/java/br/net/mirante/singular/form/wicket/mapper/attachment/DownloadLink.java
@@ -1,13 +1,15 @@
 package br.net.mirante.singular.form.wicket.mapper.attachment;
 
-import br.net.mirante.singular.form.type.core.attachment.SIAttachment;
-import br.net.mirante.singular.util.wicket.model.IReadOnlyModel;
-import br.net.mirante.singular.util.wicket.util.WicketUtils;
+import static br.net.mirante.singular.util.wicket.util.Shortcuts.$b;
+import static br.net.mirante.singular.util.wicket.util.WicketUtils.$m;
+
+import org.apache.wicket.core.util.string.JavaScriptUtils;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.model.IModel;
 
-import static br.net.mirante.singular.util.wicket.util.Shortcuts.$b;
-import static br.net.mirante.singular.util.wicket.util.WicketUtils.$m;
+import br.net.mirante.singular.form.type.core.attachment.SIAttachment;
+import br.net.mirante.singular.util.wicket.model.IReadOnlyModel;
+import br.net.mirante.singular.util.wicket.util.WicketUtils;
 
 /**
  * Classe de link para utilização em conjunto dom {@link DownloadSupportedBehavior}
@@ -28,20 +30,30 @@ public class DownloadLink extends Link<Void> {
     @Override
     protected void onInitialize() {
         super.onInitialize();
-        this.add($b.attr("onclick", (IReadOnlyModel<String>) () -> "DownloadSupportedBehavior.ajaxDownload(" +
-            "'" + downloadSupportedBehaviour.getUrl() + "'," +
-            "'" + model.getObject().getFileId() + "'," +
-            "'" + model.getObject().getFileName() + "'" +
-            ");" +
-            "return false;"));
+        this.add($b.attr("onclick",
+            (IReadOnlyModel<String>) () -> "DownloadSupportedBehavior.ajaxDownload(" +
+                jsStringOrNull(downloadSupportedBehaviour.getUrl()) + "," +
+                jsStringOrNull(model.getObject().getFileId()) + "," +
+                jsStringOrNull(model.getObject().getFileName()) +
+                ");" +
+                "return false;"));
         this.setBody($m.property(model, "fileName"));
         add(WicketUtils.$b.attr("title", $m.ofValue(model.getObject().getFileName())));
         add($b.attr("target", "_blank"));
     }
 
+    private static String jsStringOrNull(String s) {
+        return (s == null) ? "null" : "'" + JavaScriptUtils.escapeQuotes(s) + "'";
+    }
+
     @Override
     protected void onConfigure() {
         super.onConfigure();
+        setEnabled(isFileAssigned());
+    }
+
+    protected boolean isFileAssigned() {
+        return (model.getObject() != null) && (model.getObject().getFileId() != null);
     }
 
     @Override
