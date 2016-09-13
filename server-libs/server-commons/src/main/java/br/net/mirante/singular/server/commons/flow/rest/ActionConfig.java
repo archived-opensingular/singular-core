face29e55793329026fb860552cfdee5f8c5bba4
Vinicius Uriel <viniciusuriel@gmail.com>
2016-09-13 10:45:04 -0300

    [SINGULAR-PET-SERVER] corrigindo permissão de visualizar do analista ggtox

--- server-libs/server-commons/src/main/java/br/net/mirante/singular/server/commons/flow/rest/ActionConfig.java
+++ server-libs/server-commons/src/main/java/br/net/mirante/singular/server/commons/flow/rest/ActionConfig.java
@@ -5,34 +5,34 @@
 
 package br.net.mirante.singular.server.commons.flow.rest;
 
+import br.net.mirante.singular.commons.util.Loggable;
+import br.net.mirante.singular.flow.core.property.MetaDataRef;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import br.net.mirante.singular.commons.util.Loggable;
-import br.net.mirante.singular.flow.core.property.MetaDataRef;
 import static br.net.mirante.singular.server.commons.flow.action.DefaultActions.ACTION_RELOCATE;
 
 /**
  * Classe responsável por guardar a configuração de ações
  * disponível para o módulo
  */
-public class ActionConfig implements Loggable{
+public class ActionConfig implements Loggable {
 
     public static final MetaDataRef<ActionConfig> KEY = new MetaDataRef<>(ActionConfig.class.getName(), ActionConfig.class);
 
     private static final Map<ActionDefinition, Class<? extends IController>> MAP_DEFAULT_ACTIONS;
 
-    private List<ActionDefinition> defaultActions;
-
-    private Map<ActionDefinition, Class<? extends IController>> customActions;
-
     static {
         MAP_DEFAULT_ACTIONS = new HashMap<>();
         MAP_DEFAULT_ACTIONS.put(ACTION_RELOCATE, AtribuirController.class);
     }
+
+    private List<ActionDefinition> defaultActions;
+    private Map<ActionDefinition, Class<? extends IController>> customActions;
 
     public ActionConfig() {
         defaultActions = new ArrayList<>();
@@ -61,15 +61,21 @@ public class ActionConfig implements Loggable{
         Class<? extends IController> controllerClass = customActions.get(name);
 
         if (controllerClass == null) {
-            controllerClass = MAP_DEFAULT_ACTIONS.get(name);
+            controllerClass = MAP_DEFAULT_ACTIONS
+                    .entrySet()
+                    .stream()
+                    .filter(e -> e.getKey().getName().equals(name))
+                    .map(entry -> entry.getValue())
+                    .findFirst()
+                    .orElse(null);
         }
 
         return controllerClass;
     }
 
     public boolean containsAction(String name) {
-        boolean contains =  customActions.keySet().stream().anyMatch( actionDefinition -> actionDefinition.getName().equals(name)) || defaultActions.stream().anyMatch( actionDefinition -> actionDefinition.getName().equals(name));
-        if (!contains){
+        boolean contains = customActions.keySet().stream().anyMatch(actionDefinition -> actionDefinition.getName().equals(name)) || defaultActions.stream().anyMatch(actionDefinition -> actionDefinition.getName().equals(name));
+        if (!contains) {
             getLogger().debug(String.format("Action '%s' foi removido pois não está definida para esse fluxo.", name));
         }
         return contains;
