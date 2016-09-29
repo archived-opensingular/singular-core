732a1f304151036c2a405e4a454645bc298275d5
Lucas Lopes <lucas.lopes@mirante.net.br>
2016-09-29 15:14:55 -0300

    Implementação do serviço de consulta de Localidade

--- form/core/src/main/java/br/net/mirante/singular/form/provider/Provider.java
+++ form/core/src/main/java/br/net/mirante/singular/form/provider/Provider.java
@@ -1,9 +1,9 @@
 package br.net.mirante.singular.form.provider;
 
-import br.net.mirante.singular.form.SInstance;
-
 import java.io.Serializable;
 import java.util.List;
+
+import br.net.mirante.singular.form.SInstance;
 
 public interface Provider<E extends Serializable, S extends SInstance> extends Serializable {
 
