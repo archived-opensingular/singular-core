527b4b1e5f7d6ce8198363acb622d1f9fa9540fb
Vinicius Nunes <viniciusuriel@gmail.com>
2016-10-11 14:29:40 -0300

    versão 1.5.2

--- form/core/src/test/java/org/opensingular/form/type/core/attachment/BaseAttachmentPersistenceFilesTest.java
+++ form/core/src/test/java/org/opensingular/form/type/core/attachment/BaseAttachmentPersistenceFilesTest.java
@@ -9,6 +9,7 @@ import org.junit.runners.Parameterized.Parameters;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
@@ -39,13 +40,13 @@ abstract public class BaseAttachmentPersistenceFilesTest {
     public static Iterable<Object[]> data1() {
         return Arrays.asList(new Object[][]{
                 {new byte[0], "da39a3ee5e6b4b0d3255bfef95601890afd80709"},
-                {"i".getBytes(), "042dc4512fa3d391c5170cf3aa61e6a638f84342"}, //Caso importante pelo hash começa com zero
-                {"np".getBytes(), "003fffd5649fc27c0fc0d15a402a4fe5b0444ce7"},
-                {"1234".getBytes(), "7110eda4d09e062aa5e4a390b0a572ac0d2c0220"},
+                {"i".getBytes(Charset.forName("UTF-8")), "042dc4512fa3d391c5170cf3aa61e6a638f84342"}, //Caso importante pelo hash começa com zero
+                {"np".getBytes(Charset.forName("UTF-8")), "003fffd5649fc27c0fc0d15a402a4fe5b0444ce7"},
+                {"1234".getBytes(Charset.forName("UTF-8")), "7110eda4d09e062aa5e4a390b0a572ac0d2c0220"},
                 {"TesteTesteTeste".getBytes(), "ceecae2e6034de45a8303a31e9e96adb37c2443f" },
-            { "MiranteMiranteMiranteMirante".getBytes(), "79244437e10faf670b335edc3e3aada33e6790f8" },
-            { "sha1 this string".getBytes(), "cf23df2207d99a74fbe169e3eba035e633b65d94" },
-            { "The quick brown fox jumps over the lazy dog".getBytes(), "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12" },
+            { "MiranteMiranteMiranteMirante".getBytes(Charset.forName("UTF-8")), "79244437e10faf670b335edc3e3aada33e6790f8" },
+            { "sha1 this string".getBytes(Charset.forName("UTF-8")), "cf23df2207d99a74fbe169e3eba035e633b65d94" },
+            { "The quick brown fox jumps over the lazy dog".getBytes(Charset.forName("UTF-8")), "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12" },
             { newByteArray(10 * 1024 * 1024, (byte) 0), "8c206a1a87599f532ce68675536f0b1546900d7a"},
             { newByteArray(100 * 1024 * 1024, (byte) 1), "9abe36b18e1871f67d581f2d1f4b6a9036dcc23f"}
         });
