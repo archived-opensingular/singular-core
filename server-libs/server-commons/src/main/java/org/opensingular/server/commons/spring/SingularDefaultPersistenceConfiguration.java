c7d3b5ea7ddd3b1fa45355cd507d8d3036987693
Vinicius Uriel <viniciusuriel@gmail.com>
2016-10-27 10:46:48 -0200

    CACHE DE SEGUNDO NÍVEL PARA ENTIDADES DE DEFINIÇÃO DO SINGULAR FLOW E FORM

--- server-libs/server-commons/src/main/java/org/opensingular/server/commons/spring/SingularDefaultPersistenceConfiguration.java
+++ server-libs/server-commons/src/main/java/org/opensingular/server/commons/spring/SingularDefaultPersistenceConfiguration.java
@@ -175,12 +175,17 @@ public abstract class SingularDefaultPersistenceConfiguration {
     protected Properties hibernateProperties() {
         final Properties hibernateProperties = new Properties();
         hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect");
-        hibernateProperties.put("hibernate.connection.isolation", "2");
-        hibernateProperties.put("hibernate.jdbc.batch_size", "30");
-        hibernateProperties.put("hibernate.show_sql", "false");
-        hibernateProperties.put("hibernate.format_sql", "true");
-        hibernateProperties.put("hibernate.cache.use_second_level_cache", "false");
-        hibernateProperties.put("hibernate.jdbc.use_get_generated_keys", "true");
+        hibernateProperties.put("hibernate.connection.isolation", 2);
+        hibernateProperties.put("hibernate.jdbc.batch_size", 30);
+        hibernateProperties.put("hibernate.show_sql", false);
+        hibernateProperties.put("hibernate.format_sql", true);
+        hibernateProperties.put("hibernate.enable_lazy_load_no_trans", true);
+        hibernateProperties.put("hibernate.jdbc.use_get_generated_keys", true);
+        hibernateProperties.put("hibernate.cache.use_second_level_cache", true);
+        hibernateProperties.put("hibernate.cache.use_query_cache", true);
+        /*não utilizar a singleton region factory para não conflitar com o cache do singular-server */
+        hibernateProperties.put("net.sf.ehcache.configurationResourceName","/default-singular-ehcache.xml");
+        hibernateProperties.put("hibernate.cache.region.factory_class", "org.hibernate.cache.ehcache.EhCacheRegionFactory");
         return hibernateProperties;
     }
 
