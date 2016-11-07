46a198d066776b2e5fec6a3c4fd4b4199053b276
Vinicius Nunes <viniciusuriel@gmail.com>
2016-11-07 14:13:58 -0200

    corrigindo null pointer

--- form/service/src/main/java/org/opensingular/form/service/FormService.java
+++ form/service/src/main/java/org/opensingular/form/service/FormService.java
@@ -16,7 +16,6 @@
 package org.opensingular.form.service;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.opensingular.form.SFormUtil;
 import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.StringUtils;
 import org.opensingular.form.SIList;
@@ -26,6 +25,12 @@ import org.opensingular.form.document.RefType;
 import org.opensingular.form.document.SDocumentFactory;
 import org.opensingular.form.internal.xml.MElement;
 import org.opensingular.form.io.MformPersistenciaXML;
+import org.opensingular.form.persistence.AbstractBasicFormPersistence;
+import org.opensingular.form.persistence.AnnotationKey;
+import org.opensingular.form.persistence.FormKey;
+import org.opensingular.form.persistence.FormKeyLong;
+import org.opensingular.form.persistence.SPackageFormPersistence;
+import org.opensingular.form.persistence.SingularFormPersistenceException;
 import org.opensingular.form.persistence.dao.FormAnnotationDAO;
 import org.opensingular.form.persistence.dao.FormAnnotationVersionDAO;
 import org.opensingular.form.persistence.dao.FormDAO;
@@ -37,20 +42,16 @@ import org.opensingular.form.persistence.entity.FormAnnotationVersionEntity;
 import org.opensingular.form.persistence.entity.FormEntity;
 import org.opensingular.form.persistence.entity.FormTypeEntity;
 import org.opensingular.form.persistence.entity.FormVersionEntity;
-import org.opensingular.form.persistence.AbstractBasicFormPersistence;
-import org.opensingular.form.persistence.AnnotationKey;
-import org.opensingular.form.persistence.FormKey;
-import org.opensingular.form.persistence.FormKeyLong;
-import org.opensingular.form.persistence.SPackageFormPersistence;
-import org.opensingular.form.persistence.SingularFormPersistenceException;
 import org.opensingular.form.type.core.annotation.AtrAnnotation;
 import org.opensingular.form.type.core.annotation.SIAnnotation;
-import org.apache.commons.lang3.NotImplementedException;
-import org.apache.commons.lang3.StringUtils;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Transactional
