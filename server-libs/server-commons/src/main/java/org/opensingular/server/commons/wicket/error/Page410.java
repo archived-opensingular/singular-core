1a4a74196b36aeefe6a6494e49c3b0ff6382cbef
Danilo Mesquita <danilofmesquita@gmail.com>
2016-12-12 13:38:00 -0200

    adicionando erro de pagina expirada

---
+++ server-libs/server-commons/src/main/java/org/opensingular/server/commons/wicket/error/Page410.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2016 Singular Studios (a.k.a Atom Tecnologia) - www.opensingular.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensingular.server.commons.wicket.error;
+
+import org.apache.wicket.devutils.stateless.StatelessComponent;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.opensingular.server.commons.wicket.view.template.Content;
+import org.opensingular.server.commons.wicket.view.template.Template;
+import org.wicketstuff.annotation.mount.MountPath;
+
+@StatelessComponent
+@MountPath("public/error/410")
+public class Page410 extends Template {
+
+    public Page410() {
+    }
+
+    @Override
+    protected Content getContent(String id) {
+        return new Page410Content(id);
+    }
+
+    @Override
+    protected WebMarkupContainer configureHeader(String id) {
+        return (WebMarkupContainer) new WebMarkupContainer(id).setVisible(false);
+    }
+
+    @Override
+    protected boolean withMenu() {
+        return false;
+    }
+
+    @Override
+    protected boolean withTopAction() {
+        return false;
+    }
+
+    @Override
+    protected boolean withSideBar() {
+        return false;
+    }
+
+}
