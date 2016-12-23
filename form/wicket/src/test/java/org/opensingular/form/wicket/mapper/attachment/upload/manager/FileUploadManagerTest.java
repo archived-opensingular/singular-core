abf79efa2085b6614abb4fc7ebb805b41d9a6724
Danilo Mesquita <danilofmesquita@gmail.com>
2016-12-23 10:44:47 -0200

    FileUploadServlet -> adicionando teste para FileUploadManager#createFile

--- form/wicket/src/test/java/org/opensingular/form/wicket/mapper/attachment/upload/manager/FileUploadManagerTest.java
+++ form/wicket/src/test/java/org/opensingular/form/wicket/mapper/attachment/upload/manager/FileUploadManagerTest.java
@@ -5,21 +5,25 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.opensingular.form.type.core.attachment.IAttachmentPersistenceHandler;
 import org.opensingular.form.type.core.attachment.IAttachmentRef;
 import org.opensingular.form.wicket.mapper.attachment.upload.AttachmentKey;
 import org.opensingular.form.wicket.mapper.attachment.upload.UploadPathHandler;
 import org.opensingular.form.wicket.mapper.attachment.upload.factory.AttachmentKeyFactory;
 import org.opensingular.form.wicket.mapper.attachment.upload.info.FileUploadInfo;
 import org.opensingular.form.wicket.mapper.attachment.upload.info.FileUploadInfoRepository;
+import org.opensingular.form.wicket.mapper.attachment.upload.info.UploadInfo;
 import org.opensingular.form.wicket.mapper.attachment.upload.info.UploadInfoRepository;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
 import java.util.function.Function;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.eq;
@@ -32,19 +36,19 @@ import static org.mockito.Mockito.when;
 public class FileUploadManagerTest {
 
     @Mock
-    AttachmentKeyFactory attachmentKeyFactory = new AttachmentKeyFactory();
+    private AttachmentKeyFactory attachmentKeyFactory;
 
     @Mock
-    UploadInfoRepository uploadInfoRepository = new UploadInfoRepository();
+    private UploadInfoRepository uploadInfoRepository;
 
     @Mock
-    FileUploadInfoRepository fileUploadInfoRepository = new FileUploadInfoRepository();
+    private FileUploadInfoRepository fileUploadInfoRepository;
 
     @Mock
-    UploadPathHandler uploadPathHandler;
+    private UploadPathHandler uploadPathHandler;
 
     @InjectMocks
-    FileUploadManager fileUploadManager;
+    private FileUploadManager fileUploadManager;
 
     @Test
     public void testCreateUpload() {
@@ -60,7 +64,7 @@ public class FileUploadManagerTest {
         MockCallback   callback       = spy(new MockCallback());
         FileUploadInfo fileUploadInfo = mock(FileUploadInfo.class);
         IAttachmentRef attachmentRef  = mock(IAttachmentRef.class);
-        Path           path           = Files.createTempFile("mock", "file");
+        Path           path           = createTestPath().resolve(key);
 
         when(uploadPathHandler.getLocalFilePath(eq(fileUploadInfo))).thenReturn(path);
         when(attachmentKeyFactory.get()).thenReturn(attachmentKey);
@@ -74,6 +78,8 @@ public class FileUploadManagerTest {
 
         verify(callback).apply(eq(attachmentRef));
         verify(fileUploadInfoRepository).remove(eq(fileUploadInfo));
+
+        path.toFile().deleteOnExit();
     }
 
     public static class MockCallback implements Function<IAttachmentRef, Boolean> {
@@ -81,6 +87,37 @@ public class FileUploadManagerTest {
         public Boolean apply(IAttachmentRef attachmentRef) {
             return true;
         }
+    }
+
+    @Test
+    public void testCreateFile() throws IOException {
+
+        String                        key           = "123456";
+        AttachmentKey                 attachmentKey = new AttachmentKey(key);
+        UploadInfo                    ui            = mock(UploadInfo.class);
+        IAttachmentPersistenceHandler handler       = mock(IAttachmentPersistenceHandler.class);
+        InputStream                   in            = mock(InputStream.class);
+        Path                          path          = createTestPath().resolve(key);
+
+        when(ui.getPersistenceHandlerSupplier()).thenReturn(() -> handler);
+        when(uploadPathHandler.getLocalFilePath(eq(key))).thenReturn(path);
+        when(attachmentKeyFactory.get()).thenReturn(attachmentKey);
+
+        String         fileName = "my_document.pdf";
+        FileUploadInfo info     = fileUploadManager.createFile(ui, fileName, in);
+
+        assertNotNull(info);
+
+        verify(fileUploadInfoRepository).add(eq(info));
+        verify(handler).addAttachment(path.toFile(), Files.size(path), fileName);
+
+        path.toFile().deleteOnExit();
+    }
+
+    public Path createTestPath() throws IOException {
+        Path tempDirectory = Files.createTempDirectory(this.getClass().getSimpleName() + "_");
+        tempDirectory.toFile().deleteOnExit();
+        return tempDirectory;
     }
 
 }
