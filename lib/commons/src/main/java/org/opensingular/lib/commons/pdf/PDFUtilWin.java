a06b8a323bb21fd9f19ec4496406a9d9443a0a55
Daniel Bordin <daniel@mirante.net.br>
2017-01-22 15:24:30 -0200

    [FORM-CORE] Refactoring geral do PDFUtil e inclusão de JUnit (para windows): código antigo confuso, repetido, com tratamento de erro incorreto (mascaramentos) e deixava um infinidade de arquivos temporários de lixo sem apagar

--- lib/commons/src/main/java/org/opensingular/lib/commons/pdf/PDFUtilWin.java
+++ lib/commons/src/main/java/org/opensingular/lib/commons/pdf/PDFUtilWin.java
@@ -16,13 +16,10 @@
 
 package org.opensingular.lib.commons.pdf;
 
-import org.opensingular.lib.commons.util.TempFileUtils;
-
+import javax.annotation.Nonnull;
 import java.io.*;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 /**
  * Classe utilitária para a manipulação de PDF's no Windows.
@@ -30,216 +27,49 @@ import java.util.UUID;
  * {@link PDFUtil} com
  * as equivalentes para o sistema Windows.
  */
-public class PDFUtilWin extends PDFUtil {
+final class PDFUtilWin extends PDFUtil {
 
-    /**
-     * Instancia um novo objeto do tipo PDFUtilWin.
-     */
-    private PDFUtilWin() {
-    }
-
-    /**
-     * Retorna o valor atual do atributo {@link #instance}.
-     *
-     * @return O valor atual do atributo.
-     */
-    public static PDFUtil getInstance() {
-        if (instance == null) {
-            instance = new PDFUtilWin();
-        }
-        return instance;
-    }
-
-    /**
-     * Dividi as páginas de um PDF ao meio, gerando duas páginas para cada.
-     *
-     * @param pdf o PDF.
-     * @return O arquivo PDF gerado.
-     */
     @Override
-    public File splitPDF(File pdf) throws IOException, InterruptedException {
-        if (getWkhtml2pdfHomeOpt() == null) {
-            return null;
-        } else if (pdf == null || !pdf.exists()) {
-            getLogger().error("splitPDF: PDF file not found");
-            return null;
-        }
-
-        File tempFolder = pdf.getParentFile();
-        File pdfFile    = new File(tempFolder, "splited.pdf");
-
-        List<String> commandAndArgs = new ArrayList<>(0);
-        commandAndArgs.add(getExecFile(null, "runner.exe"));
-        commandAndArgs.add(getExecFile(null, "pdfsplit"));
-        commandAndArgs.add("-p2x1a4");
-        commandAndArgs.add(pdf.getAbsolutePath());
-        commandAndArgs.add(pdfFile.getAbsolutePath());
-
-        getLogger().info(commandAndArgs.toString());
-
-        ProcessBuilder pb      = new ProcessBuilder(commandAndArgs);
-        Process        process = pb.start();
-        boolean success = runProcess(process);
-        if (success && pdfFile.exists()) {
-            return pdfFile;
-        }
-        return null;
+    protected String fixExecutableName(String executable) {
+        return executable + ".exe";
     }
 
-    private boolean runProcess(Process process) throws InterruptedException {
-        StreamGobbler outReader = new StreamGobbler(process.getInputStream(), false);
-        StreamGobbler errReader = new StreamGobbler(process.getErrorStream(), true);
-
-        outReader.start();
-        errReader.start();
-
-        return process.waitFor() == 0;
-    }
-
-    /**
-     * Converte o código HTML em um arquivo PDF com o cabeçalho e rodapé especificados.
-     *
-     * @param rawHtml       o código HTML.
-     * @param rawHeader     o código HTML do cabeçalho.
-     * @param rawFooter     o código HTML do rodapé.
-     * @param additionalConfig configurações adicionais.
-     * @return O arquivo PDF gerado.
-     * @throws IOException          Caso ocorra um problema de IO.
-     * @throws InterruptedException Caso ocorra um problema de sincronismo.
-     */
     @Override
-    public File convertHTML2PDF(String rawHtml, String rawHeader, String rawFooter, List<String> additionalConfig)
-            throws IOException, InterruptedException {
-        if (getWkhtml2pdfHomeOpt() == null) {
-            return null;
-        }
-
-        final String html   = safeWrapHtml(rawHtml);
-        final String header = safeWrapHtml(rawHeader);
-        final String footer = safeWrapHtml(rawFooter);
-
-        File tempLock   = File.createTempFile("SINGULAR-", UUID.randomUUID().toString());
-        File tempFolder = new File(tempLock.getParentFile(), tempLock.getName().concat("-DIR"));
-        if (!tempFolder.mkdir()) {
-            getLogger().error("convertHTML2PDF: temp folder not found");
-            return null;
-        }
-
-        File htmlFile = new File(tempFolder, "temp.html");
-        File pdfFile  = new File(tempFolder, "temp.pdf");
-        File jarFile  = new File(tempFolder, "temp.jar");
-
-        TempFileUtils.deleteOrException(htmlFile, getClass());
-
-        if (! htmlFile.createNewFile()) {
-            return null;
-        }
-        writeToFile(htmlFile, html);
-
-        List<String> commandAndArgs = new ArrayList<>(0);
-        commandAndArgs.add(getExecFile("bin", "wkhtmltopdf.exe"));
-
-        if (additionalConfig != null) {
-            commandAndArgs.addAll(additionalConfig);
-        } else {
-            addDefaultPDFCommandArgs(commandAndArgs);
-        }
-
-        if (header != null) {
-            File headerFile = new File(tempFolder, "header.html");
-            writeToFile(headerFile, header);
-            commandAndArgs.add("--header-html");
-            commandAndArgs.add(headerFile.getAbsolutePath());
-            addDefaultHeaderCommandArgs(commandAndArgs);
-        }
-
-        if (footer != null) {
-            File footerFile = new File(tempFolder, "footer.html");
-            writeToFile(footerFile, footer);
-            commandAndArgs.add("--footer-html");
-            commandAndArgs.add(footerFile.getAbsolutePath());
-            addDefaultFooterCommandArgs(commandAndArgs);
-        }
-
-        commandAndArgs.add("--cookie-jar");
-        commandAndArgs.add(jarFile.getAbsolutePath());
-        commandAndArgs.add(htmlFile.getAbsolutePath());
-        commandAndArgs.add(pdfFile.getAbsolutePath());
-
-        ProcessBuilder pb = new ProcessBuilder(commandAndArgs);
-        pb.environment().put("LD_LIBRARY_PATH", getWkhtml2pdfHome().getAbsolutePath());
-        Process process = pb.start();
-
-        boolean success = runProcess(process);
-        if (success && pdfFile.exists()) {
-            return pdfFile;
-        }
-        return null;
-    }
-
-    private void writeToFile(File destination, String content) throws IOException {
+    protected void writeToFile(File destination, String content) throws SingularPDFException {
         try (FileOutputStream fos = new FileOutputStream(destination);
              Writer fw = new OutputStreamWriter(fos, Charset.forName("UTF-8").newEncoder())){
             fw.write(content);
+        } catch(Exception e) {
+            throw new SingularPDFException("Erro escrevendo conteúdo no arquivo" + destination.getAbsolutePath(), e);
         }
     }
 
-    /**
-     * Converte o código HTML em um arquivo PNG.
-     *
-     * @param html             o código HTML.
-     * @param additionalConfig configurações adicionais.
-     * @return O arquivo PDF gerado.
-     * @throws IOException          Caso ocorra um problema de IO.
-     * @throws InterruptedException Caso ocorra um problema de sincronismo.
-     */
     @Override
-    public File convertHTML2PNG(String html, List<String> additionalConfig) throws IOException, InterruptedException {
-        if (getWkhtml2pdfHomeOpt() == null) {
-            return null;
-        }
+    protected
+    @Nonnull
+    File runProcess(@Nonnull List<String> commandAndArgs, @Nonnull File expectedFile) throws SingularPDFException {
+        getLogger().info(commandAndArgs.toString());
 
-        File tempLock   = File.createTempFile("SINGULAR-", UUID.randomUUID().toString());
-        File tempFolder = new File(tempLock.getParentFile(), tempLock.getName().concat("-DIR"));
-        if (!tempFolder.mkdir()) {
-            getLogger().error("convertHTML2PNG: temp folder not found");
-            return null;
-        }
-
-        File htmlFile = new File(tempFolder, "temp.html");
-        File pngFile  = new File(tempFolder, "temp.png");
-        File jarFile  = new File(tempFolder, "temp.jar");
-
-        TempFileUtils.deleteOrException(htmlFile, getClass());
-
-        if (htmlFile.createNewFile()) {
-            writeToFile(htmlFile, html);
-
-            List<String> commandAndArgs = new ArrayList<>(0);
-            commandAndArgs.add(getExecFile("bin","wkhtmltoimage.exe"));
-
-            if (additionalConfig != null) {
-                commandAndArgs.addAll(additionalConfig);
-            } else {
-                addDefaultPNGCommandArgs(commandAndArgs);
-            }
-
-            commandAndArgs.add("--cookie-jar");
-            commandAndArgs.add(jarFile.getAbsolutePath());
-            commandAndArgs.add(htmlFile.getAbsolutePath());
-            commandAndArgs.add(pngFile.getAbsolutePath());
-
-            ProcessBuilder pb = new ProcessBuilder(commandAndArgs);
-            pb.environment().put("LD_LIBRARY_PATH", getWkhtml2pdfHome().getAbsolutePath());
+        ProcessBuilder pb = new ProcessBuilder(commandAndArgs);
+        pb.environment().put("LD_LIBRARY_PATH", getWkhtml2pdfHome().getAbsolutePath());
+        try {
             Process process = pb.start();
 
-            boolean success = runProcess(process);
-            if (success && pngFile.exists()) {
-                return pngFile;
-            }
-        }
+            StreamGobbler outReader = new StreamGobbler(process.getInputStream(), false);
+            StreamGobbler errReader = new StreamGobbler(process.getErrorStream(), true);
 
-        return null;
+            outReader.start();
+            errReader.start();
+
+            boolean success = (process.waitFor() == 0);
+
+            if (success && expectedFile.exists()) {
+                return expectedFile;
+            }
+            throw new SingularPDFException("Arquivo não foi gerado " + expectedFile.getAbsolutePath());
+        } catch (IOException | InterruptedException e) {
+            throw new SingularPDFException(e);
+        }
     }
 
     /**
---
+++ lib/commons/src/main/java/org/opensingular/lib/commons/pdf/SingularPDFException.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2016 Singular Studios (a.k.a Atom Tecnologia) - www.opensingular.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensingular.lib.commons.pdf;
+
+import org.opensingular.lib.commons.base.SingularException;
+
+/**
+ * Exception associadas a geração e manipulação de PDF.
+ *
+ * @author Daniel C. Bordin on 21/01/2017.
+ */
+public class SingularPDFException extends SingularException {
+
+    /**
+     * Constructs a new <code>SingularException</code> without specified
+     * detail message.
+     */
+    protected SingularPDFException() {
+        super();
+    }
+
+    /**
+     * Constructs a new <code>SingularException</code> with specified
+     * detail message.
+     *
+     * @param msg the error message
+     */
+    protected SingularPDFException(String msg) {
+        super(msg);
+    }
+
+    /**
+     * Constructs a new <code>SingularException</code> with specified
+     * cause <code>Throwable</code>.
+     *
+     * @param cause the exception or error that caused this exception to be
+     *              thrown
+     */
+    protected SingularPDFException(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Constructs a new <code>SingularException</code> with specified
+     * detail message and cause <code>Throwable</code>.
+     *
+     * @param msg   the error message
+     * @param cause the exception or error that caused this exception to be
+     *              thrown
+     */
+    protected SingularPDFException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
+
