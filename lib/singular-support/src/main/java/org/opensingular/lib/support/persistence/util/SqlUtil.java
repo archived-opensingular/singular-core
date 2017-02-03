a3121c50097a7dcab51af6a9583be39333322353
Vinicius Nunes <viniciusuriel@gmail.com>
2017-02-03 09:07:03 -0200

    método utilitário para verificar se um determinado schema é o schema atual do Singular

--- lib/singular-support/src/main/java/org/opensingular/lib/support/persistence/util/SqlUtil.java
+++ lib/singular-support/src/main/java/org/opensingular/lib/support/persistence/util/SqlUtil.java
@@ -1,13 +1,39 @@
+/*
+ * Copyright (C) 2016 Singular Studios (a.k.a Atom Tecnologia) - www.opensingular.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.opensingular.lib.support.persistence.util;
 
 import org.opensingular.lib.commons.base.SingularProperties;
 
 import static org.opensingular.lib.commons.base.SingularProperties.CUSTOM_SCHEMA_NAME;
 
+/**
+ * Utility class for sql processing.
+ */
 public class SqlUtil {
 
     private SqlUtil() {}
 
+    /**
+     * Replaces the default Singular database schema name with
+     * the name configured through {@value SingularProperties#CUSTOM_SCHEMA_NAME} singular property
+     * @param sql - an sql query
+     * @return
+     *  return the {@param sql} with the schema name replaced
+     *
+     */
     public static String replaceSchemaName(String sql) {
         if (SingularProperties.get().containsKey(CUSTOM_SCHEMA_NAME)) {
             String customSchema = SingularProperties.get().getProperty(CUSTOM_SCHEMA_NAME);
@@ -15,6 +41,16 @@ public class SqlUtil {
         } else {
             return sql;
         }
+    }
+
+    /**
+     * Test if the given {@param schemaName} is the current singular database schema
+     * @param schemaName
+     * @return
+     *  true if {@param schemaName} is the current singular schema, false otherwise
+     */
+    public static boolean isSingularSchema(String schemaName){
+        return replaceSchemaName(Constants.SCHEMA).equals(schemaName);
     }
 
 }
