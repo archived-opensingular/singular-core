23cc0d6c6c084e7b8c2e3b7901bdf9a1043d6f11
Danilo Mesquita <danilofmesquita@gmail.com>
2017-02-16 17:26:23 -0200

    [teste] evoluindo SearchModalMapperTest

--- form/wicket/src/test/java/org/opensingular/form/wicket/mapper/search/SearchModalMapperTest.java
+++ form/wicket/src/test/java/org/opensingular/form/wicket/mapper/search/SearchModalMapperTest.java
@@ -1,5 +1,9 @@
 package org.opensingular.form.wicket.mapper.search;
 
+import org.apache.wicket.markup.html.form.Button;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.opensingular.form.SInstance;
 import org.opensingular.form.STypeComposite;
 import org.opensingular.form.converter.ValueToSICompositeConverter;
@@ -10,65 +14,75 @@ import org.opensingular.form.type.core.STypeBoolean;
 import org.opensingular.form.type.core.STypeString;
 import org.opensingular.form.util.transformer.Value;
 import org.opensingular.form.view.SViewSearchModal;
-import org.opensingular.form.wicket.helpers.SingularFormBaseTest;
+import org.opensingular.form.wicket.helpers.SingularDummyFormPageTester;
 import org.opensingular.lib.wicket.util.datatable.BSDataTable;
-import org.apache.commons.lang3.tuple.Triple;
-import org.apache.wicket.markup.html.form.Button;
-import org.junit.Assert;
-import org.junit.Test;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
-public class SearchModalMapperTest extends SingularFormBaseTest {
+public class SearchModalMapperTest {
 
-    private List<Triple<String, String, Boolean>> carros    = new ArrayList<>();
-    private int                                   PAGE_SIZE = 5;
+    private static final String BRAND      = "brand";
+    private static final String MODEL      = "model";
+    private static final String MULTIMEDIA = "multimedia";
 
-    @Override
+    private static int PAGE_SIZE = 5;
+
+    private SingularDummyFormPageTester tester;
+
+    @Before
     public void setUp() {
-        super.setUp();
-        carros.add(Triple.of("Ford", "Focus", true));
-        carros.add(Triple.of("Ford", "Fusion", true));
-        carros.add(Triple.of("Ford", "Fiesta", true));
-        carros.add(Triple.of("Ford", "Ká", true));
-        carros.add(Triple.of("Fiat", "Palio", false));
-        carros.add(Triple.of("Fiat", "Mobi", true));
-        carros.add(Triple.of("Fiat", "Toro", true));
-        carros.add(Triple.of("Chevrolet", "Celta", false));
-        carros.add(Triple.of("Chevrolet", "Onix", true));
-        carros.add(Triple.of("Chevrolet", "Cobalt", true));
-        carros.add(Triple.of("Chevrolet", "Cruze", true));
-        carros.add(Triple.of("Renault", "Sandeiro", true));
-        carros.add(Triple.of("Renault", "Clio", false));
+        tester = new SingularDummyFormPageTester();
+        tester.getDummyPage().setTypeBuilder(SearchModalMapperTest::buildBaseType);
     }
 
-    @Override
-    protected void buildBaseType(STypeComposite<?> baseType) {
-        final STypeComposite<?> carro         = baseType.addFieldComposite("carro");
-        final STypeString       marca         = carro.addFieldString("marca");
-        final STypeString       modelo        = carro.addFieldString("modelo");
-        final STypeBoolean      conectividade = carro.addFieldBoolean("conectividade");
-        carro.withView(new SViewSearchModal(), (Consumer<SViewSearchModal>) view -> {
-            view.withPageSize(PAGE_SIZE);
-        });
-        carro.asAtrProvider().filteredProvider(new FilteredPagedProvider<Triple>() {
+    private static List<Car> carros() {
+        return new CarsBuilder()
+                .addCarWithMultimedia("Ford", "Focus")
+                .addCarWithMultimedia("Ford", "Fusion")
+                .addCarWithMultimedia("Ford", "Fiesta")
+                .addCarWithMultimedia("Ford", "Ká")
+                .addCarWithMultimedia("Fiat", "Mobi")
+                .addCarWithMultimedia("Fiat", "Toro")
+                .addCarWithMultimedia("Chevrolet", "Onix")
+                .addCarWithMultimedia("Chevrolet", "Cobalt")
+                .addCarWithMultimedia("Chevrolet", "Cruze")
+                .addCarWithMultimedia("Renault", "Sandeiro")
+                .addCarWhitoutMultimedia("Chevrolet", "Celta")
+                .addCarWhitoutMultimedia("Fiat", "Palio")
+                .addCarWhitoutMultimedia("Renault", "Clio")
+                .build();
+    }
+
+    protected static void buildBaseType(STypeComposite<?> baseType) {
+
+        final STypeComposite<?> car = baseType.addFieldComposite("car");
+
+        final STypeString  brand      = car.addFieldString(BRAND);
+        final STypeString  model      = car.addFieldString(MODEL);
+        final STypeBoolean multimedia = car.addFieldBoolean(MULTIMEDIA);
+
+        car.withView(new SViewSearchModal(), (Consumer<SViewSearchModal>) view -> view.withPageSize(PAGE_SIZE));
+
+        car.asAtrProvider().filteredProvider(new FilteredPagedProvider<Car>() {
 
             @Override
             public void configureProvider(Config cfg) {
-                cfg.getFilter().addFieldString("marca");
-                cfg.getFilter().addFieldString("modelo");
-                cfg.getFilter().addFieldBoolean("conectividade");
-                cfg.result().addColumn("left", "Marca")
-                        .addColumn("middle", "Modelo")
-                        .addColumn("right", "Conectividade");
+                cfg.getFilter().addFieldString(BRAND);
+                cfg.getFilter().addFieldString(MODEL);
+                cfg.getFilter().addFieldBoolean(MULTIMEDIA);
+                cfg.result()
+                        .addColumn("brand", "Brand")
+                        .addColumn("model", "Model")
+                        .addColumn("hasMultimedia", "Multimedia");
             }
 
             @Override
-            public List<Triple> load(ProviderContext<SInstance> context) {
-                return filterByInstance(context.getFilterInstance()).subList((int) context.getFirst(), (int) (context.getFirst() + context.getCount()));
+            public List<Car> load(ProviderContext<SInstance> context) {
+                return filterByInstance(context.getFilterInstance()).subList(context.getFirst(), context.getFirst() + context.getCount());
             }
 
             @Override
@@ -76,53 +90,98 @@ public class SearchModalMapperTest extends SingularFormBaseTest {
                 return (long) filterByInstance(context.getFilterInstance()).size();
             }
 
-            private List<Triple> filterByInstance(SInstance filter) {
-                String  marca         = Value.of(filter, "marca");
-                String  modelo        = Value.of(filter, "modelo");
-                Boolean conectividade = Value.of(filter, "conectividade");
-                return carros.stream().filter(triple -> {
+            private List<Car> filterByInstance(SInstance filter) {
+                String  brand         = Value.of(filter, BRAND);
+                String  model         = Value.of(filter, MODEL);
+                Boolean hasMultimedia = Value.of(filter, MULTIMEDIA);
+                return carros().stream().filter(car -> {
                     boolean contains = true;
-                    if (marca != null) {
-                        contains = triple.getLeft().equalsIgnoreCase(marca);
+                    if (brand != null) {
+                        contains = car.brand.equalsIgnoreCase(brand);
                     }
-                    if (marca != null) {
-                        contains = triple.getMiddle().equalsIgnoreCase(modelo);
+                    if (model != null) {
+                        contains = car.model.equalsIgnoreCase(model);
                     }
-                    if (marca != null) {
-                        contains = triple.getRight().equals(conectividade);
+                    if (hasMultimedia != null) {
+                        contains = car.hasMultimedia.equals(hasMultimedia);
                     }
                     return contains;
                 }).collect(Collectors.toList());
             }
 
+        });
 
-        });
-        carro.asAtrProvider().converter((ValueToSICompositeConverter<Triple<String, String, Boolean>>) (ins, triple) -> {
-            ins.setValue(marca, triple.getLeft());
-            ins.setValue(modelo, triple.getMiddle());
-            ins.setValue(conectividade, triple.getRight());
-        });
+        car.asAtrProvider()
+                .converter((ValueToSICompositeConverter<Car>) (ins, _car) -> {
+                    ins.setValue(brand, _car.brand);
+                    ins.setValue(model, _car.model);
+                    ins.setValue(multimedia, _car.hasMultimedia);
+                });
     }
 
     private void openModal() {
-        final Button modalTrigger = findOnForm(
-                Button.class,
-                page.getForm(),
-                (c) -> c.getId().equals(SearchModalPanel.MODAL_TRIGGER_ID)
-        ).findFirst().orElse(null);
-        tester.executeAjaxEvent(modalTrigger, "click");
+        tester.executeAjaxEvent(tester.getAssertionsForm()
+                .findSubComponent(c -> c.getId().equals(SearchModalPanel.MODAL_TRIGGER_ID))
+                .getTarget(Button.class), "click");
     }
 
     @Test
     public void testPagination() {
+        tester.startDummyPage();
         openModal();
-        final BSDataTable table = findOnForm(
-                BSDataTable.class,
-                page.getForm(),
-                (c) -> true
-        ).findFirst().orElse(null);
+        final BSDataTable table = tester.getAssertionsPage()
+                .findSubComponent(component -> component instanceof BSDataTable)
+                .getTarget(BSDataTable.class);
         Assert.assertEquals(table.getRowsPerPage(), PAGE_SIZE);
-        Assert.assertEquals(table.getPageCount(), Math.ceil((double) carros.size() / 5), 0);
+        Assert.assertEquals(table.getPageCount(), Math.ceil((double) carros().size() / 5), 0);
+    }
+
+    private static class Car implements Serializable {
+
+        private String  brand;
+        private String  model;
+        private Boolean hasMultimedia;
+
+        private Car(String brand, String model, Boolean hasMultimedia) {
+            this.brand = brand;
+            this.model = model;
+            this.hasMultimedia = hasMultimedia;
+        }
+
+        public String getBrand() {
+            return brand;
+        }
+
+        public String getModel() {
+            return model;
+        }
+
+        public Boolean getHasMultimedia() {
+            return hasMultimedia;
+        }
+    }
+
+    private static class CarsBuilder {
+
+        private List<Car> carros = new ArrayList<>();
+
+        CarsBuilder addCarWithMultimedia(String brand, String model) {
+            return add(brand, model, true);
+        }
+
+        CarsBuilder addCarWhitoutMultimedia(String brand, String model) {
+            return add(brand, model, false);
+        }
+
+        private CarsBuilder add(String brand, String model, Boolean hasMultimedia) {
+            carros.add(new Car(brand, model, hasMultimedia));
+            return this;
+        }
+
+        List<Car> build() {
+            return carros;
+        }
+
     }
 
 }
