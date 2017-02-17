4406172ffacd9f6e39d4e09e7dd9b29f62cdab65
Vitor Mateus Costa do Rego <vitor.rego@miranteinfo.com>
2017-02-17 15:01:33 -0200

    Correção checker factory

--- server-libs/server-module/src/main/java/org/opensingular/server/module/admin/healthsystem/webchecker/ProtocolCheckerFactory.java
+++ server-libs/server-module/src/main/java/org/opensingular/server/module/admin/healthsystem/webchecker/ProtocolCheckerFactory.java
@@ -25,35 +25,42 @@ import java.util.regex.Pattern;
 
 import javax.inject.Named;
 
-@Named
-public class ProtocolCheckerFactory {
-	private static Map<String, IProtocolChecker> protocolMap = new HashMap<>();
-	private static List<String> protocols;
-	
-	public ProtocolCheckerFactory() {
-		setMapOfProtocols();
-		setSupportedProtocols();
-	}
+public enum ProtocolCheckerFactory {
 
-	private void setMapOfProtocols() {
-		protocolMap.put("IP", new IpChecker());
-		protocolMap.put("TCP", new TcpChecker());
-		protocolMap.put("HTTP", new HttpChecker());
-		protocolMap.put("HTTPS", new HttpChecker());
-		protocolMap.put("LDAP", new LdapChecker());
-		protocolMap.put("LDAPS", new LdapChecker());
-	}
-	
-	private static void setSupportedProtocols(){
-		protocols = new ArrayList<>();
-		protocols.add("IP");
-		protocols.add("TCP");
-		protocols.add("HTTP");
-		protocols.add("HTTPS");
-		protocols.add("LDAP");
-		protocols.add("LDAPS");
-	}
-	
+	IP {
+		@Override
+		public IProtocolChecker checker() {
+			return new IpChecker();
+		}
+	}, TCP {
+		@Override
+		public IProtocolChecker checker() {
+			return new TcpChecker();
+		}
+	}, HTTP {
+		@Override
+		public IProtocolChecker checker() {
+			return new HttpChecker();
+		}
+	}, HTTPS {
+		@Override
+		public IProtocolChecker checker() {
+			return new HttpChecker();
+		}
+	}, LDAP {
+		@Override
+		public IProtocolChecker checker() {
+			return new LdapChecker();
+		}
+	}, LDAPS {
+		@Override
+		public IProtocolChecker checker() {
+			return new LdapChecker();
+		}
+	};
+
+	public abstract IProtocolChecker checker();
+
 	/**
 	 * Retorna uma implementação de IProtocolChecker de acordo com a url informada, caso não encontre uma implementação 
 	 * do protocolo, retornará uma excecão 
@@ -63,18 +70,14 @@ public class ProtocolCheckerFactory {
 	 * @throws Exception
 	 */
 	public static IProtocolChecker getProtocolChecker(String url) throws Exception{
-		for (String string : getSupportedProtocols()) {
-			Pattern pattern = Pattern.compile("^(?i)"+string+"(?i)");
+		for (ProtocolCheckerFactory checkerFactory : ProtocolCheckerFactory.values()) {
+			Pattern pattern = Pattern.compile("^(?i)"+checkerFactory.toString()+"(?i)");
 			Matcher matcher = pattern.matcher(url);
 			
 			if(matcher.find()){
-				return protocolMap.get(string);
+				return checkerFactory.checker();
 			}
 		}
 		throw new Exception("Protocolo não suportado!");
-	}
-	
-	public static List<String> getSupportedProtocols(){
-		return protocols;
 	}
 }
