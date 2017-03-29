fc98eb5c36b62d660a439540e8aee0754e96b553
Vitor Mateus Costa do Rego <vitor.rego@miranteinfo.com>
2017-03-29 13:24:35 -0300

    Testes FormatUtil

---
+++ lib/commons/src/test/java/org/opensingular/lib/commons/util/FormatUtilTest.java
@@ -0,0 +1,91 @@
+package org.opensingular.lib.commons.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.regex.Pattern;
+
+public class FormatUtilTest {
+
+    @Test
+    public void dateToDefaultTimestampStringTest(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 2017);
+        calendar.set(Calendar.MONTH, Calendar.JANUARY);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        calendar.set(Calendar.HOUR, 1);
+        calendar.set(Calendar.MINUTE, 10);
+
+        Assert.assertEquals("01/01/2017 13:10", FormatUtil.dateToDefaultTimestampString(calendar.getTime()));
+    }
+
+    @Test
+    public void appendSecondsTest(){
+        StringBuilder time = new StringBuilder();
+        FormatUtil.appendSeconds(time, 0);
+        Assert.assertEquals("", time.toString());
+
+        time = new StringBuilder();
+        FormatUtil.appendSeconds(time, 10);
+        Assert.assertEquals("10 s ", time.toString());
+
+        time = new StringBuilder();
+        FormatUtil.appendSeconds(time, 60);
+    }
+
+    @Test
+    public void appendMinutesTest(){
+        StringBuilder time = new StringBuilder();
+        FormatUtil.appendMinutes(time, 0);
+        Assert.assertEquals("", time.toString());
+
+        time = new StringBuilder();
+        FormatUtil.appendMinutes(time, 10);
+        Assert.assertEquals("10 min ", time.toString());
+
+        time = new StringBuilder();
+        FormatUtil.appendMinutes(time, 60);
+        Assert.assertEquals("1 h 0 min ", time.toString());
+    }
+
+    @Test
+    public void appendHoursTest(){
+        StringBuilder time = new StringBuilder();
+        FormatUtil.appendHours(time, 0);
+        Assert.assertEquals("", time.toString());
+
+        time = new StringBuilder();
+        FormatUtil.appendHours(time, 10);
+        Assert.assertEquals("10 h ", time.toString());
+
+        time = new StringBuilder();
+        FormatUtil.appendHours(time, 24);
+        Assert.assertEquals("1 d 0 h ", time.toString());
+    }
+
+    @Test
+    public void appendDaysTest(){
+        StringBuilder time = new StringBuilder();
+        FormatUtil.appendDays(time, 0);
+        Assert.assertEquals("", time.toString());
+
+        time = new StringBuilder();
+        FormatUtil.appendDays(time, 50);
+        Assert.assertEquals("50 d ", time.toString());
+    }
+
+    @Test
+    public void booleanDescriptionTest(){
+        String resultado = FormatUtil.booleanDescription(null, "Verdadeiro", "Falso");
+
+        Assert.assertEquals("", resultado);
+
+        resultado = FormatUtil.booleanDescription(true, "Verdadeiro", "Falso");
+        Assert.assertEquals("Verdadeiro", resultado);
+
+        resultado = FormatUtil.booleanDescription(false, "Verdadeiro", "Falso");
+        Assert.assertEquals("Falso", resultado);
+    }
+}
