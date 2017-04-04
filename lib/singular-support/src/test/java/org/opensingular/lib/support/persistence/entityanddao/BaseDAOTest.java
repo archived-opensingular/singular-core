9a0d8597e4b985e8381afc69e3f82c19793c645f
Vitor Mateus Costa do Rego <vitor.rego@miranteinfo.com>
2017-04-04 14:05:14 -0300

    Testes lib/support/persistence entity and dao

---
+++ lib/singular-support/src/test/java/org/opensingular/lib/support/persistence/entityanddao/BaseDAOTest.java
@@ -0,0 +1,84 @@
+package org.opensingular.lib.support.persistence.entityanddao;
+
+import org.hibernate.SessionFactory;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opensingular.lib.commons.base.SingularException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {DatabaseConfigurationToBeUsedByTest.class})
+public class BaseDAOTest {
+
+    @Inject
+    private TestDAO dao;
+
+    @Test
+    @Transactional
+    public void crudTest(){
+        TestEntity entity = new TestEntity(1, "entidade 1", null);
+        Integer integer = dao.save(entity);
+        Assert.assertNotNull(dao.get(integer).get());
+
+        entity.setOtherField("qualquer valor");
+        dao.merge(entity);
+
+        Assert.assertNotNull(dao.get(integer).get());
+
+        dao.delete(entity);
+        Assert.assertFalse(dao.get(integer).isPresent());
+    }
+
+    @Test
+    @Transactional
+    public void saveOrUpdateAndEvictAndGetOrExceptionTest(){
+        TestEntity entity = new TestEntity(2, "entiidade 2", null);
+
+        dao.save(entity);
+
+        entity.setName("new name");
+
+        dao.saveOrUpdate(entity);
+
+        Assert.assertNotNull(dao.getOrException(2));
+
+        dao.evict(entity);
+
+    }
+
+    @Test(expected = SingularException.class)
+    @Transactional
+    public void findTest(){
+        dao.save(new TestEntity(3, "name", null));
+
+        Assert.assertNotNull(dao.find(3));
+
+        Assert.assertNotNull(dao.findOrException(3));
+
+        dao.findOrException(15);
+    }
+
+    @Test
+    @Transactional
+    public void listAllTest(){
+        Assert.assertNotNull(dao.listAll());
+    }
+
+    @Test
+    @Transactional
+    public void findByPropertyTest(){
+        dao.save(new TestEntity(0, "entidade 1", "entidade 1"));
+        dao.save(new TestEntity(1, "name", "entidade 1"));
+
+        assertThat(dao.findByProperty("name", "entidade 1")).hasSize(1);
+    }
+}
---
+++ lib/singular-support/src/test/java/org/opensingular/lib/support/persistence/entityanddao/TestEntity.java
@@ -0,0 +1,55 @@
+package org.opensingular.lib.support.persistence.entityanddao;
+
+import org.opensingular.lib.support.persistence.entity.BaseEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TESTENTITY")
+public class TestEntity extends BaseEntity<Integer> {
+
+    public TestEntity(Integer cod, String name, String otherField){
+        this.cod = cod;
+        this.name = name;
+        this.otherField = otherField;
+    }
+
+    @Id
+    private Integer cod;
+
+    @Column
+    private String name;
+
+    @Column
+    private String otherField;
+
+    @Override
+    public Integer getCod() {
+        return cod;
+    }
+
+    public void setCod(Integer cod) {
+        this.cod = cod;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOtherField() {
+        return otherField;
+    }
+
+    public void setOtherField(String otherField) {
+        this.otherField = otherField;
+    }
+}
