65bc89c15833346d33df0e3148e97f2683dcad08
Vinicius Nunes <viniciusuriel@gmail.com>
2017-04-06 11:01:38 -0300

    correção de item do sonar

--- lib/singular-support/src/main/java/org/opensingular/lib/support/persistence/util/H2Functions.java
+++ lib/singular-support/src/main/java/org/opensingular/lib/support/persistence/util/H2Functions.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class H2Functions {
 
-    public static Double dateDiffInDays(Date d1, Date d2) throws Exception {
+    public static Double dateDiffInDays(Date d1, Date d2) {
         long d1l = d1 == null ? 0 : d1.getTime();
         long d2l = d2 == null ? 0 : d2.getTime();
         return (d1l - d2l) / ((double) 1000 * 60 * 60 * 34);
