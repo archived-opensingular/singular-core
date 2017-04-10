4666010b8928ca1ed96201ef7f44eb12d95e775f
Vitor Mateus Costa do Rego <vitor.rego@miranteinfo.com>
2017-04-10 11:04:16 -0300

    Testes Lnk restantes

--- flow/core/src/test/java/org/opensingular/flow/core/view/LnkTest.java
+++ flow/core/src/test/java/org/opensingular/flow/core/view/LnkTest.java
@@ -19,6 +19,9 @@ public class LnkTest {
         Assert.assertNotNull(Lnk.of("urlAppNew", url1));
 
         Assert.assertEquals(url1, url1.addUrlApp(null));
+
+        Lnk url2 = Lnk.of(null, "url");
+        Assert.assertTrue(url2.isUrlAppMissing());
     }
 
     @Test
@@ -28,6 +31,23 @@ public class LnkTest {
         Assert.assertEquals("<a href=\""+url1.getUrl()+"\">"+url1.getUrl()+"</a>", url1.getHrefUrl());
         Assert.assertEquals("url/path", Lnk.concat("url", "path"));
         Assert.assertEquals("url", url1.getUrl("app"));
+
+        Assert.assertNotEquals(url1, url1.appendPath("path"));
+
+        Assert.assertEquals(url1, url1.addParamSeNaoPresente("integerValue", (Integer) null));
+        Assert.assertEquals(url1, url1.addParamSeNaoPresente("integerValue", null));
+        Assert.assertNotEquals(url1, url1.addParamSeNaoPresente("integerValue", 123));
+
+        Assert.assertEquals("<img src=\"url\"/>",url1.getImg());
+        Assert.assertEquals("<img src=\"url\" title=\"title\"/> ", url1.getImg("title"));
+
+        Assert.assertEquals("<a href=\"url\" title=\"titleHref\">textLink</a>", url1.getHref("textLink", "titleHref"));
+        Assert.assertEquals("<a target=\"_blank\" href=\"url\" title=\"titleHref\">textLink</a>",
+                url1.getHrefBlank("textLink", "titleHref"));
+
+        Assert.assertNotNull(url1.toString());
+
+        Assert.assertEquals("urlApp/url", Lnk.of(null, "url").getUrl("urlApp"));
     }
 
     @Test(expected = SingularFlowException.class)
@@ -46,5 +66,8 @@ public class LnkTest {
         Assert.assertNotEquals(url1, url1.and("parameter3", "1234"));
 
         Assert.assertEquals(url1, url1.and("parameter3", (String) null));
+
+        Lnk url2 = Lnk.of(null, "url");
+        Assert.assertNotEquals(url2, url2.addUrlApp("url2"));
     }
 }
