75c9b120f25a30f800af3ce941a7ca4b12e81a64
Delfino Filho <delfino.filho@mirante.net.br>
2017-05-03 17:19:31 -0300

    [SONAR] - Retirando atributos não serializáveis de classe serializável.

--- form/core/src/main/java/org/opensingular/form/SingularInvalidFieldTypeException.java
+++ form/core/src/main/java/org/opensingular/form/SingularInvalidFieldTypeException.java
@@ -5,24 +5,12 @@ package org.opensingular.form;
  */
 public class SingularInvalidFieldTypeException extends SingularFormException {
 
-    private final SType<?> rootType, fieldType;
-
     public SingularInvalidFieldTypeException(SType<?> rootType, SType<?> fieldType) {
         super(String.format(String.format(
                 "O tipo '%s' não foi encontrado como subcampo de '%s'",
                 fieldType.getName(),
                 rootType.getName())), rootType);
-        this.rootType = rootType;
-        this.fieldType = fieldType;
         add("rootType", rootType);
         add("fieldType", fieldType);
-    }
-
-    public SType<?> getRootType() {
-        return this.rootType;
-    }
-
-    public SType<?> getFieldType() {
-        return fieldType;
     }
 }
