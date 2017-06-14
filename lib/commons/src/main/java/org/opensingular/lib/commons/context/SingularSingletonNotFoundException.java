b79cd41948cbe716a00d05c5756f7d86d34a3e65
Vinicius Nunes <viniciusuriel@gmail.com>
2017-06-14 14:10:17 -0300

    removendo construtores não utilizados

--- lib/commons/src/main/java/org/opensingular/lib/commons/context/SingularSingletonNotFoundException.java
+++ lib/commons/src/main/java/org/opensingular/lib/commons/context/SingularSingletonNotFoundException.java
@@ -4,18 +4,6 @@ import org.opensingular.lib.commons.base.SingularException;
 
 public class SingularSingletonNotFoundException extends SingularException {
 
-    public SingularSingletonNotFoundException() {
-        super("We could not find the singleton you asked for, sorry. Check if the singleton is registered.");
-    }
-
-    public SingularSingletonNotFoundException(String msg, Throwable cause) {
-        super(msg, cause);
-    }
-
-    public SingularSingletonNotFoundException(Throwable cause) {
-        super(cause);
-    }
-
     public SingularSingletonNotFoundException(String key) {
         super(String.format("We could not find the singleton you asked for (key: %s), sorry. Check if the singleton is registered.", key));
     }
