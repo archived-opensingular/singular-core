34ca76ad090221ec1c0767053f6f99724d4ee4ab
Vinicius Nunes <viniciusuriel@gmail.com>
2017-06-14 14:31:21 -0300

    Adicionando javadoc

--- lib/commons/src/main/java/org/opensingular/lib/commons/context/SingularContext.java
+++ lib/commons/src/main/java/org/opensingular/lib/commons/context/SingularContext.java
@@ -1,5 +1,11 @@
 package org.opensingular.lib.commons.context;
 
+
+/**
+ * Main context for singular bootstrap.
+ * Currently it does not provide any functionality but it is intended to
+ * group singular main technologies configurations (flow and form configurations)
+ */
 public interface SingularContext {
 
     static SingularContext get() {
