de3fa7b818c24fa33aca2bc2cece15890c4e572d
torquato.neto <torquato.neto@opensingular.com>
2017-07-14 15:39:48 -0300

    Correção Sonar

--- form/core/src/main/java/org/opensingular/form/internal/freemarker/FormFreemarkerUtil.java
+++ form/core/src/main/java/org/opensingular/form/internal/freemarker/FormFreemarkerUtil.java
@@ -123,7 +123,7 @@ public final class FormFreemarkerUtil {
     
     private static Template parseTemplate(String template, boolean ignoreError) {
         try {
-            TemplateExceptionHandler result = null;
+            TemplateExceptionHandler result;
             String ignoreProperty = SingularProperties.get().getProperty(SingularProperties.FREEMARKER_IGNORE_ERROR);
             if(ignoreProperty != null){
                 if("true".equalsIgnoreCase(ignoreProperty)){
@@ -131,10 +131,7 @@ public final class FormFreemarkerUtil {
                 }else{
                     result = TemplateExceptionHandler.RETHROW_HANDLER;
                 }
-//                result = "true".equalsIgnoreCase(ignoreProperty) ? TemplateExceptionHandler.IGNORE_HANDLER
-//                        : TemplateExceptionHandler.RETHROW_HANDLER;
             }else{
-//                result = ignoreError ? TemplateExceptionHandler.IGNORE_HANDLER : TemplateExceptionHandler.RETHROW_HANDLER;
                 if(ignoreError){
                     result = TemplateExceptionHandler.IGNORE_HANDLER;
                 }else{
