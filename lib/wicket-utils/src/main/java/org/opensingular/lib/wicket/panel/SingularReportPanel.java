92e60b2eb01ec98f43267b5afae1a77552bed5c8
Danilo Mesquita <danilofmesquita@gmail.com>
2017-07-25 15:47:52 -0300

    criando SingularReportPanel

---
+++ lib/wicket-utils/src/main/java/org/opensingular/lib/wicket/panel/SingularReportPanel.java
@@ -0,0 +1,11 @@
+package org.opensingular.lib.wicket.panel;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class SingularReportPanel extends Panel {
+
+    public SingularReportPanel(String id) {
+        super(id);
+    }
+
+}
