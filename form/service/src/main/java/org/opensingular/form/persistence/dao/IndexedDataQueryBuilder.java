7d9fc300fc5bcb328ccd4773210e737f98f03998
Danilo Mesquita <danilofmesquita@gmail.com>
2017-07-26 11:21:56 -0300

    corrigindo erros do sonar

--- form/service/src/main/java/org/opensingular/form/persistence/dao/IndexedDataQueryBuilder.java
+++ form/service/src/main/java/org/opensingular/form/persistence/dao/IndexedDataQueryBuilder.java
@@ -28,9 +28,9 @@ public class IndexedDataQueryBuilder {
 
     private void addColumnToSelect(String column) {
         if (colCount == 0) {
-            select.append("  , " + column + ".co_versao_formulario as co_versao_formulario \n");
+            select.append("  , ").append(column).append(".co_versao_formulario as co_versao_formulario \n");
         }
-        select.append("  , " + column + ".ds_valor as " + column + "\n");
+        select.append("  , ").append(column).append(".ds_valor as ").append(column).append("\n");
     }
 
     private void addJoinClause(String columnAlias, String fieldsNames) {
@@ -47,7 +47,7 @@ public class IndexedDataQueryBuilder {
 
          if (fields != null) {
              for (int i=0; i<fields.length; i++) {
-                fieldsNames.append("'" + fields[i] + "'");
+                fieldsNames.append("'").append(fields[i]).append("'");
                 if (fields.length - i > 1) {
                     fieldsNames.append(", ");
                 }
