873d6c574f74e4db74dcc0b957f35b36548c9453
Danilo Mesquita <danilofmesquita@gmail.com>
2017-07-27 13:51:21 -0300

    evoluindo interface "SingularFormReport"

--- form/core/src/main/java/org/opensingular/form/report/SingularFormReport.java
+++ form/core/src/main/java/org/opensingular/form/report/SingularFormReport.java
@@ -4,26 +4,43 @@ import org.opensingular.form.SInstance;
 import org.opensingular.form.SType;
 import org.opensingular.lib.commons.views.ViewGenerator;
 
+import java.io.Serializable;
+
 /**
  * SingularFormReport
  * <p>
  * Interface for create reports using the SingularForms engine
  *
+ * @param <E> the provider element
  * @param <T> the filter type
  * @param <I> the filter instance
  */
-public interface SingularFormReport<T extends SType<I>, I extends SInstance> {
+public interface SingularFormReport<E extends Serializable, T extends SType<I>, I extends SInstance> extends Serializable {
+
+    /**
+     * The Report Name
+     * @return the name
+     */
+    String getReportName();
 
     /**
      * The type to build the filter
      * @return the type
      */
-    T filterType();
+    T getFilterType();
 
     /**
      * the view generator to build the reports
      * @return the viewgenerator
      */
-    ViewGenerator viewGenerator(I filterInstance);
+    ViewGenerator makeViewGenerator(Iterable<E> values);
+
+
+    /**
+     * Query the report values
+     * @param filter the filter to be applied
+     * @return the result values
+     */
+    Iterable<E> queryReportValues(I filter);
 
 }
