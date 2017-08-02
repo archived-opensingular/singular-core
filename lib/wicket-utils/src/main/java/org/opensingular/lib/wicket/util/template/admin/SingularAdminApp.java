2a4e4b0f2990d8a1666b63ffb1ce5ee0653b02bb
Danilo Mesquita <danilofmesquita@gmail.com>
2017-08-02 14:02:38 -0300

    trocando "queue" por "add" em diversos pontos

--- lib/wicket-utils/src/main/java/org/opensingular/lib/wicket/util/template/admin/SingularAdminApp.java
+++ lib/wicket-utils/src/main/java/org/opensingular/lib/wicket/util/template/admin/SingularAdminApp.java
@@ -1,6 +1,7 @@
 package org.opensingular.lib.wicket.util.template.admin;
 
 import org.apache.wicket.MarkupContainer;
+import org.apache.wicket.markup.html.TransparentWebMarkupContainer;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 
 import static org.opensingular.lib.wicket.util.util.WicketUtils.$b;
@@ -14,8 +15,15 @@ public interface SingularAdminApp {
         return new WebMarkupContainer(id);
     }
 
-    default MarkupContainer buildPageBody(String id, boolean withMenu, SingularAdminTemplate adminTemplate) {
-        MarkupContainer pageBody = new WebMarkupContainer(id);
+    /**
+     * Page Body, deve ser um TransparentWebMarkupContainer. Por é pai de todos os filhos <wicket:child></wicket:child>
+     * @param id o id do component
+     * @param withMenu se deve conter menu
+     * @param adminTemplate o template utilizado
+     * @return o componente criado
+     */
+    default TransparentWebMarkupContainer buildPageBody(String id, boolean withMenu, SingularAdminTemplate adminTemplate) {
+        TransparentWebMarkupContainer pageBody = new TransparentWebMarkupContainer(id);
         if (!withMenu) {
             pageBody.add($b.classAppender("page-full-width"));
         }
