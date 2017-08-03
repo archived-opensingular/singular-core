1c20394fc0bdc8442585b91ff6f71431a7cecd61
Danilo Mesquita <danilofmesquita@gmail.com>
2017-08-03 08:41:13 -0300

    removendo teste de funcionalidade não mais existente

--- lib/wicket-utils/src/test/java/org/opensingular/lib/wicket/views/SingularReportPanelTest.java
+++ lib/wicket-utils/src/test/java/org/opensingular/lib/wicket/views/SingularReportPanelTest.java
@@ -10,12 +10,4 @@ public class SingularReportPanelTest extends WicketTestCase {
         tester.startPage(new MockSingularReportPage(id -> new BlankSingularReportPanel(id, () -> null)));
         tester.assertRenderedPage(MockSingularReportPage.class);
     }
-
-    @Test
-    public void testRenderingSearchModal() throws Exception {
-        BSModalBorder filter = new BSModalBorder("modal");
-        tester.startPage(new MockSingularReportPage(id -> new BlankSingularReportPanel(id, () -> null)));
-        tester.executeAjaxEvent("f:srp:form:search", "click");
-        tester.assertVisible(filter.getPageRelativePath());
-    }
 }
