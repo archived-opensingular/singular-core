7f2640bc3e199a162cefa46b5f54f7e136eca1cf
Danilo Mesquita <danilofmesquita@gmail.com>
2017-08-10 16:16:49 -0300

    corrigindo erro ao tentar converter valor inteiro em bigdecimal

--- form/service/src/main/java/org/opensingular/form/persistence/dao/IndexedDataDAO.java
+++ form/service/src/main/java/org/opensingular/form/persistence/dao/IndexedDataDAO.java
@@ -2,6 +2,7 @@ package org.opensingular.form.persistence.dao;
 
 import org.hibernate.SQLQuery;
 import org.hibernate.SessionFactory;
+import org.hibernate.type.StandardBasicTypes;
 import org.opensingular.form.persistence.dto.BaseDTO;
 import org.opensingular.form.persistence.dto.STypeIndexed;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import java.lang.reflect.Field;
+import java.math.BigDecimal;
 
 @Repository
 @Transactional(Transactional.TxType.NEVER)
@@ -17,7 +19,7 @@ public class IndexedDataDAO {
     @Inject
     protected SessionFactory sessionFactory;
 
-    protected String createSqlFromDTO (Class<? extends BaseDTO> dto, String schema) {
+    protected String createSqlFromDTO(Class<? extends BaseDTO> dto, String schema) {
         IndexedDataQueryBuilder builder = new IndexedDataQueryBuilder(schema);
         for (Field field : dto.getDeclaredFields()) {
             if (field.getAnnotation(STypeIndexed.class).indexedColumn()) {
@@ -30,7 +32,11 @@ public class IndexedDataDAO {
     protected void addScalarsFromDTO(SQLQuery query, Class<? extends BaseDTO> dto) {
         for (Field field : dto.getDeclaredFields()) {
             if (field.getAnnotation(STypeIndexed.class).returnColumn()) {
-                query.addScalar(field.getName());
+                if (BigDecimal.class.isAssignableFrom(field.getType())) {
+                    query.addScalar(field.getName(), StandardBasicTypes.BIG_DECIMAL);
+                } else {
+                    query.addScalar(field.getName());
+                }
             }
         }
     }
