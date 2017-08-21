d7185094b045da7ddbbce46b406f9f543a3ed9e9
Danilo Mesquita <danilofmesquita@gmail.com>
2017-08-21 14:58:22 -0300

    adicionando modulos do studio

---
+++ studio/base-app/src/main/java/org/opensingular/studio/app/config/StudioAppConfig.java
@@ -0,0 +1,11 @@
+package org.opensingular.studio.app.config;
+
+/**
+ * Studio App Config
+ *
+ * The implementation of this interfaces should be a Singleton. Providing information
+ * to build the app.
+ *
+ */
+public interface StudioAppConfig {
+}
