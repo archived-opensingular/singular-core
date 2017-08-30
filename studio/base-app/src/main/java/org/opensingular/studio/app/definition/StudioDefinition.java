53f4794b59838688a0d7419b87eb6f6c73f1f01a
Danilo Mesquita <danilofmesquita@gmail.com>
2017-08-30 15:48:33 -0300

    adicionando controle de permissões ao studio definition

--- studio/base-app/src/main/java/org/opensingular/studio/app/definition/StudioDefinition.java
+++ studio/base-app/src/main/java/org/opensingular/studio/app/definition/StudioDefinition.java
@@ -3,9 +3,12 @@ package org.opensingular.studio.app.definition;
 
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
 import org.opensingular.form.SIComposite;
+import org.opensingular.form.studio.StudioCRUDPermissionStrategy;
 import org.opensingular.lib.wicket.util.datatable.BSDataTableBuilder;
 
-public interface StudioDefinition {
+import java.io.Serializable;
+
+public interface StudioDefinition extends Serializable {
 
     String getRepositoryBeanName();
 
@@ -13,4 +16,7 @@ public interface StudioDefinition {
 
     String getTitle();
 
+    default StudioCRUDPermissionStrategy getPermissionStrategy() {
+        return StudioCRUDPermissionStrategy.ALL;
+    }
 }
