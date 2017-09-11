ffd849b6644027a66afd5944168c0031ec6b5d41
Danilo Mesquita <danilofmesquita@gmail.com>
2017-09-11 19:33:33 -0300

    ajustando servlet para ser independente de wicket

--- lib/wicket-utils/src/main/java/org/opensingular/servlet/KeepSessionAliveServlet.java
+++ lib/wicket-utils/src/main/java/org/opensingular/servlet/KeepSessionAliveServlet.java
@@ -1,6 +1,5 @@
 package org.opensingular.servlet;
 
-import org.apache.wicket.Session;
 import org.opensingular.lib.commons.util.Loggable;
 
 import javax.servlet.ServletException;
@@ -8,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(KeepSessionAliveServlet.ENDPOINT)
@@ -17,15 +17,18 @@ public class KeepSessionAliveServlet extends HttpServlet implements Loggable {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if(Session.exists()){
-            getLogger().info(String.format("Keeping session with id %s alive.", Session.get().getId()));
-        }
+        logExecution(req);
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        if(Session.exists()){
-            getLogger().info(String.format("Keeping session with id %s alive.", Session.get().getId()));
+        logExecution(req);
+    }
+
+    private void logExecution(HttpServletRequest req) {
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            getLogger().info(String.format("Keeping session with id %s alive.", session.getId()));
         }
     }
 }
