693a7bc67846c1fab2ddbadf740017f83def7238
Daniel Bordin <daniel@mirante.net.br>
2017-09-19 15:02:56 -0300

    [FORM] Implemented the conversion from SType to XSD in FormXSDUtil

---
+++ form/core/src/main/java/org/opensingular/form/io/FormToXSDConfig.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2016 Singular Studios (a.k.a Atom Tecnologia) - www.opensingular.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opensingular.form.io;
+
+import org.opensingular.form.SType;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Configures details in the generation of a XSD from {@link org.opensingular.form.SType}.
+ *
+ * @author Daniel C. Bordin on 19/09/2017.
+ * @see FormXSDUtil#toXsd(SType)
+ */
+public class FormToXSDConfig {
+
+    private boolean generateCustomAttribute;
+
+    private FormToXSDConfig() {}
+
+    /** Create a XSD customized for human reading with extra information. */
+    @Nonnull
+    public static FormToXSDConfig newForUserDisplay() {
+        FormToXSDConfig config = new FormToXSDConfig();
+        config.setGenerateCustomAttribute(true);
+        return config;
+    }
+
+    /** Create a XSD customized for generating a Web Service definition. */
+    @Nonnull
+    public static FormToXSDConfig newForWebServiceDefinition() {
+        FormToXSDConfig config = new FormToXSDConfig();
+        config.setGenerateCustomAttribute(false);
+        return config;
+    }
+
+    public void setGenerateCustomAttribute(boolean generateCustomAttribute) {
+        this.generateCustomAttribute = generateCustomAttribute;
+    }
+
+    public boolean isGenerateCustomAttribute() {
+        return generateCustomAttribute;
+    }
+}
--- form/core/src/test/java/org/opensingular/form/io/TestFormXSDUtil.java
+++ form/core/src/test/java/org/opensingular/form/io/TestFormXSDUtil.java
@@ -4,10 +4,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.opensingular.form.PackageBuilder;
+import org.opensingular.form.SDictionary;
+import org.opensingular.form.SIComposite;
 import org.opensingular.form.SType;
+import org.opensingular.form.STypeComposite;
+import org.opensingular.form.STypeList;
 import org.opensingular.form.TestCaseForm;
 import org.opensingular.form.helpers.AssertionsSForm;
 import org.opensingular.form.helpers.AssertionsSType;
+import org.opensingular.form.helpers.AssertionsXML;
+import org.opensingular.form.type.core.STypeBoolean;
+import org.opensingular.form.type.core.STypeDate;
+import org.opensingular.form.type.core.STypeDateTime;
+import org.opensingular.form.type.core.STypeDecimal;
+import org.opensingular.form.type.core.STypeHTML;
+import org.opensingular.form.type.core.STypeInteger;
+import org.opensingular.form.type.core.STypeLong;
+import org.opensingular.form.type.core.STypeString;
+import org.opensingular.form.type.core.STypeTime;
 
 /**
  * Testa se o correto funcionamento da conversão de um XSD em Stype. Mais
@@ -20,6 +34,143 @@ public class TestFormXSDUtil extends TestCaseForm {
 
     public TestFormXSDUtil(TestFormConfig testFormConfig) {
         super(testFormConfig);
+    }
+
+    @Test
+    public void toXsdSimpleType() {
+        SDictionary dictionary = createTestDictionary();
+        AssertionsXML xml = toXsd(dictionary.getType(STypeString.class));
+        xml = xml.getOnlyChild("xs:element");
+        xml.hasAttributes(3).isAttribute("name", "String").isAttribute("type", "xs:string").isAttribute("xsf:maxLength",
+                "100").hasNoChildren();
+
+        xml = toXsdWebService(dictionary.getType(STypeString.class));
+        xml = xml.getOnlyChild("xs:element");
+        xml.hasAttributes(2).isAttribute("name", "String").isAttribute("type", "xs:string").hasNoChildren();
+
+        xml = toXsd(dictionary.getType(STypeInteger.class));
+        xml = xml.getOnlyChild("xs:element");
+        xml.isAttribute("name", "Integer").isAttribute("type", "xs:integer").attributeNotPresent("minOccurs")
+                .hasNoChildren();
+
+        xml = toXsd(dictionary.getType(STypeLong.class));
+        xml = xml.getOnlyChild("xs:element");
+        xml.isAttribute("name", "Long").isAttribute("type", "xs:long").attributeNotPresent("minOccurs").hasNoChildren();
+
+        xml = toXsd(dictionary.getType(STypeDecimal.class));
+        xml = xml.getOnlyChild("xs:element");
+        xml.isAttribute("name", "Decimal").isAttribute("type", "xs:decimal").hasNoChildren();
+
+        xml = toXsd(dictionary.getType(STypeBoolean.class));
+        xml = xml.getOnlyChild("xs:element");
+        xml.isAttribute("name", "Boolean").isAttribute("type", "xs:boolean").hasNoChildren();
+
+        xml = toXsd(dictionary.getType(STypeDate.class));
+        xml = xml.getOnlyChild("xs:element");
+        xml.isAttribute("name", "Date").isAttribute("type", "xs:date").hasNoChildren();
+
+        xml = toXsd(dictionary.getType(STypeTime.class));
+        xml = xml.getOnlyChild("xs:element");
+        xml.isAttribute("name", "Time").isAttribute("type", "xs:time").hasNoChildren();
+
+        xml = toXsd(dictionary.getType(STypeDateTime.class));
+        xml = xml.getOnlyChild("xs:element");
+        xml.isAttribute("name", "DateTime").isAttribute("type", "xs:dateTime").hasNoChildren();
+
+        xml = toXsd(dictionary.getType(STypeHTML.class));
+        xml = xml.getOnlyChild("xs:element");
+        xml.isAttribute("name", "HTML").isAttribute("type", "xs:string").hasNoChildren();
+    }
+
+    @Test
+    public void toXsdCompositeType() {
+        PackageBuilder pkg = createTestPackage();
+        STypeComposite<SIComposite> order = pkg.createCompositeType("order");
+        order.addFieldString("description").asAtr().required();
+        order.addFieldString("observation");
+        order.addFieldInteger("number").asAtr().required();
+        order.addFieldDate("submission");
+
+        AssertionsXML xml = toXsd(order);
+        xml = xml.getOnlyChild("xs:element");
+        xml.isAttribute("name", "order").attributeNotPresent("type").attributeNotPresent("minOccurs");
+        xml = xml.getOnlyChild("xs:complexType");
+        xml = xml.getOnlyChild("xs:sequence");
+        xml.hasChildren(4);
+        xml.getChild(0).isName("xs:element").isAttribute("name", "description").isAttribute("type", "xs:string")
+                .attributeNotPresent("minOccurs").hasNoChildren();
+        xml.getChild(1).isName("xs:element").isAttribute("name", "observation").isAttribute("type", "xs:string")
+                .isAttribute("minOccurs", "0").hasNoChildren();
+        xml.getChild(2).isName("xs:element").isAttribute("name", "number").isAttribute("type", "xs:integer")
+                .attributeNotPresent("minOccurs").hasNoChildren();
+        xml.getChild(3).isName("xs:element").isAttribute("name", "submission").isAttribute("type", "xs:date")
+                .isAttribute("minOccurs", "0").hasNoChildren();
+
+
+        STypeComposite<SIComposite> orderSpecial = pkg.createType("orderSpecial", order);
+        orderSpecial.addFieldBoolean("special");
+
+        xml = toXsd(orderSpecial);
+        xml = xml.getOnlyChild("xs:element");
+        xml.isAttribute("name", "orderSpecial").attributeNotPresent("type").attributeNotPresent("minOccurs");
+        xml = xml.getOnlyChild("xs:complexType");
+        xml = xml.getOnlyChild("xs:sequence");
+        xml.hasChildren(5);
+        xml.getChild(4).isName("xs:element").isAttribute("name", "special").isAttribute("type", "xs:boolean")
+                .isAttribute("minOccurs", "0").hasNoChildren();
+
+    }
+
+    @Test
+    public void toXsdListTypeOfComposite() {
+        PackageBuilder pkg = createTestPackage();
+        STypeList<STypeComposite<SIComposite>, SIComposite> items = pkg.createListOfNewCompositeType("items", "item");
+        STypeComposite<SIComposite> item = items.getElementsType();
+        item.addFieldString("name").asAtr().required();
+
+        AssertionsXML xml = toXsd(items);
+        xml = xml.getOnlyChild("xs:element");
+        xml.isAttribute("name", "items").hasAttributes(1);
+        xml = xml.getOnlyChild("xs:complexType").hasAttributes(0);
+        xml = xml.getOnlyChild("xs:sequence").hasAttributes(0);
+        xml = xml.getOnlyChild("xs:element").hasAttributes(2).isAttribute("name", "item").isAttribute("maxOccurs",
+                "unbounded");
+        xml = xml.getOnlyChild("xs:complexType").hasAttributes(0);
+        xml = xml.getOnlyChild("xs:sequence").hasAttributes(0);
+        xml = xml.getOnlyChild("xs:element").hasAttributes(3).isAttribute("name", "name").isAttribute("type",
+                "xs:string").isAttribute("xsf:maxLength", "100").hasNoChildren();
+
+        items.withMiniumSizeOf(2).withMaximumSizeOf(4);
+        xml = toXsd(items);
+        xml = xml.getOnlyChild("xs:element");
+        xml = xml.getOnlyChild("xs:complexType").hasAttributes(0);
+        xml = xml.getOnlyChild("xs:sequence").hasAttributes(0);
+        xml = xml.getOnlyChild("xs:element").hasAttributes(3).isAttribute("name", "item").isAttribute("minOccurs", "2")
+                .isAttribute("maxOccurs", "4");
+    }
+
+    private AssertionsXML toXsd(SType<?> type) {
+        return toXsd(type, FormToXSDConfig.newForUserDisplay());
+    }
+
+    private AssertionsXML toXsdWebService(SType<?> type) {
+        return toXsd(type, FormToXSDConfig.newForWebServiceDefinition());
+    }
+
+    private AssertionsXML toXsd(SType<?> type, FormToXSDConfig config) {
+        AssertionsXML xml = new AssertionsXML(FormXSDUtil.toXsd(type, config)).isNotNull();
+        xml.getTarget().printTabulado();
+        xml.isName(FormXSDUtil.XSD_NAMESPACE_PREFIX, "schema");
+        assertNameSpaceXsd(xml);
+        xml.hasChildren(1);
+        return xml;
+    }
+
+    private void assertNameSpaceXsd(AssertionsXML xml) {
+        xml.isNameSpaceUri(FormXSDUtil.XSD_NAMESPACE_URI);
+        for (AssertionsXML x : xml) {
+            assertNameSpaceXsd(x);
+        }
     }
 
     @Test
