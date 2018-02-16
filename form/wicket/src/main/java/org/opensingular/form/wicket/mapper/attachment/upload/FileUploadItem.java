700eb71a5a9d96a1829efcabca37cb7961bfa996
Douglas Silva <douglas.silva@mirante.net.br>
2018-02-16 09:03:44 -0200

    [SGL-558] Adicionando comentário à interface.

--- form/wicket/src/main/java/org/opensingular/form/wicket/mapper/attachment/upload/FileUploadItem.java
+++ form/wicket/src/main/java/org/opensingular/form/wicket/mapper/attachment/upload/FileUploadItem.java
@@ -21,19 +21,50 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- *
+ * Common interface to a file upload item.
  */
 public interface FileUploadItem<T> {
 
+    /**
+     * Gets the input stream of the file.
+     *
+     * @return an instance of {@link InputStream}.
+     * @throws IOException
+     */
     InputStream getInputStream() throws IOException;
 
+    /**
+     * Checks if the file item is a form field.
+     *
+     * @return <code>true</code> if the file is a form field.
+     */
     boolean isFormField();
 
+    /**
+     * Gets the content type of a file.
+     *
+     * @return file content type.
+     */
     String getContentType();
 
+    /**
+     * Gets the size of the file.
+     *
+     * @return file size.
+     */
     long getSize();
 
+    /**
+     * Gets the file name.
+     *
+     * @return the file name.
+     */
     String getName();
 
+    /**
+     * Gets the file item that implements this interface.
+     *
+     * @return file upload item wrapped.
+     */
     T getWrapped();
 }
