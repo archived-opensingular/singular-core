c852ac910846de9d2d5d05c751f08eba33c49502
vinicius.nunes <vinicius.nunes@opensingular.com>
2018-04-04 21:43:07 -0300

    [SGL-547] fixing h2 embbeded database reinitialization

--- lib/singular-support/src/main/java/org/opensingular/lib/support/persistence/util/H2Functions.java
+++ lib/singular-support/src/main/java/org/opensingular/lib/support/persistence/util/H2Functions.java
@@ -18,13 +18,38 @@
 
 package org.opensingular.lib.support.persistence.util;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 
 public class H2Functions {
+
+    public static final String DROPALLONCE_SCRIPT = "CREATE ALIAS if not exists dropAllObjects FOR \"" + H2Functions.class.getName() + ".dropAllObjects\";select dropAllObjects();";
 
     public static Double dateDiffInDays(Date d1, Date d2) {
         long d1l = d1 == null ? 0 : d1.getTime();
         long d2l = d2 == null ? 0 : d2.getTime();
         return (d1l - d2l) / ((double) 1000 * 60 * 60 * 24);
+    }
+
+    /**
+     * Function to trick H2 to drop all database objects using INIT script (see h2 docs) only once per database initialization.
+     * Using plain "DROP ALL OBJECTS" in the INIT connection parameter makes H2 drop the entire database every time the connection pool creates
+     * a new connections.
+     * The corresponding INIT script to be used with this functions is defined in constant {@link #DROPALLONCE_SCRIPT}
+     * @param conn
+     * @throws SQLException
+     */
+    public static void dropAllObjects(Connection conn) throws SQLException {
+        conn.createStatement().executeUpdate("CREATE TEMPORARY table IF NOT EXISTS INITONCE (initialized BOOLEAN not null);");
+        ResultSet rs = conn.createStatement().executeQuery(" SELECT COUNT(*) FROM INITONCE");
+        if (rs.next()) {
+            if (rs.getInt(1) == 0) {
+                conn.createStatement().executeUpdate("DROP ALL OBJECTS");
+                conn.createStatement().executeUpdate("CREATE TEMPORARY table IF NOT EXISTS INITONCE (initialized BOOLEAN not null);");
+                conn.createStatement().executeUpdate("insert into initonce values (true)");
+            }
+        }
     }
 }
