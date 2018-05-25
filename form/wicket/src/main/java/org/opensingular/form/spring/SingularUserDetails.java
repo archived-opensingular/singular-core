f93040c004163ec3c3aae84500b8d08b2a771682
vinicius.nunes <vinicius.nunes@opensingular.com>
2018-05-25 17:15:49 -0300

    [REFACTORING] renaming user details, improving logout

---
+++ form/wicket/src/main/java/org/opensingular/form/spring/SingularUserDetails.java
@@ -0,0 +1,63 @@
+/*
+ *
+ *  * Copyright (C) 2016 Singular Studios (a.k.a Atom Tecnologia) - www.opensingular.com
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  * http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package org.opensingular.form.spring;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public interface SingularUserDetails extends UserDetails {
+
+    String getDisplayName();
+
+    @Override
+    default Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    default String getPassword() {
+        return null;
+    }
+
+
+    @Override
+    default boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    default boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    default boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    default boolean isEnabled() {
+        return true;
+    }
+
+
+}
