c862a5271631c8dbd024b132080023357da201dc
thiago.bomfim <thiago.bomfim@opensingular.com>
2018-06-20 11:16:48 -0300

    [SGL_654] - Correção do componente de timer picker para caso seja digitado 90:0 fique 9:00

--- form/wicket/src/main/java/org/opensingular/form/wicket/mapper/datetime/CreateTimePickerBehavior.js
+++ form/wicket/src/main/java/org/opensingular/form/wicket/mapper/datetime/CreateTimePickerBehavior.js
@@ -16,10 +16,28 @@
                     $(this).timepicker('hideWidget');
             }
         });
-        $timepicker.on('remove',
-            function (e) {
-                console.log('teste');
-                $(this).timepicker('remove');
-            });
+
+        $timepicker.on('keyup', function (e) {
+            if (!(e.keyCode == 8 || e.keyCode == 46)) {
+                var timeArray = $timepicker.val().replace(/[^0-9\:]/g, '').split(':');
+
+                hour = timeArray[0] ? timeArray[0].toString() : timeArray.toString();
+                minute = timeArray[1] ? timeArray[1].toString() : '';
+
+                if (hour.length == 2 && minute.length == 1) {
+                    minute = hour.charAt(1) + minute;
+                    hour = hour.charAt(0);
+                    $timepicker.val(hour + ":" + minute);
+                } else if (minute.length == 2 && hour.length == 1 && ((e.keyCode >= 48 && e.keyCode <= 57) || (e.keyCode >= 96 && e.keyCode <= 105))) {
+                    hour = hour.charAt(0) + minute.charAt(0);
+                    minute = minute.charAt(1) + e.key;
+                    $timepicker.val(hour + ":" + minute);
+                }
+            }
+        });
+
+        $timepicker.on('remove', function (e) {
+            $(this).timepicker('remove');
+        });
     }
 })();
