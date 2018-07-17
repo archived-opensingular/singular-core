c6987b84506af0ec9dcc27312b2d858b413ca90d
thiago.bomfim <thiago.bomfim@opensingular.com>
2018-07-17 17:58:59 -0300

    [SGL-645] - Adequação dos testes unitarios.

--- form/wicket/src/main/java/org/opensingular/form/wicket/mapper/buttons/AddButton.java
+++ form/wicket/src/main/java/org/opensingular/form/wicket/mapper/buttons/AddButton.java
@@ -1,24 +1,27 @@
 package org.opensingular.form.wicket.mapper.buttons;
 
+import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
-import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.model.IModel;
 import org.opensingular.form.SIList;
 import org.opensingular.form.SInstance;
 import org.opensingular.form.wicket.mapper.MapperCommons;
-import org.opensingular.lib.wicket.util.ajax.ActionAjaxButton;
 import org.opensingular.lib.wicket.util.bootstrap.layout.BSContainer;
 import org.opensingular.lib.wicket.util.resource.DefaultIcons;
 import org.opensingular.lib.wicket.util.scripts.Scripts;
 
 import static org.opensingular.lib.wicket.util.util.WicketUtils.$b;
 
-public class AddButton extends ActionAjaxButton {
+public class AddButton extends AjaxLink<String> {
+
     private final IModel<SIList<SInstance>> listModel;
 
-    public AddButton(String id, Form<?> form, IModel<SIList<SInstance>> mList) {
-        super(id, form);
-        this.setDefaultFormProcessing(false);
+    private Component component;
+
+    public AddButton(String id, Component component, IModel<SIList<SInstance>> mList) {
+        super(id);
+        this.component = component;
         listModel = mList;
         add($b.attr("title", "Adicionar Linha"));
     }
@@ -29,14 +32,14 @@ public class AddButton extends ActionAjaxButton {
     }
 
     @Override
-    protected void onAction(AjaxRequestTarget target, Form<?> form) {
+    public void onClick(AjaxRequestTarget target) {
         final SIList<SInstance> list = listModel.getObject();
         if (list.getType().getMaximumSize() != null && list.getType().getMaximumSize() == list.size()) {
             target.appendJavaScript(";bootbox.alert('A quantidade máxima de valores foi atingida.');");
             target.appendJavaScript(Scripts.multipleModalBackDrop());
         } else {
             list.addNew();
-            target.add(form);
+            target.add(component);
             target.focusComponent(this);
         }
     }
