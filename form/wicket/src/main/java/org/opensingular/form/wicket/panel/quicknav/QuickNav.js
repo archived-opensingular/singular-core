5600357d8e602849c81d89e4b0942013928be6ff
Danilo Mesquita <danilofmesquita@gmail.com>
2018-08-07 10:10:35 -0300

    [SGL-749] Melhorando legibilidade do script de QuickNav

--- form/wicket/src/main/java/org/opensingular/form/wicket/panel/quicknav/QuickNav.js
+++ form/wicket/src/main/java/org/opensingular/form/wicket/panel/quicknav/QuickNav.js
@@ -1,17 +1,22 @@
-var QuickNav = function () {
-    return {
-        init: function () {
-            if ($(".quick-nav").length > 0) {
-                var i = $(".quick-nav");
-                i.each(function () {
-                    var i = $(this), n = i.find(".quick-nav-trigger");
-                    n.on("click", function (n) {
-                        n.preventDefault(), i.toggleClass("nav-is-visible")
-                    })
-                }), $(document).on("click", function (n) {
-                    !$(n.target).is(".quick-nav-trigger") && !$(n.target).is(".quick-nav-trigger span") && i.removeClass("nav-is-visible")
+(function () {
+    "use strict";
+    window.QuickNav = {};
+    window.QuickNav.init = function () {
+        var quickNavs = $(".quick-nav");
+        if (quickNavs.length > 0) {
+            quickNavs.each(function () {
+                var quickNav = $(this),
+                    quickNavTrigger = quickNav.find(".quick-nav-trigger");
+                quickNavTrigger.on("click", function (quickNavTriggerClickEvent) {
+                    quickNavTriggerClickEvent.preventDefault();
+                    quickNav.toggleClass("nav-is-visible");
                 })
-            }
+            });
+            $(document).on("click", function (event) {
+                if (!$(event.target).is(".quick-nav-trigger") && !$(event.target).is(".quick-nav-trigger span")) {
+                    i.removeClass("nav-is-visible")
+                }
+            });
         }
-    }
-}();
+    };
+})();
