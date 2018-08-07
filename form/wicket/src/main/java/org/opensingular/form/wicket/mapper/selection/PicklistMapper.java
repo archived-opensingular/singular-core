b866fc12ae4104a92b8d68bfba22d5f5e17944a8
Danilo Mesquita <danilofmesquita@gmail.com>
2018-08-07 11:19:49 -0300

    [SGL-706] Habilitando pageblock ao clicar no picklist

--- form/wicket/src/main/java/org/opensingular/form/wicket/mapper/selection/PicklistMapper.java
+++ form/wicket/src/main/java/org/opensingular/form/wicket/mapper/selection/PicklistMapper.java
@@ -17,33 +17,14 @@
 package org.opensingular.form.wicket.mapper.selection;
 
 import org.apache.wicket.Component;
-import org.apache.wicket.model.IModel;
-import org.opensingular.form.SInstance;
-import org.opensingular.form.wicket.IAjaxUpdateListener;
 import org.opensingular.form.wicket.WicketBuildContext;
-import org.opensingular.form.wicket.behavior.AjaxUpdateInputBehavior;
-import org.opensingular.form.wicket.mapper.SingularEventsHandlers;
 import org.opensingular.form.wicket.model.ReadOnlyModelValue;
 import org.opensingular.lib.wicket.util.bootstrap.layout.BSControls;
 
-import static org.opensingular.form.wicket.mapper.SingularEventsHandlers.OPTS_ORIGINAL_PROCESS_EVENT;
-import static org.opensingular.form.wicket.mapper.SingularEventsHandlers.OPTS_ORIGINAL_VALIDATE_EVENT;
-
 @SuppressWarnings("serial")
 public class PicklistMapper extends MultipleSelectMapper {
-
-    private String JS_PICKLIST_CHANGE_EVENT = "picklist:selected";
     @Override
     protected Component appendFormGroup(BSControls formGroup, WicketBuildContext ctx) {
         return formGroup.appendPicklist(retrieveChoices(ctx.getModel(), new ReadOnlyModelValue(ctx.getModel())));
-    }
-
-    @Override
-    public void addAjaxUpdate(WicketBuildContext ctx, Component component, IModel<SInstance> model, IAjaxUpdateListener listener) {
-        component.add(new SingularEventsHandlers(SingularEventsHandlers.FUNCTION.ADD_TEXT_FIELD_HANDLERS)
-                .setOption(OPTS_ORIGINAL_PROCESS_EVENT, JS_PICKLIST_CHANGE_EVENT)
-                .setOption(OPTS_ORIGINAL_VALIDATE_EVENT, JS_PICKLIST_CHANGE_EVENT))
-                .add(AjaxUpdateInputBehavior.forProcess(model, listener))
-                .add(AjaxUpdateInputBehavior.forValidate(model, listener));
     }
 }
