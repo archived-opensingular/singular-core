e0c5b8bdef964350ad9da400b1853765df1e27e6
vinicius.nunes <vinicius.nunes@opensingular.com>
2018-08-15 10:13:06 -0300

    [BUGFIX] fixing wicketserializationdebugutil, fixing wicket debug property usage, improving serialization configuration and modurality

--- lib/wicket-utils/src/main/java/org/opensingular/lib/wicket/util/application/LZ4Serializer.java
+++ lib/wicket-utils/src/main/java/org/opensingular/lib/wicket/util/application/LZ4Serializer.java
@@ -28,34 +28,28 @@ import java.io.Serializable;
 import java.nio.ByteBuffer;
 
 /**
- * Wraps the FST serializer combined with the LZ4 compression algorithm
+ * Decorates a {@link ISerializer} with the LZ4 compression algorithm
+ * Default constructor configures the {@link FSTSerializer}
  */
-public class SingularSerializer implements ISerializer {
+public class LZ4Serializer implements ISerializer {
 
-    private final FSTConfiguration    conf         = FSTConfiguration.createDefaultConfiguration();
+
     private final LZ4Factory          factory      = LZ4Factory.fastestInstance();
     private final LZ4Compressor       compressor   = factory.fastCompressor();
     private final LZ4FastDecompressor decompressor = factory.fastDecompressor();
+    private final ISerializer serializer;
 
-    public SingularSerializer(String... packages) {
-        SingularClassPathScanner.get().findSubclassesOf(Serializable.class, "org.opensingular").forEach(this::registerClass);
-        SingularClassPathScanner.get().findSubclassesOf(Serializable.class, "com.opensingular").forEach(this::registerClass);
-        SingularClassPathScanner.get().findSubclassesOf(Serializable.class, "net.sf.cglib").forEach(this::registerClass);
-        SingularClassPathScanner.get().findSubclassesOf(Serializable.class, "org.javassist").forEach(this::registerClass);
-        SingularClassPathScanner.get().findSubclassesOf(Serializable.class, "org.springframework.beans").forEach(this::registerClass);
-        SingularClassPathScanner.get().findSubclassesOf(Serializable.class, "org.apache.wicket").forEach(this::registerClass);
-        for (String pequiqueiji : packages) {
-            SingularClassPathScanner.get().findSubclassesOf(Serializable.class, pequiqueiji).forEach(this::registerClass);
-        }
+    public LZ4Serializer(ISerializer serializer) {
+        this.serializer = serializer;
     }
 
-    public void registerClass(Class... c) {
-        conf.registerClass(c);
+    public LZ4Serializer() {
+        this(new FSTSerializer());
     }
 
     @Override
     public byte[] serialize(Object object) {
-        byte[] content = conf.asByteArray(object);
+        byte[] content = serializer.serialize(object);
         int length = content.length;
         byte[] lengthBytes = ByteBuffer.allocate(4).putInt(length).array();
         return ArrayUtils.addAll(lengthBytes, compressor.compress(content));
@@ -65,6 +59,6 @@ public class SingularSerializer implements ISerializer {
     public Object deserialize(byte[] data) {
         int length = ByteBuffer.wrap(ArrayUtils.subarray(data, 0, 4)).getInt();
         byte[] content = ArrayUtils.subarray(data, 4, data.length);
-        return conf.asObject(decompressor.decompress(content, length));
+        return serializer.deserialize(decompressor.decompress(content, length));
     }
 }
