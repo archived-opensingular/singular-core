02d6ff47369afb1b6659e3d0e003e705dfb47f56
vinicius.nunes <vinicius.nunes@opensingular.com>
2019-07-19 16:41:24 -0300

    [RELEASE] SINGULAR 1.9.1 RC 6

--- form/wicket/src/main/java/org/opensingular/form/wicket/util/OcrUtil.java
+++ form/wicket/src/main/java/org/opensingular/form/wicket/util/OcrUtil.java
@@ -4,19 +4,24 @@ import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.ITesseract.RenderedFormat;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
+import org.apache.commons.io.IOUtils;
 import org.opensingular.lib.commons.base.SingularException;
 import org.opensingular.lib.commons.base.SingularProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.text.MessageFormat;
 import java.util.Collections;
 import java.util.UUID;
 
 public class OcrUtil {
 
-    public static final String PDF_TO_TIFF_COMMAND       = "singular.fileupload.ocr.pdf_to_tiff.command";
-    public static final String TESSERACT_PDF_OCR_COMMAND = "singular.fileupload.ocr.tesseract.command";
+    private static final Logger LOGGER                    = LoggerFactory.getLogger(OcrUtil.class);
+    public static final  String PDF_TO_TIFF_COMMAND       = "singular.fileupload.ocr.pdf_to_tiff.command";
+    public static final  String TESSERACT_PDF_OCR_COMMAND = "singular.fileupload.ocr.tesseract.command";
 
     public static File runOcrOnPdf(String path) {
         ITesseract tesseract = new Tesseract();
@@ -40,18 +45,29 @@ public class OcrUtil {
 
     public static File runOcrOnPdfCommandLine(File pdfFile) {
         try {
-            Runtime rt        = Runtime.getRuntime();
-            File    tiffFile  = File.createTempFile(UUID.randomUUID().toString(), ".tiff");
-            Process pdfToTiff = rt.exec(MessageFormat.format(SingularProperties.get(PDF_TO_TIFF_COMMAND), pdfFile.getAbsolutePath(), tiffFile.getAbsolutePath()));
-            if (pdfToTiff.waitFor() == 0) {
-                Process tesseract = rt.exec(MessageFormat.format(SingularProperties.get(TESSERACT_PDF_OCR_COMMAND), tiffFile.getAbsolutePath(), pdfFile.getAbsolutePath()));
-                tesseract.waitFor();
-                return new File(pdfFile.getAbsolutePath() + ".pdf");
+            File   tiffFile  = File.createTempFile(UUID.randomUUID().toString(), ".tiff");
+            String pdtToTiff = MessageFormat.format(SingularProperties.get(PDF_TO_TIFF_COMMAND), pdfFile.getAbsolutePath(), tiffFile.getAbsolutePath());
+            if (runCommand(pdtToTiff)) {
+                String tesseract = MessageFormat.format(SingularProperties.get(TESSERACT_PDF_OCR_COMMAND), tiffFile.getAbsolutePath(), pdfFile.getAbsolutePath());
+                if (runCommand(tesseract)) {
+                    return new File(pdfFile.getAbsolutePath() + ".pdf");
+                }
             }
         } catch (IOException | InterruptedException e) {
             throw SingularException.rethrow(e.getMessage(), e);
         }
         return pdfFile;
+    }
+
+    private static boolean runCommand(String command) throws IOException, InterruptedException {
+        LOGGER.info("Executing command Line: $" + command);
+        Runtime rt = Runtime.getRuntime();
+        Process pr = rt.exec(command);
+        LOGGER.info(IOUtils.toString(pr.getInputStream(), StandardCharsets.UTF_8));
+        LOGGER.error(IOUtils.toString(pr.getErrorStream(), StandardCharsets.UTF_8));
+        int exitValue = pr.waitFor();
+        LOGGER.info("Exit Value:" + exitValue);
+        return exitValue == 0;
     }
 
 }
